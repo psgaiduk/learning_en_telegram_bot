fbfb20901ce78d745113cdddf5f2dbf21a5a467c
psgaiduk <psgaiduk@gmail.com>
2023-03-03 10:45:18 +0400

    create logger

--- bot.py
+++ bot.py
@@ -1,7 +1,19 @@
+from os import path
+
 from aiogram.utils import executor
+from loguru import logger
 
 from telegram_bot_app.core import dispatcher
 from telegram_bot_app.handlers import *
+
+
+file_path = path.join(path.dirname(__file__), 'logs', 'debug', 'logs.log')
+logger.add(
+    file_path,
+    format="{time:YYYY-MM-DD HH:mm:ss} || {extra[chat_id]} || {extra[work_id]} || {level} || {message}",
+    level="DEBUG",
+    rotation='00:00',
+)
 
 
 if __name__ == '__main__':
