9195391a87a345fff5336397be9d8d6954066a25
psgaiduk <psgaiduk@gmail.com>
2023-03-03 10:45:39 +0400

    add logger for cmd start

--- telegram_bot_app/handlers/start.py
+++ telegram_bot_app/handlers/start.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext
+from loguru import logger
 
 from telegram_bot_app.core import dispatcher
 
@@ -7,6 +10,9 @@ from telegram_bot_app.core import dispatcher
 @dispatcher.message_handler(commands='start')
 async def cmd_start(message: types.Message, state: FSMContext):
     """Work with command start."""
+    chat_id = message.from_id
+    logger.configure(extra={'chat_id': chat_id, 'work_id': datetime.now().timestamp()})
+    logger.debug(f'message = {message}')
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add('Отменить уроки', 'Перенести уроки')
