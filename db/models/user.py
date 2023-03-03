59033f2f5824c9dfa5e8bbfa075ff92d1dd1848b
psgaiduk <psgaiduk@gmail.com>
2023-03-03 10:38:23 +0400

    add new column in User model

--- db/models/user.py
+++ db/models/user.py
@@ -3,10 +3,10 @@ from sqlalchemy import Column, Integer, String
 from db.models.base import Base
 
 
-
 class User(Base):
     """Model of user."""
     __tablename__ = 'user'
 
     telegram_id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
+    level = Column(Integer)
