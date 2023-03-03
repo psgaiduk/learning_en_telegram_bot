205e0ef6d346c501faeb556145444d0e1032a329
psgaiduk <psgaiduk@gmail.com>
2023-03-03 10:28:48 +0400

    add telegram bot token

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ class MainSettings(BaseSettings):
     environment: str = Field(..., env='ENVIRONMENT')
     openai_token: str = Field(..., env='OPEN_AI_TOKEN')
     path_to_database: str = path.join(path.dirname(__file__), 'database.db')
+    telegram_token: str = Field(..., env='TELEGRAM_BOT_TOKEN')
 
     class Config:
         """Config."""
