dcd37d8ff2b969e2334cca8f6e0be182d724530f
psgaiduk <psgaiduk@gmail.com>
2023-03-09 00:58:18 +0400

    rename table user

--- db/models/users.py
+++ db/models/users.py
@@ -3,10 +3,10 @@ from sqlalchemy import Column, Integer, String
 from db.models.base import Base
 
 
-class User(Base):
+class Users(Base):
     """Model of user."""
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
-    telegram_id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, primary_key=True)
     name = Column(String)
     level = Column(Integer)
