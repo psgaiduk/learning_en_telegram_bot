3c23c2d28e5d6d12d1fab4c49afc1ca4f604bf9d
psgaiduk <psgaiduk@gmail.com>
2023-03-11 23:20:37 +0400

    add create base

--- db/core/__init__.py
+++ db/core/__init__.py
@@ -6,5 +6,5 @@ from settings import settings
 
 
 engine = create_engine(f'sqlite:///{settings.path_to_database}', echo=True)
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 Session = sessionmaker(autoflush=False, bind=engine)
