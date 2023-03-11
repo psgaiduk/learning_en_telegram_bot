c18bc6442fabb77a49654e8e88de98d0da495d9d
psgaiduk <psgaiduk@gmail.com>
2023-03-11 20:31:59 +0400

    update function add new text to db for new model Text

--- db/utils/add_new_text.py
+++ db/utils/add_new_text.py
@@ -1,8 +1,12 @@
 from openai_app.functions import create_new_text
 from db.functions.texts import create_text
+from nlp_translate_app.utils import translate_text
 
 
 def add_new_text_to_db():
-    new_text = create_new_text()
-    create_text(text=new_text)
-    return new_text
+    new_en_text = create_new_text()
+    text_ru = translate_text(text_on_en=new_en_text, language='ru')
+    text_es = translate_text(text_on_en=new_en_text, language='es')
+    text_ge = translate_text(text_on_en=new_en_text, language='de')
+    text_fr = translate_text(text_on_en=new_en_text, language='fr')
+    create_text(text_en=new_en_text, text_ru=text_ru, text_es=text_es, text_fr=text_fr, text_ge=text_ge)
