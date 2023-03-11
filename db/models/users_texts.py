fa70029d97b5784846d60b58467a9dcf8c80cddf
psgaiduk <psgaiduk@gmail.com>
2023-03-11 22:12:47 +0400

    update date field in model users text

--- db/models/users_texts.py
+++ db/models/users_texts.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Date, func, String, Integer
+from sqlalchemy import Column, Date, String, Integer
 from sqlalchemy import Table
 from sqlalchemy import ForeignKey
 
@@ -9,7 +9,7 @@ users_texts = Table(
     'users_texts',
     Base.metadata,
     Column('id', Integer, index=True, primary_key=True),
-    Column('date', Date, server_default=func.now()),
+    Column('date', Date),
     Column('language', String),
     Column('user_telegram_id', ForeignKey('users.telegram_id')),
     Column('text_id', ForeignKey('texts.id')),
