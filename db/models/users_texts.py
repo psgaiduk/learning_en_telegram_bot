74815e06761186550e9fa607b84744d1f8b4c85b
psgaiduk <psgaiduk@gmail.com>
2023-03-11 20:30:33 +0400

    add language settings in model users texts

--- db/models/users_texts.py
+++ db/models/users_texts.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column
+from sqlalchemy import Column, Date, func, String
 from sqlalchemy import Table
 from sqlalchemy import ForeignKey
 
@@ -8,6 +8,8 @@ from db.models.base import Base
 users_texts = Table(
     'users_texts',
     Base.metadata,
+    Column('date', Date, server_default=func.now()),
+    Column('language', String),
     Column('user_telegram_id', ForeignKey('users.telegram_id'), primary_key=True),
     Column('text_id', ForeignKey('texts.id'), primary_key=True),
 )
