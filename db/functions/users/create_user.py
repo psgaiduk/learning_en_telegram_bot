420dfc6ab46ebe811f078bbdc9ffcee55a4e5c35
psgaiduk <psgaiduk@gmail.com>
2023-03-11 20:31:11 +0400

    update create user for new model User

--- db/functions/users/create_user.py
+++ db/functions/users/create_user.py
@@ -4,7 +4,7 @@ from db.models import Users
 
 async def create_user(telegram_id: int, name: str):
     with Session() as session:
-        new_user = Users(telegram_id=telegram_id, name=name, level=1)
+        new_user = Users(telegram_id=telegram_id, name=name, level=3, main_language='ru', learn_language='en')
         session.add(new_user)
         session.commit()
         session.refresh(new_user)
