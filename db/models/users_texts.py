bcf3f263cdbe0a1563c83c96d966b273d40cd359
psgaiduk <psgaiduk@gmail.com>
2023-03-11 20:37:10 +0400

    add column id for users texts

--- db/models/users_texts.py
+++ db/models/users_texts.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Date, func, String
+from sqlalchemy import Column, Date, func, String, Integer
 from sqlalchemy import Table
 from sqlalchemy import ForeignKey
 
@@ -8,8 +8,9 @@ from db.models.base import Base
 users_texts = Table(
     'users_texts',
     Base.metadata,
+    Column('id', Integer, index=True, primary_key=True),
     Column('date', Date, server_default=func.now()),
     Column('language', String),
-    Column('user_telegram_id', ForeignKey('users.telegram_id'), primary_key=True),
-    Column('text_id', ForeignKey('texts.id'), primary_key=True),
+    Column('user_telegram_id', ForeignKey('users.telegram_id')),
+    Column('text_id', ForeignKey('texts.id')),
 )
