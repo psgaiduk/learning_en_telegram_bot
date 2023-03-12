9f5d854736ed178e687586f1242b6866096cb47f
psgaiduk <psgaiduk@gmail.com>
2023-03-12 15:34:45 +0400

    delete not used code

--- db/core/__init__.py
+++ db/core/__init__.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from db.models import Base
 from settings import settings
 
 
 engine = create_engine(f'sqlite:///{settings.path_to_database}', echo=True)
-# Base.metadata.create_all(bind=engine)
 Session = sessionmaker(autoflush=False, bind=engine)
