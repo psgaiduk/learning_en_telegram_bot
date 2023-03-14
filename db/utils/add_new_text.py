2b2412c3647be44632366c3930f0d4f596716fe1
psgaiduk <psgaiduk@gmail.com>
2023-03-14 23:49:47 +0400

    update translate method

--- db/utils/add_new_text.py
+++ db/utils/add_new_text.py
@@ -5,7 +5,7 @@ from nlp_translate_app.utils import translate_text
 
 def add_new_text_to_db():
     new_en_text = create_new_text()
-    text_ru = translate_text(text_on_en=new_en_text, language='ru')
+    text_ru = translate_text(text_on_en=new_en_text, language='RU')
     # text_es = translate_text(text_on_en=new_en_text, language='es')
     # text_ge = translate_text(text_on_en=new_en_text, language='de')
     # text_fr = translate_text(text_on_en=new_en_text, language='fr')
--- nlp_translate_app/utils/translate_text.py
+++ nlp_translate_app/utils/translate_text.py
@@ -1,41 +1,25 @@
-from requests import get
-from nltk.text import sent_tokenize
+from requests import post
 
 from settings import settings
 
 
 def translate_text(text_on_en: str, language: str) -> str:
-    MAX_LETTERS = 800
-    url = 'https://nlp-translation.p.rapidapi.com/v1/translate'
+    url = 'https://deepl-translator.p.rapidapi.com/translate'
+
     headers = {
+        'content-type': 'application/json',
         'X-RapidAPI-Key': settings.nlp_token,
-        'X-RapidAPI-Host': 'nlp-translation.p.rapidapi.com'
+        'X-RapidAPI-Host': 'deepl-translator.p.rapidapi.com',
     }
 
-    translate = []
     text_on_en = text_on_en.replace('\n', '').replace('.', '. ')
-    sentences_en = sent_tokenize(text=text_on_en, language='english')
-    part_text_for_translate = ''
 
-    for sentence in sentences_en:
-        if len(part_text_for_translate + sentence) > MAX_LETTERS:
-            params_for_translate = {
-                'text': part_text_for_translate,
-                'to': language,
-                'from': 'en',
-            }
-            response = get(url=url, params=params_for_translate, headers=headers).json()
-            translate.append(response.get('translated_text', {}).get(language))
-            part_text_for_translate = ''
-        else:
-            part_text_for_translate += sentence
-
-    params_for_translate = {
-        'text': part_text_for_translate,
-        'to': language,
-        'from': 'en',
+    payload = {
+        'text': text_on_en,
+        'source': 'EN',
+        'target': language,
     }
-    response = get(url=url, params=params_for_translate, headers=headers).json()
-    translate.append(response.get('translated_text', {}).get(language))
 
-    return ''.join(translate)
+    response = post(url, json=payload, headers=headers).json()
+
+    return response['text']
