c7f1c2ae92c45d64d2c0cf3b25721ea6d7ce9e8a
psgaiduk <psgaiduk@gmail.com>
2023-03-14 23:22:00 +0400

    not get es, fr, ge translate

--- db/utils/add_new_text.py
+++ db/utils/add_new_text.py
@@ -6,7 +6,7 @@ from nlp_translate_app.utils import translate_text
 def add_new_text_to_db():
     new_en_text = create_new_text()
     text_ru = translate_text(text_on_en=new_en_text, language='ru')
-    text_es = translate_text(text_on_en=new_en_text, language='es')
-    text_ge = translate_text(text_on_en=new_en_text, language='de')
-    text_fr = translate_text(text_on_en=new_en_text, language='fr')
-    create_text(text_en=new_en_text, text_ru=text_ru, text_es=text_es, text_fr=text_fr, text_ge=text_ge)
+    # text_es = translate_text(text_on_en=new_en_text, language='es')
+    # text_ge = translate_text(text_on_en=new_en_text, language='de')
+    # text_fr = translate_text(text_on_en=new_en_text, language='fr')
+    create_text(text_en=new_en_text, text_ru=text_ru, text_es='', text_fr='', text_ge='')
