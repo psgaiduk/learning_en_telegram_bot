f94d89275796222c7aa6a09f17e4fee9c7629805
psgaiduk <psgaiduk@gmail.com>
2023-03-16 21:38:44 +0400

    make function for add texts

--- add_new_text.py
+++ add_new_text.py
@@ -1,3 +1,10 @@
+from time import sleep
+
 from db.utils import add_new_text_to_db
 
-add_new_text_to_db()
+if __name__ == '__main__':
+    eight_hours_in_seconds = 60 * 60 * 8
+    while True:
+        add_new_text_to_db()
+        sleep(eight_hours_in_seconds)
+
