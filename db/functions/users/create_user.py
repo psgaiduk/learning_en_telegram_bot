c32a74e2a4ff96d40bc7ca813865afa5a3c6430e
psgaiduk <psgaiduk@gmail.com>
2023-03-25 20:56:58 +0400

    add level for create user

--- db/functions/users/create_user.py
+++ db/functions/users/create_user.py
@@ -2,9 +2,9 @@ from db.core import Session
 from db.models import Users
 
 
-async def create_user(telegram_id: int, name: str):
+async def create_user(telegram_id: int, name: str, level: int = 1):
     with Session() as session:
-        new_user = Users(telegram_id=telegram_id, name=name, level=3, main_language='ru', learn_language='en')
+        new_user = Users(telegram_id=telegram_id, name=name, level=level, main_language='ru', learn_language='en')
         session.add(new_user)
         session.commit()
         session.refresh(new_user)
