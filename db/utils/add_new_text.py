6f7821bf5b42cef4c0c5a9c044c7eb685b67e4c8
psgaiduk <psgaiduk@gmail.com>
2023-03-25 20:21:06 +0400

    add levels for texts

--- db/utils/add_new_text.py
+++ db/utils/add_new_text.py
@@ -4,9 +4,7 @@ from nlp_translate_app.utils import translate_text
 
 
 def add_new_text_to_db():
-    new_en_text = create_new_text()
-    text_ru = translate_text(text_on_en=new_en_text, language='RU')
-    # text_es = translate_text(text_on_en=new_en_text, language='es')
-    # text_ge = translate_text(text_on_en=new_en_text, language='de')
-    # text_fr = translate_text(text_on_en=new_en_text, language='fr')
-    create_text(text_en=new_en_text, text_ru=text_ru, text_es='', text_fr='', text_ge='')
+    for level_id in range(4):
+        new_en_text = create_new_text(level_id=level_id)
+        text_ru = translate_text(text_on_en=new_en_text, language='RU')
+        create_text(text_en=new_en_text, text_ru=text_ru, text_es='', text_fr='', text_ge='', level=level_id)
--- openai_app/functions/create_new_text.py
+++ openai_app/functions/create_new_text.py
@@ -5,29 +5,29 @@ import openai
 from settings import settings
 
 
-def create_new_text() -> str:
+def create_new_text(level_id: int) -> str:
     """
     Create new text from chatGPT.
 
     :return: text.
     """
-    authors = [
-        'Лев Толстой',
-        'Владимир Набоков',
-        'Фёдор Достевскй',
-        'Уильям Фолкнер',
-        'Чарльз Диккенс',
-        'Антон Чехов',
-        'Гюстав Флобер',
-        'Джейн Остин',
-    ]
 
     openai.api_key = settings.openai_token
 
+    levels = {
+        0: {'min_words': 50, 'max_words': 100, 'words': 500, 'authors': ['Милн', 'Кэрролл', 'Поттер', 'Льюис', 'Браун']},
+        1: {'min_words': 100, 'max_words': 200, 'words': 1000, 'authors': ['Кинни', 'Риордан', 'Паласио', 'Пилки']},
+        2: {'min_words': 200, 'max_words': 300, 'words': 2500, 'authors': ['Чехов', 'Фолкнер', 'Флобер', 'Диккинс', 'Бакман']},
+        3: {'min_words': 300, 'max_words': 400, 'words': 5000, 'authors': ['Достоевский', 'Набоков', 'Гюго', 'Толстой', 'Джойс']},
+    }
+
+    level = levels.get(level_id)
+
     text = (
         'Напиши короткий рассказ на английском.',
-        'В рассказе должно быть от 300 до 400 слов из списка 5000 самых распространнённых слов английского языка.',
-        f'Рассказ должен быть в стиле писателя {choice(authors)}',
+        f'В рассказе должно быть от {level["min_words"]} до {level["max_words"]} слов из списка ',
+        f'{level["words"]} самых распространнённых слов английского языка.',
+        f'Рассказ должен быть в стиле писателя {choice(level["authors"])}',
     )
 
     completion = openai.ChatCompletion.create(
