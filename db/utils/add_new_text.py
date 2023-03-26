c9f78a256bae29cab6a4953d2376971012d9d5b6
psgaiduk <psgaiduk@gmail.com>
2023-03-26 13:08:11 +0400

    add languages

--- db/utils/add_new_text.py
+++ db/utils/add_new_text.py
@@ -4,8 +4,18 @@ from nlp_translate_app.utils import translate_text
 
 
 def add_new_text_to_db():
-    for level_id in range(1):
+    for level_id in range(4):
         new_en_text = create_new_text(level_id=level_id)
         text_ru = translate_text(text_on_en=new_en_text, language='RU')
+        text_es = translate_text(text_on_en=new_en_text, language='ES')
+        text_fr = translate_text(text_on_en=new_en_text, language='FR')
+        text_ge = translate_text(text_on_en=new_en_text, language='DE')
 
-        create_text(text_en=new_en_text, text_ru=text_ru, text_es='', text_fr='', text_ge='', level=level_id)
+        create_text(
+            text_en=new_en_text,
+            text_ru=text_ru,
+            text_es=text_es,
+            text_fr=text_fr,
+            text_ge=text_ge,
+            level=level_id,
+        )
