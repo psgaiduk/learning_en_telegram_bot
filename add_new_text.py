b748370aea8f228719362baddf19044ce07d8349
psgaiduk <psgaiduk@gmail.com>
2023-03-26 13:41:16 +0400

    return cycle

--- add_new_text.py
+++ add_new_text.py
@@ -1,4 +1,9 @@
+from time import sleep
+
 from db.utils import add_new_text_to_db
 
 if __name__ == '__main__':
-    add_new_text_to_db()
+    eight_hours_in_seconds = 60 * 60 * 8
+    while True:
+        add_new_text_to_db()
+        sleep(eight_hours_in_seconds)
