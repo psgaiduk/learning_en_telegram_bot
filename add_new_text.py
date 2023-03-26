913dd3cbe5b2ad46cfed4a303a2f3e7bbdc3cb63
psgaiduk <psgaiduk@gmail.com>
2023-03-26 13:27:48 +0400

    work one time add new text

--- add_new_text.py
+++ add_new_text.py
@@ -1,10 +1,4 @@
-from time import sleep
-
 from db.utils import add_new_text_to_db
 
 if __name__ == '__main__':
-    eight_hours_in_seconds = 60 * 60 * 8
-    while True:
-        add_new_text_to_db()
-        sleep(eight_hours_in_seconds)
-
+    add_new_text_to_db()
