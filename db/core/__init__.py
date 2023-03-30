f739d5bd10d11a90d647f6e677a28c2bdaf63ca4
psgaiduk <psgaiduk@gmail.com>
2023-03-30 10:10:58 +0400

    update core

--- db/core/__init__.py
+++ db/core/__init__.py
@@ -4,5 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from settings import settings
 
 
-engine = create_engine(f'postgresql://{settings.postgres.user}:{settings.postgres.password}@{settings.postgres.host}/{settings.postgres.db_name}', echo=True)
+engine = create_engine(
+    'postgresql://{user}:{password}@{host}/{db_name}'.format(
+        user=settings.postgres.user,
+        password=settings.postgres.password,
+        host=settings.postgres.host,
+        db_name=settings.postgres.db_name,
+    ),
+    echo=True,
+)
 Session = sessionmaker(autoflush=False, bind=engine)
