cdd782a6bcd017ebadd00a618e0caa8a0f8ccf93
psgaiduk <psgaiduk@gmail.com>
2023-03-31 20:53:50 +0400

    create get users who not read

--- db/functions/users/__init__.py
+++ db/functions/users/__init__.py
@@ -2,3 +2,4 @@ from db.functions.users.create_user import create_user
 from db.functions.users.get_user_by_id import get_user_by_telegram_id
 from db.functions.users.update_level import update_user_level
 from db.functions.users.get_all_users import get_all_users
+from db.functions.users.get_users_who_not_read import get_users_who_not_read_today
