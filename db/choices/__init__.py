3dd000b3f0d2891cc4fb39890f525eb09de737e5
psgaiduk <psgaiduk@gmail.com>
2023-03-31 21:32:49 +0400

    create reminders type

---
+++ db/choices/__init__.py
@@ -0,0 +1,1 @@
+from db.choices.type_reminders import RemindersType
---
+++ db/choices/type_reminders.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class RemindersType(Enum):
+    """Type of reminders."""
+
+    start_read_text = 'start_read_text'
+    end_read_text = 'end_read_text'
