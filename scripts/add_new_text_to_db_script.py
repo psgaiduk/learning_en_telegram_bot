8b5a18ce4eb60daee737ad97fbccb54388747892
psgaiduk <psgaiduk@gmail.com>
2023-04-02 19:40:27 +0400

    add scripts for work cron

---
+++ scripts/add_new_text_to_db_script.py
@@ -0,0 +1,4 @@
+from db.utils import add_new_text_to_db
+
+if __name__ == "__main__":
+    add_new_text_to_db()
---
+++ scripts/send_reminders_script.py
@@ -0,0 +1,4 @@
+from telegram_messenger_app.functions import send_reminders
+
+if __name__ == "__main__":
+    send_reminders()
