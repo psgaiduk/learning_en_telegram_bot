342f1a5c601cc302fd69c64f0c60e409165c834e
psgaiduk <psgaiduk@gmail.com>
2023-04-02 19:47:21 +0400

    add logger for scripts

--- scripts/add_new_text_to_db_script.py
+++ scripts/add_new_text_to_db_script.py
@@ -1,4 +1,7 @@
+from loguru import logger
+
 from db.utils import add_new_text_to_db
 
 if __name__ == "__main__":
+    logger.configure(extra={'chat_id': 1, 'work_id': 1})
     add_new_text_to_db()
--- scripts/send_reminders_script.py
+++ scripts/send_reminders_script.py
@@ -1,4 +1,7 @@
+from loguru import logger
+
 from telegram_messenger_app.functions import send_reminders
 
 if __name__ == "__main__":
+    logger.configure(extra={'chat_id': 1, 'work_id': 1})
     send_reminders()
