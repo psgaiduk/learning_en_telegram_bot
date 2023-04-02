52f9dc41fd2d8167d1d1c0e8548d64f58a40c236
psgaiduk <psgaiduk@gmail.com>
2023-04-02 20:01:20 +0400

    fix work scripts

--- scripts/add_new_text_to_db_script.py
+++ scripts/add_new_text_to_db_script.py
@@ -1,11 +1,15 @@
-from loguru import logger
-
-from db.utils import add_new_text_to_db
 import os
 import sys
 
+# Используйте абсолютный путь к корневому каталогу вашего проекта
 project_root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root_path)
+
+
+from loguru import logger
+
+from db.utils import add_new_text_to_db
+
 
 if __name__ == "__main__":
     logger.configure(extra={'chat_id': 1, 'work_id': 1})
--- scripts/send_reminders_script.py
+++ scripts/send_reminders_script.py
@@ -1,11 +1,14 @@
-from loguru import logger
-
-from telegram_messenger_app.functions import send_reminders
 import os
 import sys
 
+# Используйте абсолютный путь к корневому каталогу вашего проекта
 project_root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root_path)
+
+
+from loguru import logger
+
+from telegram_messenger_app.functions import send_reminders
 
 if __name__ == "__main__":
     logger.configure(extra={'chat_id': 1, 'work_id': 1})
