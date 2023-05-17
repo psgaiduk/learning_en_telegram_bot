7b51e09b240920b9d704c707a2d438fee91f3b7a
psgaiduk <psgaiduk@gmail.com>
2023-05-17 23:59:57 +0400

    update words model

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -3,5 +3,5 @@ from db.models.users import Users
 from db.models.texts import Texts
 from db.models.users_texts import UsersText
 from db.models.current_user_text import CurrentUserText
-from db.models.words import Word
+from db.models.words import Words
 from db.models.text_reminder import TextReminder
--- db/models/words.py
+++ db/models/words.py
@@ -1,10 +1,19 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 
 from db.models.base import Base
 
 
-class Word(Base):
+class Words(Base):
+    """Model of user."""
     __tablename__ = 'words'
 
-    english = Column(String, primary_key=True)
-    russian = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    level = Column(Integer)
+    words_en = Column(String)
+    words_ru = Column(String)
+    words_fr = Column(String)
+    words_es = Column(String)
+    words_ge = Column(String)
+
+
+
