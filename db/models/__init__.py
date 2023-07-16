43967dbd0d55a75f97a507229215cc84bea99a04
psgaiduk <psgaiduk@gmail.com>
2023-07-16 20:51:15 +0400

    create model users referrals

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -2,3 +2,4 @@ from db.models.base import Base
 from db.models.users import Users
 from db.models.hero_levels import HeroLevels
 from db.models.levels_en import LevelsEn
+from db.models.users_referrals import UsersReferrals
