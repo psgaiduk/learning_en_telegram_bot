df250236e9fd15d9e51f35e1bd3844f36dd888a3
psgaiduk <psgaiduk@gmail.com>
2023-07-21 20:27:10 +0400

    create model type words

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -19,3 +19,4 @@ from db.models.type_grammar_exercises import TypeGrammarExercises
 from db.models.achievements import Achievements
 from db.models.users_achievements_history import UsersAchievementsHistory
 from db.models.users_grammar_exercises_history import UsersGrammarExercisesHistory
+from db.models.type_words import TypeWords
---
+++ db/models/type_words.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+
+from db.models.base import Base
+
+
+class TypeWords(Base):
+    """Model of type of words."""
+
+    __tablename__ = 'type_words'
+
+    type_word_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(128))
+
