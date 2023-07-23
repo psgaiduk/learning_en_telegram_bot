2361f093bb1dda9e847c166f72f21e376e9b9915
psgaiduk <psgaiduk@gmail.com>
2023-07-23 21:21:21 +0400

    create context manager

--- bot/db/core/__init__.py
+++ bot/db/core/__init__.py
@@ -4,13 +4,30 @@ from sqlalchemy.orm import sessionmaker
 from settings import settings
 
 
-engine = create_engine(
-    'postgresql://{user}:{password}@{host}/{db_name}'.format(
-        user=settings.postgres.user,
-        password=settings.postgres.password,
-        host=settings.postgres.host,
-        db_name=settings.postgres.db_name,
-    ),
-    echo=True,
-)
-Session = sessionmaker(autoflush=False, bind=engine)
+class DatabaseSessionManager:
+    def __init__(self, commit=True, database_url=None):
+        self.commit = commit
+        if database_url is None:
+            database_url = 'postgresql://{user}:{password}@{host}/{db_name}'.format(
+                user=settings.postgres.user,
+                password=settings.postgres.password,
+                host=settings.postgres.host,
+                db_name=settings.postgres.db_name,
+            )
+        echo = False
+        if settings.environment == 'local':
+            echo = True
+        self.engine = create_engine(database_url, echo=echo)
+        self.Session = sessionmaker(bind=self.engine, autoflush=True)
+
+    def __enter__(self):
+        self.session = self.Session()
+        return self.session
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.commit:
+            if exc_type is None:
+                self.session.commit()
+            else:
+                self.session.rollback()
+        self.session.close()
