9affb00056bd41c9da749cbc61b54ecb61f6d589
psgaiduk <psgaiduk@gmail.com>
2023-07-24 09:47:12 +0400

    add import userssubscribes

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -4,6 +4,7 @@ from db.models.hero_levels import HeroLevels
 from db.models.levels_en import LevelsEn
 from db.models.users_referrals import UsersReferrals
 from db.models.subscribes import Subscribes
+from db.models.users_subscribes import UsersSubscribes
 from db.models.users_games_history import UsersGamesHistory
 from db.models.games import Games
 from db.models.main_languages import MainLanguages
