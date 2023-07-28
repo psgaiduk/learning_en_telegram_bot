1fc0bdca92e58fe0c59c96ea704207dd135e36d3
psgaiduk <psgaiduk@gmail.com>
2023-07-28 10:14:31 +0400

    add columns for display

--- admin/telegram_users/admin/hero_levels.py
+++ admin/telegram_users/admin/hero_levels.py
@@ -5,4 +5,4 @@ from telegram_users.models import HeroLevelsModel
 
 @admin.register(HeroLevelsModel)
 class HeroLevelsAdmin(admin.ModelAdmin):
-    list_display = ('__str__', )
+    list_display = ('__str__', 'need_experience', 'count_sentences', 'count_games', 'order')
