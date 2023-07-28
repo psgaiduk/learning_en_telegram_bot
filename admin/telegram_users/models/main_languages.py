5cc7353ff165b7ebad0a42eb4505c7f5d53b0a5d
psgaiduk <psgaiduk@gmail.com>
2023-07-28 10:04:51 +0400

    add pretty name

--- admin/telegram_users/models/main_languages.py
+++ admin/telegram_users/models/main_languages.py
@@ -7,5 +7,8 @@ class MainLanguagesModel(Model):
     """Model of main languages."""
     title = CharField(max_length=64, choices=Language.choices())
 
+    def __str__(self) -> str:
+        return f'{self.get_title_display()}'
+
     class Meta:
         db_table = 'main_languages'
