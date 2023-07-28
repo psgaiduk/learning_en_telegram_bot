fcb59333e1c778c9488e1416ad446eaa43e6f0a5
psgaiduk <psgaiduk@gmail.com>
2023-07-28 10:17:12 +0400

    add blank true

--- admin/telegram_users/models/telegram_users.py
+++ admin/telegram_users/models/telegram_users.py
@@ -7,11 +7,11 @@ class TelegramUsersModel(Model):
     telegram_id = BigIntegerField(primary_key=True)
     level_en = ForeignKey('LevelsEnModel', on_delete=SET_NULL, null=True)
     main_language = ForeignKey('MainLanguagesModel', on_delete=CASCADE)
-    user_name = CharField(max_length=64, null=True)
+    user_name = CharField(max_length=64, null=True, blank=True)
     experience = BigIntegerField(default=0)
     hero_level = ForeignKey('HeroLevelsModel', on_delete=CASCADE)
-    previous_stage = CharField(max_length=64, null=True)
-    stage = CharField(max_length=64, null=True)
+    previous_stage = CharField(max_length=64, null=True, blank=True)
+    stage = CharField(max_length=64, null=True, blank=True)
 
     def __str__(self) -> str:
         return f'{self.telegram_id} - {self.user_name}'
