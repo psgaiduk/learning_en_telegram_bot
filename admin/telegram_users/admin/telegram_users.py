f63f33a012bcc1d5531d1e948515047586d9d62f
psgaiduk <psgaiduk@gmail.com>
2023-07-28 10:38:22 +0400

    update filters

--- admin/telegram_users/admin/telegram_users.py
+++ admin/telegram_users/admin/telegram_users.py
@@ -6,5 +6,5 @@ from telegram_users.models import TelegramUsersModel
 @admin.register(TelegramUsersModel)
 class TelegramUsersAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'level_en', 'main_language', 'hero_level', 'stage', 'experience')
-    list_filter = ('level_en', 'main_language', 'hero_level')
+    list_filter = ('level_en__title', 'main_language__title', 'hero_level__title', 'stage')
     search_fields = ('telegram_id',)
