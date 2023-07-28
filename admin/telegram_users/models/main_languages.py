d47d05577463575491180b760d5c63efe1c775f3
psgaiduk <psgaiduk@gmail.com>
2023-07-28 10:00:49 +0400

    add choices for model

--- admin/telegram_users/models/main_languages.py
+++ admin/telegram_users/models/main_languages.py
@@ -1,9 +1,11 @@
 from django.db.models import CharField, Model
 
+from telegram_users.choices import Language
+
 
 class MainLanguagesModel(Model):
     """Model of main languages."""
-    title = CharField(max_length=64)
+    title = CharField(max_length=64, choices=Language.choices())
 
     class Meta:
         db_table = 'main_languages'
