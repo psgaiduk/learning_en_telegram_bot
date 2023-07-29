4b36b38e5330f0916c8fe0e0e1b2fd6d43b54b96
psgaiduk <psgaiduk@gmail.com>
2023-07-29 09:21:43 +0400

    create type words choices

---
+++ admin/books/choices/__init__.py
@@ -0,0 +1,1 @@
+from books.choices.type_words import TypeWord
---
+++ admin/books/choices/type_words.py
@@ -0,0 +1,9 @@
+from app.choices import AdminChoiceEnum
+
+
+class TypeWord(AdminChoiceEnum):
+    """Type of words."""
+
+    word = ('word', 'Word')
+    phrase_verb = ('phrase_verb', 'Phrase verb')
+    idiomatic_expression = ('idiomatic_expression', 'Idiomatic expression')
