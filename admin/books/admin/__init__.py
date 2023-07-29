61b232b9f6f9dd6a6426511ffb698827d6a973bd
psgaiduk <psgaiduk@gmail.com>
2023-07-29 09:38:19 +0400

    create type words admin

--- admin/books/admin/__init__.py
+++ admin/books/admin/__init__.py
@@ -1,1 +1,2 @@
 from books.admin.books import BooksAdmin
+from books.admin.type_words import TypeWordsAdmin
---
+++ admin/books/admin/type_words.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from books.models import TypeWordsModel
+
+
+@admin.register(TypeWordsModel)
+class TypeWordsAdmin(admin.ModelAdmin):
+    """TypeWords admin."""
+
+    list_display = ('__str__', )
