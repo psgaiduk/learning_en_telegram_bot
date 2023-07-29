e714ef8b37c8652e5ec12145424b0372c3eecfad
psgaiduk <psgaiduk@gmail.com>
2023-07-29 09:23:26 +0400

    create model type words

--- admin/books/models/__init__.py
+++ admin/books/models/__init__.py
@@ -1,1 +1,2 @@
 from books.models.books import BooksModel
+from books.models.type_words import TypeWordsModel
---
+++ admin/books/models/type_words.py
@@ -0,0 +1,18 @@
+from django.db.models import AutoField, CharField, Model
+
+from books.choices import TypeWord
+
+
+class TypeWordsModel(Model):
+    """Model of type of words."""
+
+    type_word_id = AutoField(primary_key=True, choices=TypeWord.choices())
+    title = CharField(max_length=128)
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Type of word'
+        verbose_name_plural = 'Types of words'
+        db_table = 'type_words'
