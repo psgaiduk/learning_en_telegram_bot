66c169c533b47b5382345d84cd6c2083b2f61276
psgaiduk <psgaiduk@gmail.com>
2023-08-03 09:08:08 +0400

    update type word in form upload words

--- admin/books/forms/add_words.py
+++ admin/books/forms/add_words.py
@@ -1,6 +1,8 @@
 from django import forms
 
+from books.choices import TypeWord
+
 
 class WordsUploadForm(forms.Form):
     words = forms.CharField(widget=forms.Textarea, help_text="Add words here separated by a new line")
-    type_words = forms.CharField(max_length=200, help_text="Type of words")
+    type_words = forms.ChoiceField(choices=TypeWord.choices(), help_text="Type of words")
