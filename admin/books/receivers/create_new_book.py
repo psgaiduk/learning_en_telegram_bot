c5a72b03ab10b98210dd5f47ce83aab7848228b7
psgaiduk <psgaiduk@gmail.com>
2023-08-07 21:19:20 +0400

    add translate and create sentences and words

--- admin/books/receivers/create_new_book.py
+++ admin/books/receivers/create_new_book.py
@@ -1,30 +1,50 @@
+from django.db.models import Q
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from books.models import BooksModel, BooksSentencesModel, WordsModel
+from books.choices import TypeWord
+from books.models import BooksModel, BooksSentencesModel, WordsModel, TypeWordsModel
 from books.services import CreateWordsAndSentencesService
+from telegram_users.choices import Language
+from nlp_translate import translate_word, translate_text
 
 
 @receiver(post_save, sender=BooksModel)
 def create_words_and_sentences(sender, instance, created, **kwargs):
-    print('create_words_and_sentences')
     if not kwargs.get('created', False):
         instance.books_sentences.all().delete()
 
     book_text = instance.text
     sentences = CreateWordsAndSentencesService().work(text=book_text)
-    print(sentences)
 
+    for sentence in sentences:
+        words = WordsModel.objects.filter(word__in=sentence.words)
+        new_words = set(sentence.words) - set(words.values_list('word', flat=True))
+        type_word = TypeWordsModel.objects.get(title=TypeWord.word.value)
+        for word in new_words:
+            translates_word = {}
+            for language_code, _ in Language.choices():
+                translates_word[language_code] = translate_word(word=word, language=language_code)
+            WordsModel.objects.create(word=word, translation=translates_word, type_word=type_word)
 
-    # for i, sentence_text in enumerate(sentences):
-    #     # sentence = BooksSentencesModel.objects.create(
-    #     #     book=instance,
-    #     #     order=i + 1,
-    #     #     text=sentence_text,
-    #     # )
-    #
-    #     words = sentence_text.split()
-    #
-    #     for word_text in words:
-    #         word, _ = WordsModel.objects.get_or_create(word=word_text)
-    #         sentence.words.add(word)
+        translates_sentence = {}
+        for language_code, _ in Language.choices():
+            translate_sentence = translate_text(text_on_en=sentence.text, language=language_code)
+            translates_sentence[language_code] = translate_sentence
+
+        words = WordsModel.objects.filter(
+            Q(word__in=sentence.words) |
+            Q(word__in=sentence.idiomatic_expression) |
+            Q(word__in=sentence.phrase_verb)
+        )
+
+        book_sentence, created = BooksSentencesModel.objects.update_or_create(
+            book=instance,
+            order=sentence.index,
+            defaults={
+                'text': sentence.text,
+                'translation': translates_sentence,
+            }
+        )
+
+        book_sentence.words.set(words)
