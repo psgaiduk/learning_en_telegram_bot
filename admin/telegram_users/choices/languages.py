a5ddba35a2dc872183956064361eb34a7519de87
psgaiduk <psgaiduk@gmail.com>
2023-08-08 20:57:54 +0400

    add parameter language name

--- admin/telegram_users/choices/languages.py
+++ admin/telegram_users/choices/languages.py
@@ -1,7 +1,20 @@
-from app.choices import AdminChoiceEnum
+from enum import Enum
 
 
-class Language(AdminChoiceEnum):
+class Language(Enum):
     """Languages."""
 
-    russian = ('ru', 'Русский язык')
+    russian = ('ru', 'Русский язык', 'russian')
+
+    def __init__(self, value, description, language_name) -> None:
+        self._value_ = value
+        self.description = description
+        self.language_name = language_name
+
+    @classmethod
+    def choices(cls):
+        return [(choice.value, choice.description) for choice in cls]
+
+    @classmethod
+    def get_info(cls):
+        return [(choice.value, choice.language_name) for choice in cls]
