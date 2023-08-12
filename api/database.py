c6e0d77c5fcc5efee1c4c1533d8c7708d0d4960a
psgaiduk <psgaiduk@gmail.com>
2023-08-12 11:44:11 +0400

    update database url

--- api/database.py
+++ api/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import MetaData
 from databases import Database
 from sqlalchemy.ext.declarative import declarative_base
-import settings
+from settings import settings
 
-DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = settings.postgres.database_url
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
