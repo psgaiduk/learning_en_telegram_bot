1d00e0833379e865cc823e44e83d6e083cfacb16
psgaiduk <psgaiduk@gmail.com>
2023-08-12 12:19:48 +0400

    create test endpoint

--- api/main.py
+++ api/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from books.models import BooksModel
+from database import get_db
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {'message': 'Welcome to FastAPI'}
+def read_root(db: Session = Depends(get_db)):
+    query = select(BooksModel)  # Создаем запрос на получение всех книг
+    result = db.execute(query)  # Выполняем запрос
+    books = result.fetchall()
+
+    print(books)
+
+    # Преобразуем результаты в список словарей
+    return {'message': 'Hello world!'}
