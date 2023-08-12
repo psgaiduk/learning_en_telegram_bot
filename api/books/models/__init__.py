04030252d3939327b4c40695d6bcc1e0e7d058f6
psgaiduk <psgaiduk@gmail.com>
2023-08-12 11:39:36 +0400

    create model books

---
+++ api/books/models/__init__.py
@@ -0,0 +1,1 @@
+from books.models.books import BooksModel
---
+++ api/books/models/books.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class BooksModel(Base):
+    """Model of books."""
+
+    __tablename__ = 'books'
+
+    book_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(128))
+    level_en_id = Column(ForeignKey('levels_en.level_en_id'))
+    author = Column(String(128))
+    #
+    # level_en = relationship('LevelsEn', back_populates='books', uselist=False)
+    # users_books_history = db.relationship('UsersBooksHistory', back_populates='book', uselist=False)
+    # books_sentences = db.relationship('BooksSentences', back_populates='book')
