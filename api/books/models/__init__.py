40febbac37d162d4f8a48332abcaa80f42e169bd
psgaiduk <psgaiduk@gmail.com>
2023-08-14 21:15:50 +0400

    add model type words

--- api/books/models/__init__.py
+++ api/books/models/__init__.py
@@ -1,2 +1,3 @@
 from books.models.books import BooksModel
 from books.models.books_sentences import BooksSentences
+from books.models.type_words import TypeWords
---
+++ api/books/models/type_words.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class TypeWords(Base):
+    """Model of type of words."""
+
+    __tablename__ = 'type_words'
+
+    type_word_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(128))
+
+    # words = db.relationship('Words', back_populates='type_word')
