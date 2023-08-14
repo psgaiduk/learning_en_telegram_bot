80b7e6c3d2ac0398cb02c74db81e2b962d121bbf
psgaiduk <psgaiduk@gmail.com>
2023-08-14 20:41:35 +0400

    add first router

--- api/main.py
+++ api/main.py
@@ -1,19 +1,8 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from sqlalchemy import select
+from fastapi import FastAPI
 
-from books.models import BooksModel
-from database import get_db
+from books.api import v1_books_router
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root(db: Session = Depends(get_db)):
-    query = select(BooksModel)  # Создаем запрос на получение всех книг
-    result = db.execute(query)  # Выполняем запрос
-    books = result.fetchall()
-
-    print(books)
-
-    # Преобразуем результаты в список словарей
-    return {'message': 'Hello world!'}
+app.include_router(v1_books_router)
