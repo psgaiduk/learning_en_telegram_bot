ac96269681d86af38567e00af092e60269fa0f7f
psgaiduk <psgaiduk@gmail.com>
2023-08-15 20:40:19 +0400

    update import model

--- api/books/models/__init__.py
+++ api/books/models/__init__.py
@@ -3,3 +3,4 @@ from books.models.books import BooksModel
 from books.models.books_sentences import BooksSentences
 from books.models.type_words import TypeWords
 from books.models.words import Words
+from telegram_users.models import LevelsEn
