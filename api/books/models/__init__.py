a27b30fdacfb582956b3fd7b54fb6d3cdb82455d
psgaiduk <psgaiduk@gmail.com>
2023-08-15 20:04:21 +0400

    add model words

--- api/books/models/__init__.py
+++ api/books/models/__init__.py
@@ -1,3 +1,4 @@
 from books.models.books import BooksModel
 from books.models.books_sentences import BooksSentences
 from books.models.type_words import TypeWords
+from books.models.words import Words
---
+++ api/books/models/words.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer, JSON, String
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Words(Base):
+    """Model of words."""
+
+    __tablename__ = 'words'
+
+    word_id = Column(Integer, primary_key=True, autoincrement=True)
+    type_word_id = Column(ForeignKey('type_words.type_word_id'))
+    word = Column(String(128))
+    translation = Column(JSON)
+
+    type_word = relationship('TypeWords', back_populates='words', uselist=False)
+    # users_words_history = db.relationship('UsersWordsHistory', back_populates='word')
