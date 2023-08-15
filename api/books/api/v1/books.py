893e1fa49a6445d7c98dfb1bfea68852d20a1d60
psgaiduk <psgaiduk@gmail.com>
2023-08-15 20:40:42 +0400

    add more info for api

--- api/books/api/v1/books.py
+++ api/books/api/v1/books.py
@@ -13,4 +13,12 @@ v1_books_router = APIRouter()
 @api_key_required
 async def get_random_book_for_user(request: Request, telegram_id: int, db: Session = Depends(get_db)):
     books = db.query(BooksModel).all()
+    for book in books:
+        sentences = book.books_sentences
+        for sentence in sentences:
+            print(sentence.text)
+            print(sentence.translation)
+            words = sentence.words
+            for word in words:
+                print(word.word)
     return [{"book_id": book.book_id, "title": book.title, "author": book.author} for book in books]
