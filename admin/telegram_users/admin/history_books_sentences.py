84f975e50d13e3cc050b59521f0c7f31a9542a4d
psgaiduk <psgaiduk@gmail.com>
2023-08-16 20:59:18 +0400

    create users history sentences books admin

---
+++ admin/telegram_users/admin/history_books_sentences.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from telegram_users.models import UsersBooksSentencesHistoryModel
+
+
+@admin.register(UsersBooksSentencesHistoryModel)
+class UsersBooksSentencesHistoryAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'telegram_user', 'is_read')
+    list_filter = ('sentence__book__level_en__title', 'is_read')
+    search_fields = ('telegram_user__telegram_id', 'sentence__book__title')
