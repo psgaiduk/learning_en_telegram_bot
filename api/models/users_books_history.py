d6ee6d99d770747542544a6eb195fdf8048c8ad3
psgaiduk <psgaiduk@gmail.com>
2023-08-16 21:01:59 +0400

    delete not used import

--- api/models/users_books_history.py
+++ api/models/users_books_history.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 
 from database import Base
