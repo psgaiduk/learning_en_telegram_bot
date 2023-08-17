83819e1deccd5882e6b35e61594caeba1e7825e8
psgaiduk <psgaiduk@gmail.com>
2023-08-17 20:34:10 +0400

    add admin for users history words

--- admin/telegram_users/admin/__init__.py
+++ admin/telegram_users/admin/__init__.py
@@ -1,6 +1,7 @@
 from telegram_users.admin.hero_levels import HeroLevelsAdmin
 from telegram_users.admin.history_books import UsersBooksHistoryAdmin
 from telegram_users.admin.history_books_sentences import UsersBooksSentencesHistoryAdmin
+from telegram_users.admin.history_words import UsersWordsHistoryAdmin
 from telegram_users.admin.history_hero_level import UsersHeroLevelsHistoryAdmin
 from telegram_users.admin.levels_en import LevelsEnAdmin
 from telegram_users.admin.main_languages import MainLanguagesAdmin
