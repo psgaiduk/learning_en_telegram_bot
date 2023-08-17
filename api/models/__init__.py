a488edc8a92ab23d418611242c93f1dda2baa886
psgaiduk <psgaiduk@gmail.com>
2023-08-17 20:39:17 +0400

    import history models

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -6,5 +6,9 @@ from models.hero_level import HeroLevels
 from models.main_language import MainLanguages
 from models.telegram_user import Users
 from models.type_words import TypeWords
+from models.users_books_history import UsersBooksHistory
+from models.users_books_sentences_history import UsersBooksSentencesHistory
+from models.users_hero_levels_history import UsersHeroLevelsHistory
+from models.users_words_history import UsersWordsHistory
 from models.words import Words
 
