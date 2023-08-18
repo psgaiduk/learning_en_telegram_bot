f5dec6238a69f0a55a538ceb9547040b357ad5dd
psgaiduk <psgaiduk@gmail.com>
2023-08-18 09:17:52 +0400

    update fields

--- api/dto/user.py
+++ api/dto/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -5,10 +7,10 @@ class TelegramUserDTO(BaseModel):
     """Telegram user DTO."""
 
     telegram_id: int
-    level_en_id: int
-    main_language_id: int
-    user_name: str
-    experience: int
-    hero_level_id: int
-    previous_stage: str
-    stage: str
+    level_en: Optional[str]
+    main_language: Optional[str]
+    user_name: Optional[str]
+    experience: int = 0
+    hero_level: Optional[int]
+    previous_stage: Optional[str]
+    stage: Optional[str]
