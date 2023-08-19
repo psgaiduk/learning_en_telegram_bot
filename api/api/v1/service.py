a6dd1e5f0671fe1bf51afd7530b182b07c823e81
psgaiduk <psgaiduk@gmail.com>
2023-08-19 18:54:26 +0300

    update work with levels en dto

--- api/api/v1/service.py
+++ api/api/v1/service.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
 from functions import api_key_required
-from models import LevelsEn
+from models import LevelsEn, LevelsEnDTO
 
 
 version_1_service_router = APIRouter(
@@ -14,12 +14,7 @@ version_1_service_router = APIRouter(
 
 
 @version_1_service_router.get('/english_levels/')
-async def create_user(db: Session = Depends(get_db)):
-    """
-    Get all english levels.
-
-    :param db: Session
-    :return:
-    """
+async def get_english_levels(db: Session = Depends(get_db)) -> list[LevelsEnDTO]:
+    """Get all english levels."""
     english_levels = db.query(LevelsEn).all()
-    return english_levels
+    return [LevelsEnDTO(**level.__dict__) for level in english_levels]
