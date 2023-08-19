0cbd0bf237a503d635c6d7a9f292379b18e25469
psgaiduk <psgaiduk@gmail.com>
2023-08-19 19:00:30 +0300

    add endpoint languages

--- api/api/v1/service.py
+++ api/api/v1/service.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from functions import api_key_required
-from models import LevelsEn, LevelsEnModelDTO
+from models import LevelsEn, LevelsEnModelDTO, MainLanguages, MainLanguagesModelDTO
 
 
 version_1_service_router = APIRouter(
@@ -18,4 +18,11 @@ async def get_english_levels(db: Session = Depends(get_db)) -> list[LevelsEnMode
     """Get all english levels."""
     english_levels = db.query(LevelsEn).all()
     return [LevelsEnModelDTO(**level.__dict__) for level in english_levels]
+
+
+@version_1_service_router.get('/languages/')
+async def get_languages(db: Session = Depends(get_db)) -> list[MainLanguagesModelDTO]:
+    """Get all languages."""
+    languages = db.query(MainLanguages).all()
+    return [MainLanguagesModelDTO(**level.__dict__) for level in languages]
 
