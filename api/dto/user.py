eede772e79d9d50946c9d9d83b2b3d973149222f
psgaiduk <psgaiduk@gmail.com>
2023-08-21 19:55:59 +0300

    add main_language

--- api/dto/user.py
+++ api/dto/user.py
@@ -2,15 +2,18 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+from dto import MainLanguageDTO
+
 
 class TelegramUserDTO(BaseModel):
     """Telegram user DTO."""
 
     telegram_id: int
-    level_en_id: str
-    main_language_id: str
+    level_en_id: int
+    main_language_id: int
     user_name: str = 'New client'
     experience: int = 0
     hero_level_id: int
     previous_stage: Optional[str]
     stage: Optional[str]
+    main_language: Optional[MainLanguageDTO]
