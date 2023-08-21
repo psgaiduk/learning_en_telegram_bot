b4277c30574387692258cc27a012de0d16a4da54
psgaiduk <psgaiduk@gmail.com>
2023-08-21 19:12:26 +0300

    create method for create telegram users

--- api/api/v1/telegram_users.py
+++ api/api/v1/telegram_users.py
@@ -12,3 +12,27 @@ version_1_telegram_user_router = APIRouter(
     tags=['Users'],
     dependencies=[Depends(api_key_required)]
 )
+
+
+@version_1_telegram_user_router.post('/', response_model=TelegramUserDTO)
+async def create_user(user: TelegramUserDTO, db: Session = Depends(get_db)):
+
+    new_user = Users(
+        telegram_id=user.telegram_id,
+        level_en_id=user.level_en_id,
+        main_language_id=user.main_language_id,
+        user_name=user.user_name,
+        experience=user.experience,
+        hero_level_id=user.hero_level_id,
+        previous_stage=user.previous_stage,
+        stage=user.stage
+    )
+    db.add(new_user)
+    try:
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    finally:
+        db.close()
+    return user
