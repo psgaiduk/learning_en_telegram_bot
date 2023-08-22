ab9214f47fd8ecb95295cb42f911748b42cf9956
psgaiduk <psgaiduk@gmail.com>
2023-08-22 19:28:11 +0300

    add dto for update telegram user

--- api/dto/user.py
+++ api/dto/user.py
@@ -17,3 +17,13 @@ class TelegramUserDTO(BaseModel):
     previous_stage: Optional[str]
     stage: Optional[str]
     main_language: Optional[MainLanguageDTO]
+
+
+class UpdateTelegramUserDTO(TelegramUserDTO):
+    """Update telegram user DTO for patch."""
+    telegram_id: Optional[int]
+    level_en_id: Optional[int]
+    main_language_id: Optional[int]
+    user_name: Optional[str]
+    experience: Optional[int]
+    hero_level_id: Optional[int]
