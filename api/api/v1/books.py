80bce3938c6965162b7c1e9c1c4c4525adf84db2
psgaiduk <psgaiduk@gmail.com>
2023-08-24 20:19:09 +0300

    create function for book dto

--- api/api/v1/books.py
+++ api/api/v1/books.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import and_, func
 from sqlalchemy.orm import Session, joinedload
 
 from database import get_db
 from dto.models import BooksModelDTO
 from functions import api_key_required
-from models import BooksModel, BooksSentences
+from models import BooksModel, BooksSentences, Users, UsersBooksHistory
 
 
 version_1_books_router = APIRouter(
@@ -13,6 +14,18 @@ version_1_books_router = APIRouter(
     dependencies=[Depends(api_key_required)],
     responses={status.HTTP_401_UNAUTHORIZED: {'description': 'Invalid API Key'}},
 )
+
+
+async def get_book_dto(book: BooksModel) -> BooksModelDTO:
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+
+    book_dict = book.__dict__
+    book_dict['books_sentences'] = [sentence.__dict__ for sentence in book.books_sentences]
+    for sentence in book_dict['books_sentences']:
+        sentence['words'] = [word.__dict__ for word in sentence['words']]
+
+    return BooksModelDTO(**book_dict)
 
 
 @version_1_books_router.get('/{book_id}/')
@@ -28,12 +41,7 @@ async def get_book_by_id(book_id: int, db: Session = Depends(get_db)):
     if not book:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
 
-    book_dict = book.__dict__
-    book_dict['books_sentences'] = [sentence.__dict__ for sentence in book.books_sentences]
-    for sentence in book_dict['books_sentences']:
-        sentence['words'] = [word.__dict__ for word in sentence['words']]
-
-    return BooksModelDTO(**book_dict)
+    return await get_book_dto(book)
 
 
 
