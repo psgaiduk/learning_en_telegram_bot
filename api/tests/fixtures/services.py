766e9b3caac5e6ec74957c9978ee4a28c5fe5e1a
psgaiduk <psgaiduk@gmail.com>
2023-08-27 09:41:05 +0300

    create type words mock

--- api/tests/fixtures/services.py
+++ api/tests/fixtures/services.py
@@ -1,7 +1,7 @@
 from pytest import fixture
 
 from database import get_db
-from models import LevelsEn
+from models import LevelsEn, TypeWords
 
 
 @fixture
@@ -13,6 +13,22 @@ def level_en_mock():
         {'id': 3, 'title': 'B1', 'description': 'Intermediate', 'order': 3},
         {'id': 4, 'title': 'B2', 'description': 'Upper Intermediate', 'order': 4},
         {'id': 5, 'title': 'C1', 'description': 'Advanced', 'order': 5},
+    ]
+
+    with get_db() as db:
+        for level_data in levels_data:
+            level = LevelsEn(**level_data)
+            db.add(level)
+        db.commit()
+
+
+@fixture
+def type_words_mock():
+
+    levels_data = [
+        {'type_word_id': 1, 'title': 'word'},
+        {'type_word_id': 2, 'title': 'phrase verb'},
+        {'type_word_id': 3, 'title': 'idiom'},
     ]
 
     with get_db() as db:
