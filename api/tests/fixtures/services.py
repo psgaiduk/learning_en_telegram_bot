b55758e7822afedc23baf929b7450e94c92dfa05
psgaiduk <psgaiduk@gmail.com>
2023-08-27 09:35:32 +0300

    refactoring level en mock

--- api/tests/fixtures/services.py
+++ api/tests/fixtures/services.py
@@ -6,48 +6,17 @@ from models import LevelsEn
 
 @fixture
 def level_en_mock():
-    with get_db() as db:
-        level_1 = LevelsEn(
-            id=1,
-            title='A1',
-            description='Beginner',
-            order=1,
-        )
-        level_2 = LevelsEn(
-            id=2,
-            title='A2',
-            description='Elementary',
-            order=2,
-        )
-        level_3 = LevelsEn(
-            id=3,
-            title='B1',
-            description='Intermediate',
-            order=3,
-        )
-        level_4 = LevelsEn(
-            id=4,
-            title='B2',
-            description='Upper Intermediate',
-            order=4,
-        )
-        level_5 = LevelsEn(
-            id=5,
-            title='C1',
-            description='Advanced',
-            order=5,
-        )
-        level_6 = LevelsEn(
-            id=6,
-            title='C2',
-            description='Proficiency',
-            order=6,
-        )
 
-        db.add(level_1)
-        db.add(level_2)
-        db.add(level_3)
-        db.add(level_4)
-        db.add(level_5)
-        db.add(level_6)
+    levels_data = [
+        {'id': 1, 'title': 'A1', 'description': 'Beginner', 'order': 1},
+        {'id': 2, 'title': 'A2', 'description': 'Elementary', 'order': 2},
+        {'id': 3, 'title': 'B1', 'description': 'Intermediate', 'order': 3},
+        {'id': 4, 'title': 'B2', 'description': 'Upper Intermediate', 'order': 4},
+        {'id': 5, 'title': 'C1', 'description': 'Advanced', 'order': 5},
+    ]
+
+    with get_db() as db:
+        for level_data in levels_data:
+            level = LevelsEn(**level_data)
+            db.add(level)
         db.commit()
