8a147c85b1eaef1e5f0c5181b7f81a52b78068dc
psgaiduk <psgaiduk@gmail.com>
2023-08-27 09:37:46 +0300

    update books fixture

--- api/tests/fixtures/book.py
+++ api/tests/fixtures/book.py
@@ -1,35 +1,30 @@
 from pytest import fixture
 
 from database import get_db
-from models import BooksModel
+from models import BooksModel, LevelsEn
+from .services import level_en_mock
 
 
 @fixture
-def book_mock():
+def book_mock(level_en_mock):
     with get_db() as db:
-        book_first = BooksModel(
-            title='First Book',
-            level_en_id=1,
-            author='First Author',
-            text='Reading practice to help you understand simple information, words and sentences about known topics.'
-                 ' Texts include posters, messages, forms and timetables.',
-        )
-        book_second = BooksModel(
-            title='Second Book',
-            level_en_id=1,
-            author='Second Author',
-            text='Reading practice to help you understand simple information, words and sentences about known topics.'
-                 ' Texts include posters, messages, forms and timetables.',
-        )
-        book_third = BooksModel(
-            title='Third Book',
-            level_en_id=1,
-            author='Third Author',
-            text='Reading practice to help you understand simple information, words and sentences about known topics.'
-                 ' Texts include posters, messages, forms and timetables.',
-        )
+        levels = db.query(LevelsEn).all()
 
-        db.add(book_first)
-        db.add(book_second)
-        db.add(book_third)
-        db.commit()
+        for level_en in levels:
+            book_texts = [
+                (
+                    'Reading practice to help you understand simple information, words and sentences about ',
+                    'known topics. Texts include posters, messages, forms and timetables.'
+                ),
+            ]
+
+            books_data = [
+                {'title': 'First Book', 'level_en_id': level_en.id, 'author': 'First Author', 'text': book_texts[0]},
+                {'title': 'Second Book', 'level_en_id': level_en.id, 'author': 'Second Author', 'text': book_texts[0]},
+                {'title': 'Third Book', 'level_en_id': level_en.id, 'author': 'Third Author', 'text': book_texts[0]},
+            ]
+
+            for book_data in books_data:
+                book = BooksModel(**book_data)
+                db.add(book)
+            db.commit()
