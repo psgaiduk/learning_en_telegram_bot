43d26dc10535c7c30c97eca93fcbd2abdd53fdb0
psgaiduk <psgaiduk@gmail.com>
2023-08-28 17:28:15 +0300

    create fixtures for sentences

--- api/tests/fixtures/test_fixtures_book.py
+++ api/tests/fixtures/test_fixtures_book.py
@@ -1,7 +1,7 @@
 from pytest import fixture
 
 from tests.connect_db import db_session
-from models import BooksModel, LevelsEn
+from models import BooksModel, LevelsEn, BooksSentences
 from tests.fixtures.test_fixtures_services import level_en_mock
 
 
@@ -30,3 +30,35 @@ def book_mock(level_en_mock):
                 book = BooksModel(**book_data)
                 db.add(book)
             db.commit()
+
+
+@fixture()
+def book_sentences_mock():
+    with db_session() as db:
+
+        levels = db.query(LevelsEn).all()
+
+        sentence_id = 0
+
+        for level_en in levels:
+
+            books_in_level = db.query(BooksModel).filter(BooksModel.level_en_id == level_en.id).all()
+
+            for book in books_in_level:
+
+                sentences_data = [
+                    {'sentence_id': sentence_id + 1, 'book_id': book.book_id, 'order': 1, 'text': 'First sentence',
+                     'translation': {'ru': 'Первое предложение'}},
+                    {'sentence_id': sentence_id + 2, 'book_id': book.book_id, 'order': 2, 'text': 'Second sentence',
+                     'translation': {'ru': 'Второе предложение'}},
+                    {'sentence_id': sentence_id + 3, 'book_id': book.book_id, 'order': 3, 'text': 'Third sentence',
+                     'translation': {'ru': 'Третье предложение'}},
+                ]
+
+                sentence_id += 3
+
+                for sentence_data in sentences_data:
+                    sentence = BooksSentences(**sentence_data)
+                    db.add(sentence)
+
+        db.commit()
