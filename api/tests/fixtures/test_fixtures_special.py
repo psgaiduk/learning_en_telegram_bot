2ac700121a385d1117fc6dd3dbfb1b0fca9c7be7
psgaiduk <psgaiduk@gmail.com>
2023-08-28 16:50:43 +0300

    create fuxture for create test database

---
+++ api/tests/fixtures/test_fixtures_special.py
@@ -0,0 +1,10 @@
+from pytest import fixture
+
+from database import engine, Base
+
+
+@fixture(scope='function', autouse=True)
+def create_test_database():
+    """Delete and Create test database."""
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
