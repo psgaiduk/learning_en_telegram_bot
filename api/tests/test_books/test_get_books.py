26453de0c6dbb60026d9aacaa7365e5fa292ae2c
psgaiduk <psgaiduk@gmail.com>
2023-08-30 19:26:41 +0300

    add test words and texts

--- api/tests/test_books/test_get_books.py
+++ api/tests/test_books/test_get_books.py
@@ -4,8 +4,8 @@ from fastapi import status
 import pytest
 
 from tests.connect_db import db_session
-from tests.fixtures import book_mock, create_test_database, level_en_mock, book_sentences_mock
-from models import BooksModel
+from tests.fixtures import book_mock, create_test_database, level_en_mock, book_sentences_mock, words_mock, type_words_mock
+from models import BooksModel, BooksSentences, Words, sentence_word_association
 from settings import settings
 
 
@@ -16,18 +16,35 @@ class TestBookAPI:
         cls._headers = {'X-API-Key': settings.api_key}
         cls._client = TestClient(app)
 
-    def test_good_get_book_by_id(self, create_test_database, book_mock, book_sentences_mock):
+    def test_good_get_book_by_id(self, create_test_database, book_mock, book_sentences_mock, words_mock):
         with db_session() as db:
-            first_book = db.query(BooksModel).first()
+            first_book = (
+                db.query(BooksModel)
+                .join(BooksSentences, BooksModel.book_id == BooksSentences.book_id)
+                .join(sentence_word_association)
+                .join(Words)
+                .order_by(BooksModel.book_id, BooksSentences.order)
+                .first()
+            )
 
             if not first_book:
                 pytest.fail('Test data is not initialized properly.')
 
             response = self._client.get(f'/api/v1/books/{first_book.book_id}/', headers=self._headers)
             assert response.status_code == status.HTTP_200_OK
-            assert response.json()['title'] == first_book.title
-            assert response.json()['author'] == first_book.author
-            assert len(response.json()['books_sentences']) == len(first_book.books_sentences)
+
+            response = response.json()
+            assert response['title'] == first_book.title
+            assert response['author'] == first_book.author
+
+            book_first_sentence = response['books_sentences'][0]
+            assert len(response['books_sentences']) == len(first_book.books_sentences)
+            assert book_first_sentence['text'] == first_book.books_sentences[0].text
+            assert book_first_sentence['translation'] == first_book.books_sentences[0].translation
+
+            assert len(book_first_sentence['words']) == len(first_book.books_sentences[0].words)
+            assert book_first_sentence['words'][0]['word'] == first_book.books_sentences[0].words[0].word
+            assert book_first_sentence['words'][0]['translation'] == first_book.books_sentences[0].words[0].translation
 
     def test_not_found_get_book_by_id(self, create_test_database, book_mock):
         with db_session() as db:
