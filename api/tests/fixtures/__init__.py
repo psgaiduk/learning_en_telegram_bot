780ecea43276582a5353e668dc4e21e0587093bc
psgaiduk <psgaiduk@gmail.com>
2023-08-30 19:50:39 +0300

    add fixture telegram users

--- api/tests/fixtures/__init__.py
+++ api/tests/fixtures/__init__.py
@@ -1,3 +1,4 @@
 from tests.fixtures.test_fixtures_book import book_mock, book_sentences_mock, words_mock
 from tests.fixtures.test_fixtures_services import hero_level_mock, level_en_mock, type_words_mock, main_language_mock
 from tests.fixtures.test_fixtures_special import create_test_database
+from tests.fixtures.telegram_users_fixture import telegram_users_mock
---
+++ api/tests/fixtures/telegram_users_fixture.py
@@ -0,0 +1,35 @@
+from faker import Faker
+from pytest import fixture
+
+from tests.connect_db import db_session
+from models import LevelsEn, MainLanguages
+from tests.fixtures.test_fixtures_services import hero_level_mock, level_en_mock, main_language_mock
+
+
+fake = Faker()
+fake_ru = Faker('ru_RU')
+
+
+@fixture
+def telegram_users_mock(level_en_mock, main_language_mock, hero_level_mock):
+    with db_session() as db:
+        levels = db.query(LevelsEn).all()
+        main_languages = db.query(MainLanguages).all()
+
+        for level_en in levels:
+            for language in main_languages:
+
+                telegram_user = {
+                    'telegram_id': fake.random_int(min=1, max=100000000),
+                    'level_en_id': level_en.id,
+                    'main_language_id': language.id,
+                    'user_name': fake.name(),
+                    'experience': fake.random_int(min=1, max=100000000),
+                    'hero_level_id': 1,
+                    'previous_stage': '',
+                    'stage': '',
+                }
+
+                db.add(**telegram_user)
+
+        db.commit()
