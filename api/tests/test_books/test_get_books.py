84c5e2a1bcba84cf15566736f15a630cf092895c
psgaiduk <psgaiduk@gmail.com>
2023-08-30 20:07:30 +0300

    add tests for get random books

--- api/tests/test_books/test_get_books.py
+++ api/tests/test_books/test_get_books.py
@@ -1,11 +1,23 @@
+from datetime import datetime
+
 from fastapi.testclient import TestClient
 from main import app
 from fastapi import status
 import pytest
 
 from tests.connect_db import db_session
-from tests.fixtures import book_mock, create_test_database, level_en_mock, book_sentences_mock, words_mock, type_words_mock
-from models import BooksModel, BooksSentences, Words, sentence_word_association
+from tests.fixtures import (
+    create_test_database,
+    book_mock,
+    book_sentences_mock,
+    words_mock,
+    telegram_users_mock, 
+    level_en_mock,
+    hero_level_mock,
+    main_language_mock,
+    type_words_mock,
+)
+from models import BooksModel, BooksSentences, Words, sentence_word_association, Users, UsersBooksHistory
 from settings import settings
 
 
@@ -55,3 +67,36 @@ class TestBookAPI:
 
             response = self._client.get(f'/api/v1/books/{last_book.book_id + 1}/', headers=self._headers)
             assert response.status_code == 404
+    
+    def test_good_get_random_book_for_user(self, create_test_database, book_mock, telegram_users_mock):
+        with db_session() as db:
+            user = db.query(Users).first()
+
+            if not user:
+                pytest.fail('Test data is not initialized properly.')
+
+            response = self._client.get(f'/api/v1/books/get-random-book/{user.telegram_id}/', headers=self._headers)
+            assert response.status_code == status.HTTP_200_OK
+
+            response = response.json()
+            assert response['level_en_id'] == user.level_en_id
+
+    def test_not_found_get_random_book_for_user(self, create_test_database, book_mock, telegram_users_mock):
+        with db_session() as db:
+            user = db.query(Users).first()
+            all_books_in_this_levels = db.query(BooksModel).filter(BooksModel.level_en_id == user.level_en_id).all()
+
+            for book in all_books_in_this_levels:
+                user_history = UsersBooksHistory(
+                    telegram_user_id=user.telegram_id,
+                    book_id=book.book_id,
+                    start_read=datetime.utcnow(),
+                    end_read=datetime.utcnow(),
+                )
+
+                db.add(user_history)
+
+            db.commit()
+
+            response = self._client.get(f'/api/v1/books/get-random-book/{user.telegram_id}/', headers=self._headers)
+            assert response.status_code == status.HTTP_404_NOT_FOUND
