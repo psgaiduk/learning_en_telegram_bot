4ce5b1bbf687de7df62616440a24346cdf7daaf7
psgaiduk <psgaiduk@gmail.com>
2023-09-03 11:26:47 +0300

    create fixture history book not complete

---
+++ api/tests/fixtures/history_fixture.py
@@ -0,0 +1,26 @@
+from faker import Faker
+from pytest import fixture
+
+from tests.connect_db import db_session
+from models import BooksModel, Users, UsersBooksHistory
+from tests.fixtures.test_fixtures_book import book_mock
+from tests.fixtures.telegram_users_fixture import telegram_users_mock
+
+
+fake = Faker()
+fake_ru = Faker('ru_RU')
+
+
+@fixture
+def history_book_not_complete_mock(book_mock, telegram_users_mock):
+    with db_session() as db:
+        book = db.query(BooksModel).first()
+        telegram_user = db.query(Users).first()
+
+        history_book = UsersBooksHistory(
+            telegram_user_id=telegram_user.telegram_id,
+            book_id=book.book_id,
+        )
+
+        db.add(history_book)
+        db.commit()
