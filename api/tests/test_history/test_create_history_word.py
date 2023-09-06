2bc9f8097ce4bdbfb305e5638a5efdf1135b6765
psgaiduk <psgaiduk@gmail.com>
2023-09-06 19:23:56 +0300

    complete tests for create word history

--- api/tests/test_history/test_create_history_word.py
+++ api/tests/test_history/test_create_history_word.py
@@ -55,3 +55,41 @@ class TestCreateHistoryWordAPI:
             assert words_history.correct_answers_in_row == response['correct_answers_in_row'] == 0
             assert words_history.created_at == datetime.strptime(response['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             assert words_history.updated_at == datetime.strptime(response['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
+    def test_not_create_history_word_without_api_key(self):
+        with db_session() as db:
+            word = db.query(Words).first()
+            telegram_user = db.query(Users).first()
+
+        url = f'{self._url}/{telegram_user.telegram_id}/{word.word_id}/'
+        response = self._client.post(url=url)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_not_create_history_word_with_wrong_api_key(self):
+        with db_session() as db:
+            word = db.query(Words).first()
+            telegram_user = db.query(Users).first()
+
+        url = f'{self._url}/{telegram_user.telegram_id}/{word.word_id}/'
+        response = self._client.post(url=url, headers={'X-API-Key': 'wrong_api_key'})
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_not_create_history_word_with_wrong_book_id(self):
+        with db_session() as db:
+            word = db.query(Words).order_by(Words.word_id.desc()).first()
+            word_id = word.word_id + 1
+            telegram_user = db.query(Users).first()
+
+        url = f'{self._url}/{telegram_user.telegram_id}/{word_id}/'
+        response = self._client.post(url=url, headers=self._headers)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        
+    def test_not_create_history_word_with_wrong_user_id(self):
+        with db_session() as db:
+            word = db.query(Words).first()
+            telegram_user = db.query(Users).order_by(Users.telegram_id).first()
+            telegram_user_id = telegram_user.telegram_id - 1
+
+        url = f'{self._url}/{telegram_user_id}/{word.word_id}/'
+        response = self._client.post(url=url, headers=self._headers)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
