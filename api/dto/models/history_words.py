842969b7c9509464e40e43374123515b5ede001f
psgaiduk <psgaiduk@gmail.com>
2023-09-07 19:08:58 +0300

    update model dto for words history

--- api/dto/models/history_words.py
+++ api/dto/models/history_words.py
@@ -8,11 +8,14 @@ class HistoryWordModelDTO(BaseModel):
 
     id: int
     telegram_user_id: int
+    type_word_id: int
     word_id: int
+    word: str
     is_known: bool
     count_view: int
     correct_answers: int
     incorrect_answers: int
     correct_answers_in_row: int
+    translation: dict
     created_at: datetime
     updated_at: datetime
