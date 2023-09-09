b71b29a8feff2d9f47d04b328f8c50275b6a8de9
psgaiduk <psgaiduk@gmail.com>
2023-09-09 09:31:34 +0300

    create history word mock

--- api/tests/fixtures/history_fixture.py
+++ api/tests/fixtures/history_fixture.py
@@ -4,8 +4,8 @@ from faker import Faker
 from pytest import fixture
 
 from tests.connect_db import db_session
-from models import BooksModel, Users, UsersBooksHistory
-from tests.fixtures.test_fixtures_book import book_mock
+from models import BooksModel, Users, UsersBooksHistory, UsersWordsHistory, Words
+from tests.fixtures.test_fixtures_book import book_mock, words_mock
 from tests.fixtures.telegram_users_fixture import telegram_users_mock
 
 
@@ -39,6 +39,28 @@ def history_book_complete_mock(book_mock, telegram_users_mock):
             book_id=book.book_id,
             start_read=datetime.utcnow() - timedelta(days=3),
             end_read=datetime.utcnow(),
+        )
+
+        db.add(history_book)
+        db.commit()
+
+
+@fixture
+def history_word_mock(words_mock, telegram_users_mock):
+    with db_session() as db:
+        word = db.query(Words).first()
+        telegram_user = db.query(Users).first()
+
+        history_book = UsersWordsHistory(
+            telegram_user_id=telegram_user.telegram_id,
+            word_id=word.word_id,
+            is_known=False,
+            count_view=0,
+            correct_answers=0,
+            incorrect_answers=0,
+            correct_answers_in_row=0,
+            created_at=datetime.utcnow(),
+            updated_at=datetime.utcnow(),
         )
 
         db.add(history_book)
