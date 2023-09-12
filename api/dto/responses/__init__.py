28c379745cb256248bbd54b86249d548415082c4
psgaiduk <psgaiduk@gmail.com>
2023-09-12 19:30:34 +0300

    create response dto OneResponseDto

--- api/dto/responses/__init__.py
+++ api/dto/responses/__init__.py
@@ -1,1 +1,2 @@
+from dto.responses.one_result import OneResponseDTO
 from dto.responses.pagination_result import PaginatedResponseDTO
---
+++ api/dto/responses/one_result.py
@@ -0,0 +1,10 @@
+from pydantic.generics import GenericModel
+from typing import TypeVar, Generic
+
+DataT = TypeVar('DataT')
+
+
+class OneResponseDTO(GenericModel, Generic[DataT]):
+    """One response DTO."""
+
+    detail: list[DataT]
