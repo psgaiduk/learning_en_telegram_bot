d6d622f7306a153115b177cf18f51940dbb035a3
psgaiduk <psgaiduk@gmail.com>
2023-09-17 19:32:55 +0400

    delete not used code

--- bot/settings.py
+++ bot/settings.py
@@ -12,28 +12,17 @@ class PostgresSettings(BaseSettings):
     password: str = Field(..., env='POSTGRES_PASSWORD')
     db_name: str = Field(..., env='POSTGRES_DB')
 
-    class Config:
-        """Config."""
-
-        env_file = path.join(path.dirname(__file__), '../.env')
-
 
 class MainSettings(BaseSettings):
     """All settings."""
 
     environment: str = Field(..., env='ENVIRONMENT')
     openai_token: str = Field(..., env='OPEN_AI_TOKEN')
-    path_to_database: str = path.join(path.dirname(__file__), 'database.db')
     telegram_token: str = Field(..., env='TELEGRAM_BOT_TOKEN')
     nlp_token: str = Field(..., env='NLP_TOKEN')
     translate_word_token: str = Field(..., env='TRANSLATE_WORD_TOKEN')
 
     postgres: PostgresSettings = PostgresSettings()
-
-    class Config:
-        """Config."""
-
-        env_file = path.join(path.dirname(__file__), '../.env')
 
 
 @lru_cache()
