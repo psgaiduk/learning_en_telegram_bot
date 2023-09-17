f3dbe96f3bdcfc9999e4736585d54c5061ef324e
psgaiduk <psgaiduk@gmail.com>
2023-09-17 19:35:36 +0400

    add for test

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,9 @@
 from aiogram.utils import executor
 
 from telegram_bot_app.core import dispatcher
+from telegram_bot_app.commands import *
 
 
 if __name__ == '__main__':
+    print('start work')
     executor.start_polling(dispatcher, skip_updates=True)
