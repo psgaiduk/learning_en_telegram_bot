bc5f0fa4442e27ddc949b7e58a8c310ed17b336c
psgaiduk <psgaiduk@gmail.com>
2023-09-19 20:22:09 +0400

    create admin user referrals

---
+++ admin/telegram_users/admin/referrals.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from telegram_users.models import UserReferralsModel
+
+
+@admin.register(UserReferralsModel)
+class UserReferralsAdmin(admin.ModelAdmin):
+    """User referrals admin."""
+
+    list_display = ('__str__', )
+    search_fields = ('telegram_id__telegram_id', )
