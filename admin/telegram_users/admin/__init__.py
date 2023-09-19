2fcb2205e9e7b51eeaab6aeb570bd66d3fb47c7e
psgaiduk <psgaiduk@gmail.com>
2023-09-19 20:17:09 +0400

    create model user referrals

--- admin/telegram_users/admin/__init__.py
+++ admin/telegram_users/admin/__init__.py
@@ -5,4 +5,5 @@ from telegram_users.admin.history_words import UsersWordsHistoryAdmin
 from telegram_users.admin.history_hero_level import UsersHeroLevelsHistoryAdmin
 from telegram_users.admin.levels_en import LevelsEnAdmin
 from telegram_users.admin.main_languages import MainLanguagesAdmin
+from telegram_users.admin.referrals import UserReferralsAdmin
 from telegram_users.admin.telegram_users import TelegramUsersAdmin
---
+++ admin/telegram_users/models/referrals.py
@@ -0,0 +1,24 @@
+from django.db.models import CASCADE, SET_NULL, ForeignKey, Model
+
+from telegram_users.models.telegram_users import TelegramUsersModel
+
+
+class UserReferralsModel(Model):
+    """Users referrals model."""
+
+    telegram_id = ForeignKey(TelegramUsersModel, on_delete=CASCADE)
+    friend_telegram_id = ForeignKey(
+        TelegramUsersModel,
+        on_delete=SET_NULL,
+        null=True,
+        blank=True,
+        related_name='friend_telegram_id',
+    )
+
+    def __str__(self) -> str:
+        return f'{self.telegram_id.user_name} - пригласил {self.friend_telegram_id.user_name}'
+
+    class Meta:
+        verbose_name = 'User referrals'
+        verbose_name_plural = 'Users referrals'
+        db_table = 'users_referrals'
