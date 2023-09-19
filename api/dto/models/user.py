fc2abdf785b44d9ac3979ebd8e1add4de6d1660c
psgaiduk <psgaiduk@gmail.com>
2023-09-19 21:06:20 +0400

    move update telegram users dto

--- api/dto/models/user.py
+++ api/dto/models/user.py
@@ -19,13 +19,3 @@ class TelegramUserDTO(BaseModel):
     main_language: Optional[MainLanguageDTO] = None
     level_en: Optional[LevelsEnModelDTO] = None
     hero_level: Optional[HeroLevelsModelDTO] = None
-
-
-class UpdateTelegramUserDTO(TelegramUserDTO):
-    """Update telegram user DTO for patch."""
-    telegram_id: Optional[int]
-    level_en_id: Optional[int]
-    main_language_id: Optional[int]
-    user_name: Optional[str]
-    experience: Optional[int]
-    hero_level_id: Optional[int]
