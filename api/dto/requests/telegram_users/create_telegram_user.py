7ad241650ff95bda52b349dedd0f3b2d44fd7e01
psgaiduk <psgaiduk@gmail.com>
2023-09-19 20:56:49 +0400

    update hero level

--- api/dto/requests/telegram_users/create_telegram_user.py
+++ api/dto/requests/telegram_users/create_telegram_user.py
@@ -11,6 +11,6 @@ class CreateTelegramUserDTO(BaseModel):
     main_language_id: int
     user_name: str = 'New client'
     experience: int = 0
-    hero_level_id: int = 0
+    hero_level_id: int
     previous_stage: Optional[str] = None
     stage: Optional[str] = None
