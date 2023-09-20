d206f6f5478d95b6c69c0430d48649371320b1fd
psgaiduk <psgaiduk@gmail.com>
2023-09-20 20:45:00 +0400

    delete not used code

--- bot/settings.py
+++ bot/settings.py
@@ -1,28 +1,13 @@
 from functools import lru_cache
-from os import path
 
 from pydantic import BaseSettings, Field
-
-
-class PostgresSettings(BaseSettings):
-    """Settings for postgres."""
-
-    host: str = Field(..., env='POSTGRES_HOST')
-    user: str = Field(..., env='POSTGRES_USER')
-    password: str = Field(..., env='POSTGRES_PASSWORD')
-    db_name: str = Field(..., env='POSTGRES_DB')
 
 
 class MainSettings(BaseSettings):
     """All settings."""
 
     environment: str = Field(..., env='ENVIRONMENT')
-    openai_token: str = Field(..., env='OPEN_AI_TOKEN')
     telegram_token: str = Field(..., env='TELEGRAM_BOT_TOKEN')
-    nlp_token: str = Field(..., env='NLP_TOKEN')
-    translate_word_token: str = Field(..., env='TRANSLATE_WORD_TOKEN')
-
-    postgres: PostgresSettings = PostgresSettings()
 
 
 @lru_cache()
