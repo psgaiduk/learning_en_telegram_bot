5a58cd61c96ac240f0a0cf8de1fcd800a682ea3a
psgaiduk <psgaiduk@gmail.com>
2023-09-23 11:33:01 +0400

    add not create referral if again telegram friend user

--- api/tests/test_referrals/test_create_referral.py
+++ api/tests/test_referrals/test_create_referral.py
@@ -30,16 +30,13 @@ class TestCreateReferralAPI:
             'telegram_user_id': first_telegram_user.telegram_id,
             'friend_telegram_id': second_telegram_user.telegram_id,
         }
-        print(data_for_create_referral)
 
         response = self._client.post(url=self._url, headers=self._headers, json=data_for_create_referral)
         assert response.status_code == status.HTTP_201_CREATED
-        # response = response.json()
-        # assert response['detail']['telegram_id'] == first_telegram_user.telegram_id
-        # print(response['detail']['telegram_id'])
-        # print(response['detail']['friends'])
-        # assert len(response['detail']['friends']) == 1
-        # assert response['detail']['friends'] == [second_telegram_user.telegram_id]
+        response = response.json()
+        assert response['detail']['telegram_id'] == first_telegram_user.telegram_id
+        assert len(response['detail']['friends']) == 1
+        assert response['detail']['friends'] == [second_telegram_user.telegram_id]
 
         data_for_create_referral = {
             'telegram_user_id': first_telegram_user.telegram_id,
@@ -51,6 +48,39 @@ class TestCreateReferralAPI:
         response = response.json()
         assert response['detail']['telegram_id'] == first_telegram_user.telegram_id
         assert len(response['detail']['friends']) == 2
-        print(response['detail']['telegram_id'])
-        print(response['detail']['friends'])
         assert response['detail']['friends'] == [second_telegram_user.telegram_id, third_telegram_user.telegram_id]
+
+    def test_not_create_again_referral_user(self):
+        with db_session() as db:
+            telegram_users = db.query(Users).all()
+            first_telegram_user = telegram_users[0]
+            second_telegram_user = telegram_users[1]
+            third_telegram_user = telegram_users[2]
+
+        data_for_create_referral = {
+            'telegram_user_id': first_telegram_user.telegram_id,
+            'friend_telegram_id': second_telegram_user.telegram_id,
+        }
+
+        response = self._client.post(url=self._url, headers=self._headers, json=data_for_create_referral)
+        assert response.status_code == status.HTTP_201_CREATED
+        response = response.json()
+        assert response['detail']['telegram_id'] == first_telegram_user.telegram_id
+        assert len(response['detail']['friends']) == 1
+        assert response['detail']['friends'] == [second_telegram_user.telegram_id]
+
+        data_for_create_referral = {
+            'telegram_user_id': first_telegram_user.telegram_id,
+            'friend_telegram_id': second_telegram_user.telegram_id,
+        }
+
+        response = self._client.post(url=self._url, headers=self._headers, json=data_for_create_referral)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+        data_for_create_referral = {
+            'telegram_user_id': third_telegram_user.telegram_id,
+            'friend_telegram_id': second_telegram_user.telegram_id,
+        }
+
+        response = self._client.post(url=self._url, headers=self._headers, json=data_for_create_referral)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
