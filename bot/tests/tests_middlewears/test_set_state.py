91590f81bbb5068eeb33fa7f5b93598132ec6f13
psgaiduk <psgaiduk@gmail.com>
2023-10-01 22:58:57 +0700

    refactoring

--- bot/tests/tests_middlewears/test_set_state.py
+++ bot/tests/tests_middlewears/test_set_state.py
@@ -13,18 +13,24 @@ from settings import settings
 class TestSetStateMiddleware:
     """Tests for SetStateMiddleware."""
 
+    @classmethod
+    def setup_class(cls):
+        cls._chat = 12345
+        cls._user = 12345
+        cls._message = Mock()
+        cls._get_method_target = 'context_managers.aio_http_client.AsyncHttpClient.get'
+
     @pytest.mark.asyncio
     async def test_get_state_registration(self, mocker):
-        chat = 12345
-        user = 12345
-        message = Mock(from_user=Mock(id=user), chat=Mock(id=chat), spec=types.Message)
+
+        message = Mock(from_user=Mock(id=self._user), chat=Mock(id=self._chat), spec=types.Message)
         storage_mock = Mock(spec=BaseStorage)
         dispatcher_mock = Mock(storage=storage_mock)
         service = SetStateMiddleware(dispatcher_mock)
         response_mock = mocker.AsyncMock(spec=ClientResponse)
         response_mock.status = HTTPStatus.NOT_FOUND
 
-        storage_mock.check_address.return_value = (chat, user)
+        storage_mock.check_address.return_value = (self._chat, self._user)
 
         fsm_context_mock = mocker.Mock(spec=FSMContext)
 
@@ -32,48 +38,44 @@ class TestSetStateMiddleware:
 
         fsm_context_constructor_mock = mocker.patch('middlewears.set_state.FSMContext', return_value=fsm_context_mock)
 
-        path_to_context_manager = 'context_managers.aio_http_client.AsyncHttpClient.get'
-        with patch(path_to_context_manager, return_value=response_mock) as mocked_get:
+        with patch(self._get_method_target, return_value=response_mock) as mocked_get:
             await service.on_pre_process_message(message=message, data={})
 
         mocked_get.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{chat}',
+            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
             headers=settings.api_headers,
         )
 
         fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_once_with(storage=storage_mock, chat=chat, user=user)
+        fsm_context_constructor_mock.assert_called_once_with(storage=storage_mock, chat=self._chat, user=self._user)
         expected_state = 'REGISTRATION'
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
     @pytest.mark.asyncio
     async def test_get_state_from_user(self, mocker):
-        chat = 12345
-        user = 12345
-        message = Mock(from_user=Mock(id=user), chat=Mock(id=chat), spec=types.Message)
+        message = Mock(from_user=Mock(id=self._user), chat=Mock(id=self._chat), spec=types.Message)
         storage_mock = Mock(spec=BaseStorage)
         dispatcher_mock = Mock(storage=storage_mock)
         service = SetStateMiddleware(dispatcher_mock)
         response_mock = mocker.AsyncMock(spec=ClientResponse)
         response_mock.status = HTTPStatus.OK
         response_mock.json = mocker.AsyncMock(return_value={'detail': {'stage': 'WAIT_NAME'}})
-        storage_mock.check_address.return_value = (chat, user)
+        storage_mock.check_address.return_value = (self._chat, self._user)
 
         fsm_context_mock = mocker.Mock(spec=FSMContext)
         fsm_context_mock.set_state = mocker.AsyncMock()
         fsm_context_constructor_mock = mocker.patch('middlewears.set_state.FSMContext', return_value=fsm_context_mock)
 
-        path_to_context_manager = 'context_managers.aio_http_client.AsyncHttpClient.get'
-        with patch(path_to_context_manager, return_value=response_mock) as mocked_get:
+        with patch(self._get_method_target, return_value=response_mock) as mocked_get:
             await service.on_pre_process_message(message=message, data={})
 
         mocked_get.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{chat}',
+            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
             headers=settings.api_headers,
         )
 
         fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_once_with(storage=storage_mock, chat=chat, user=user)
+        fsm_context_constructor_mock.assert_called_once_with(storage=storage_mock, chat=self._chat, user=self._user)
         expected_state = 'WAIT_NAME'
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
