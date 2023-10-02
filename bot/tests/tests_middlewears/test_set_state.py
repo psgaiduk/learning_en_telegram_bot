101590a37697227f9d8dc81588086e432e19c4b9
psgaiduk <psgaiduk@gmail.com>
2023-10-02 23:37:46 +0700

    add tests not get update profile

--- bot/tests/tests_middlewears/test_set_state.py
+++ bot/tests/tests_middlewears/test_set_state.py
@@ -51,7 +51,7 @@ class TestSetStateMiddleware:
     @pytest.mark.asyncio
     @pytest.mark.parametrize(
         'stage',
-        [('WAIT_NAME',), ('WAIT_EN_LEVEL',), ('READ_BOOK',), ('CHECK_WORDS',), ('GRAMMAR',), ('UPDATE_PROFILE',)],
+        ['WAIT_NAME', 'WAIT_EN_LEVEL', 'READ_BOOK', 'CHECK_WORDS', 'GRAMMAR', 'UPDATE_PROFILE'],
     )
     async def test_get_state_from_user(self, mocker, stage):
         message = Mock(from_user=Mock(id=self._user), chat=Mock(id=self._chat), spec=types.Message)
@@ -78,8 +78,9 @@ class TestSetStateMiddleware:
         expected_state = stage
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
+
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('stage', [('WAIT_NAME',), ('WAIT_EN_LEVEL',), ('READ_BOOK',)])
+    @pytest.mark.parametrize('stage', ['WAIT_NAME', 'WAIT_EN_LEVEL', 'READ_BOOK'])
     async def test_get_state_update_profile(self, mocker, stage):
         message = Mock(from_user=Mock(id=self._user), chat=Mock(id=self._chat), text='/profile', spec=types.Message)
         response_mock = mocker.AsyncMock(spec=ClientResponse)
@@ -105,4 +106,30 @@ class TestSetStateMiddleware:
         expected_state = 'UPDATE_PROFILE'
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('stage', ['CHECK_WORDS', 'GRAMMAR'])
+    async def test_not_get_state_update_profile(self, mocker, stage):
+        message = Mock(from_user=Mock(id=self._user), chat=Mock(id=self._chat), text='/profile', spec=types.Message)
+        response_mock = mocker.AsyncMock(spec=ClientResponse)
+        response_mock.status = HTTPStatus.OK
+        response_mock.json = mocker.AsyncMock(return_value={'detail': {'stage': stage}})
+
+        self._storage_mock.check_address.return_value = (self._chat, self._user)
+
+        fsm_context_mock = mocker.Mock(spec=FSMContext)
+        fsm_context_mock.set_state = mocker.AsyncMock()
+        fsm_context_constructor_mock = mocker.patch('middlewears.set_state.FSMContext', return_value=fsm_context_mock)
+
+        with patch(self._get_method_target, return_value=response_mock) as mocked_get:
+            await self._service.on_pre_process_message(message=message, data={})
+
+        mocked_get.assert_called_once_with(
+            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
+            headers=settings.api_headers,
+        )
+
+        fsm_context_constructor_mock.assert_called()
+        fsm_context_constructor_mock.assert_called_once_with(storage=self._storage_mock, chat=self._chat, user=self._user)
+        expected_state = stage
+        fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
