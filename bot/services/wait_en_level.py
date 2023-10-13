84fafe8fd458a61d13a7b506bb017cb09e2bde69
psgaiduk <psgaiduk@gmail.com>
2023-10-13 10:55:41 +0700

    update structure

--- bot/services/wait_en_level.py
+++ bot/services/wait_en_level.py
@@ -1,13 +1,14 @@
 from http import HTTPStatus
-from typing import Optional, Union
+from typing import Optional
 
 from aiogram.dispatcher.storage import FSMContext
-from aiogram.types import CallbackQuery, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import CallbackQuery, ReplyKeyboardMarkup, KeyboardButton
 
 from bot import bot
 from choices import State
 from context_managers import http_client
 from dto.telegram_user import TelegramUserDTOModel
+from services.update_profile import UpdateProfileService
 from settings import settings
 
 
@@ -17,51 +18,52 @@ class WaitEnLevelService:
     _telegram_user: Optional[TelegramUserDTOModel]
     _stage: str
     _new_level: int
-    _keyboard: Optional[Union[ReplyKeyboardMarkup, InlineKeyboardMarkup]]
-    _message_text: str
 
     def __init__(self, callback_query: CallbackQuery, state: FSMContext):
         """Init."""
         self._callback_query = callback_query
         self._state = state
         self._telegram_user = None
-        self._keyboard = None
         self._new_level = int(self._callback_query.data.split('_')[-1])
 
     async def do(self) -> None:
         """Wait en level."""
         await self._get_user()
         await self._get_message_text()
-        await self._update_user()
-        await bot.send_message(chat_id=self._callback_query.from_user.id, text=self._message_text, reply_markup=self._keyboard)
 
-    async def _get_user(self):
+    async def _get_user(self) -> None:
         data = await self._state.get_data()
         self._telegram_user = data['user']
 
-    async def _get_message_text(self):
+    async def _get_message_text(self) -> None:
         if self._telegram_user.previous_stage == State.new_client.value:
             await self._update_en_level_for_new_client()
         else:
             await self._update_en_level_for_old_client()
 
-    async def _update_en_level_for_new_client(self):
+    async def _update_en_level_for_new_client(self) -> None:
         self._stage = State.read_book.value
-        first_task_complete_text = 'Подздравляю ты выполнил первое задание на день! Теперь ты можешь прочитать первый рассказ.'
+
+        is_update_user = await self._update_user()
+        if not is_update_user:
+            return
+
+        first_task_complete_text = 'Поздравляю ты выполнил первое задание на день! Теперь ты можешь прочитать первый рассказ.'
         await bot.send_message(chat_id=self._callback_query.from_user.id, text=first_task_complete_text)
-        self._message_text = 'Теперь ты готов к изучению английского языка. Для начала прочитай первый рассказ.'
-        self._keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton(text='Read'))
+        message_text = 'Теперь ты готов к изучению английского языка. Для начала прочитай первый рассказ.'
+        keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton(text='Read'))
+        await bot.send_message(chat_id=self._callback_query.from_user.id, text=message_text, reply_markup=keyboard)
 
-    async def _update_en_level_for_old_client(self):
+    async def _update_en_level_for_old_client(self) -> None:
         self._stage = State.update_profile.value
-        self._message_text = 'Уровень английского языка изменен.'
-        self._stage = State.update_profile.value
-        self._keyboard = InlineKeyboardMarkup()
-        self._keyboard.add(InlineKeyboardButton(text='Change english level', callback_data='user_profile_change_en_level'))
-        self._keyboard.add(InlineKeyboardButton(text='Change name', callback_data='user_pofile_change_name'))
-        self._keyboard.add(InlineKeyboardButton(text='Close', callback_data='user_profile_close'))
+        
+        is_update_user = await self._update_user()
+        if not is_update_user:
+            return
 
-    async def _update_user(self):
+        await UpdateProfileService(chat_id=self._callback_query.from_user.id, start_message_text='🤖 Уровень английского изменён.\n').do()
+
+    async def _update_user(self) -> bool:
         async with http_client() as client:
             url_update_telegram_user = f'{settings.api_url}/v1/telegram_user/{self._telegram_user.telegram_id}'
             data_for_update_user = {
@@ -76,5 +78,8 @@ class WaitEnLevelService:
             )
 
         if response_status != HTTPStatus.OK:
-            self._message_text = '🤖 Что-то пошло не так. Попробуйте еще раз, чуть позже.'
-            self._keyboard = None
+            message_text = '🤖 Что-то пошло не так. Попробуйте еще раз, чуть позже.'
+            await bot.send_message(chat_id=self._callback_query.from_user.id, text=message_text)
+            return False
+
+        return True
