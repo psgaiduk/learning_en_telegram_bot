6ba0579ac342aca080d3828d2c6395be83c9777b
psgaiduk <psgaiduk@gmail.com>
2023-10-14 12:17:04 +0700

    create test for get user wait en level service

---
+++ bot/tests/test_services/test_wait_en_level.py
@@ -0,0 +1,45 @@
+from http import HTTPStatus
+
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pytest import mark
+from unittest.mock import AsyncMock, Mock, patch
+
+from choices import State
+from dto import HeroLevelDTOModel, TelegramUserDTOModel
+from settings import settings
+from services import WaitEnLevelService
+
+
+class TestWaitEnLevelService:
+    """Tests for wait en level"""
+
+    @classmethod
+    def setup_class(cls):
+        cls._callback = Mock()
+        cls._post_method_target = 'context_managers.aio_http_client.AsyncHttpClient.post'
+        cls._state = Mock()
+
+    @mark.asyncio
+    async def test_get_user(self, mocker):
+        """Test get user."""
+
+        self._callback.from_user.id = 12345
+        self._callback.data = 'level_en_1'
+
+        telegram_user_model = TelegramUserDTOModel(
+            telegram_id=12345,
+            user_name='UserName',
+            experience=10,
+            previous_stage='PreviousStage',
+            stage='CurrentStage',
+            main_language=None,
+            level_en=None,
+            hero_level=None,
+        )
+        self._state.get_data = AsyncMock(return_value={'user': telegram_user_model})
+
+        self._service = WaitEnLevelService(callback_query=self._callback, state=self._state)
+
+        await self._service._get_user()
+        assert self._service._telegram_user == telegram_user_model
+        self._state.get_data.assert_awaited_once()
