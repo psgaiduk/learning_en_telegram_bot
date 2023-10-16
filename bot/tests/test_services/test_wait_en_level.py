10845bbc92f83e135aefdb045f12ab6f191bd3f7
psgaiduk <psgaiduk@gmail.com>
2023-10-16 10:22:35 +0700

    create test_update_en_level_for_old_client

--- bot/tests/test_services/test_wait_en_level.py
+++ bot/tests/test_services/test_wait_en_level.py
@@ -7,7 +7,7 @@ from unittest.mock import AsyncMock, Mock, patch
 from choices import State
 from dto import HeroLevelDTOModel, TelegramUserDTOModel
 from settings import settings
-from services import WaitEnLevelService
+from services import WaitEnLevelService, UpdateProfileService
 
 
 class TestWaitEnLevelService:
@@ -90,3 +90,35 @@ class TestWaitEnLevelService:
 
         self._service._update_en_level_for_new_client.assert_not_called()
         self._service._update_en_level_for_old_client.assert_called_once()
+
+    @mark.asyncio
+    async def test_update_en_level_for_old_client(self):
+        chat_id = 12345
+        start_message_text = '🤖 Уровень английского изменён.\n'
+        self._callback.data = 'level_en_1'
+        self._callback.from_user.id = chat_id
+        self._service = WaitEnLevelService(callback_query=self._callback, state=self._state)
+        self._service._update_user = AsyncMock(return_value=True)
+
+        self._service._telegram_user = TelegramUserDTOModel(
+            telegram_id=12345,
+            user_name='UserName',
+            experience=10,
+            previous_stage='',
+            stage='CurrentStage',
+            main_language=None,
+            level_en=None,
+            hero_level=None,
+        )
+
+        update_profile_service_do_mock = AsyncMock()
+        with patch.object(UpdateProfileService, 'do', update_profile_service_do_mock):
+            await self._service._update_en_level_for_old_client()
+
+        assert self._service._stage == State.update_profile.value
+        assert self._service._start_message_text == start_message_text
+        assert self._service._chat_id == chat_id
+
+        self._service._update_user.assert_awaited_once()
+
+        update_profile_service_do_mock.assert_awaited_once()
