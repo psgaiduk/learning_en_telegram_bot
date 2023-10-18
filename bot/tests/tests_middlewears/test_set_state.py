ea574ce91b4a4cb8eea798d51eb41217c729f0d4
psgaiduk <psgaiduk@gmail.com>
2023-10-18 11:11:27 +0700

    delete callback tests

--- bot/tests/tests_middlewears/test_set_state.py
+++ bot/tests/tests_middlewears/test_set_state.py
@@ -57,21 +57,6 @@ class TestSetStateMiddleware:
         expected_state = 'REGISTRATION'
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
-        callback_query = Mock(from_user=Mock(id=self._user), message=Mock(chat=Mock(id=self._chat)), spec=types.CallbackQuery)
-
-        with patch(self._get_method_target, return_value=({}, HTTPStatus.NOT_FOUND)) as mocked_get_callback:
-            await self._service.on_pre_process_callback_query(callback_query=callback_query, data={})
-
-        mocked_get_callback.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
-            headers=settings.api_headers,
-        )
-
-        fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_with(storage=self._storage_mock, chat=self._chat, user=self._user)
-        expected_state = 'REGISTRATION'
-        fsm_context_mock.set_state.assert_called_with(state=expected_state)
-
     @pytest.mark.asyncio
     @pytest.mark.parametrize(
         'stage',
@@ -104,24 +89,6 @@ class TestSetStateMiddleware:
         expected_data = {'user': response_data['detail']}
         fsm_context_mock.set_data.assert_called_once_with(data=expected_data)
 
-        callback_query = Mock(from_user=Mock(id=self._user), message=Mock(chat=Mock(id=self._chat)), spec=types.CallbackQuery)
-
-        with patch(self._get_method_target, return_value=(response_data, HTTPStatus.OK)) as mocked_get_callback:
-            await self._service.on_pre_process_callback_query(callback_query=callback_query, data={})
-
-        mocked_get_callback.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
-            headers=settings.api_headers,
-        )
-
-        fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_with(storage=self._storage_mock, chat=self._chat, user=self._user)
-        expected_state = stage
-        fsm_context_mock.set_state.assert_called_with(state=expected_state)
-
-        expected_data = {'user': response_data['detail']}
-        fsm_context_mock.set_data.assert_called_with(data=expected_data)
-
     @pytest.mark.asyncio
     @pytest.mark.parametrize('stage', ['WAIT_NAME', 'WAIT_EN_LEVEL', 'READ_BOOK'])
     async def test_get_state_update_profile(self, mocker, stage):
@@ -147,21 +114,6 @@ class TestSetStateMiddleware:
         fsm_context_constructor_mock.assert_called_once_with(storage=self._storage_mock, chat=self._chat, user=self._user)
         expected_state = 'UPDATE_PROFILE'
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
-
-        callback_query = Mock(from_user=Mock(id=self._user), message=Mock(chat=Mock(id=self._chat)), spec=types.CallbackQuery)
-
-        with patch(self._get_method_target, return_value=(response_data, HTTPStatus.OK)) as mocked_get_callback:
-            await self._service.on_pre_process_callback_query(callback_query=callback_query, data={})
-
-        mocked_get_callback.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
-            headers=settings.api_headers,
-        )
-
-        fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_with(storage=self._storage_mock, chat=self._chat, user=self._user)
-        expected_state = 'UPDATE_PROFILE'
-        fsm_context_mock.set_state.assert_called_with(state=expected_state)
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize('stage', ['CHECK_WORDS', 'GRAMMAR'])
@@ -189,20 +141,6 @@ class TestSetStateMiddleware:
         expected_state = stage
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
-        callback_query = Mock(from_user=Mock(id=self._user), message=Mock(chat=Mock(id=self._chat)), spec=types.CallbackQuery)
-
-        with patch(self._get_method_target, return_value=(response_data, HTTPStatus.OK)) as mocked_get_callback:
-            await self._service.on_pre_process_callback_query(callback_query=callback_query, data={})
-
-        mocked_get_callback.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
-            headers=settings.api_headers,
-        )
-
-        fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_with(storage=self._storage_mock, chat=self._chat, user=self._user)
-        fsm_context_mock.set_state.assert_called_with(state=expected_state)
-
     @pytest.mark.asyncio
     @pytest.mark.parametrize('stage', ['WAIT_NAME', 'WAIT_EN_LEVEL', 'READ_BOOK'])
     async def test_get_state_records(self, mocker, stage):
@@ -228,21 +166,6 @@ class TestSetStateMiddleware:
         fsm_context_constructor_mock.assert_called_once_with(storage=self._storage_mock, chat=self._chat, user=self._user)
         expected_state = 'RECORDS'
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
-
-        callback_query = Mock(from_user=Mock(id=self._user), message=Mock(chat=Mock(id=self._chat)), spec=types.CallbackQuery)
-
-        with patch(self._get_method_target, return_value=(response_data, HTTPStatus.OK)) as mocked_get_callback:
-            await self._service.on_pre_process_callback_query(callback_query=callback_query, data={})
-
-        mocked_get_callback.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
-            headers=settings.api_headers,
-        )
-
-        fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_with(storage=self._storage_mock, chat=self._chat, user=self._user)
-        expected_state = 'RECORDS'
-        fsm_context_mock.set_state.assert_called_with(state=expected_state)
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize('stage', ['CHECK_WORDS', 'GRAMMAR', 'UPDATE_PROFILE'])
@@ -270,21 +193,6 @@ class TestSetStateMiddleware:
         expected_state = stage
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
-        callback_query = Mock(from_user=Mock(id=self._user), message=Mock(chat=Mock(id=self._chat)), spec=types.CallbackQuery)
-
-        with patch(self._get_method_target, return_value=(response_data, HTTPStatus.OK)) as mocked_get_callback:
-            await self._service.on_pre_process_callback_query(callback_query=callback_query, data={})
-
-        mocked_get_callback.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
-            headers=settings.api_headers,
-        )
-
-        fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_with(storage=self._storage_mock, chat=self._chat, user=self._user)
-        expected_state = stage
-        fsm_context_mock.set_state.assert_called_with(state=expected_state)
-
     @pytest.mark.asyncio
     @pytest.mark.parametrize('stage', ['WAIT_NAME', 'WAIT_EN_LEVEL', 'READ_BOOK'])
     async def test_get_state_achievements(self, mocker, stage):
@@ -311,21 +219,6 @@ class TestSetStateMiddleware:
         expected_state = 'ACHIEVEMENTS'
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
 
-        callback_query = Mock(from_user=Mock(id=self._user), message=Mock(chat=Mock(id=self._chat)), spec=types.CallbackQuery)
-
-        with patch(self._get_method_target, return_value=(response_data, HTTPStatus.OK)) as mocked_get_callback:
-            await self._service.on_pre_process_callback_query(callback_query=callback_query, data={})
-
-        mocked_get_callback.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
-            headers=settings.api_headers,
-        )
-
-        fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_with(storage=self._storage_mock, chat=self._chat, user=self._user)
-        expected_state = 'ACHIEVEMENTS'
-        fsm_context_mock.set_state.assert_called_with(state=expected_state)
-
     @pytest.mark.asyncio
     @pytest.mark.parametrize('stage', ['CHECK_WORDS', 'GRAMMAR', 'UPDATE_PROFILE'])
     async def test_not_get_state_achievements(self, mocker, stage):
@@ -351,18 +244,3 @@ class TestSetStateMiddleware:
         fsm_context_constructor_mock.assert_called_once_with(storage=self._storage_mock, chat=self._chat, user=self._user)
         expected_state = stage
         fsm_context_mock.set_state.assert_called_once_with(state=expected_state)
-
-        callback_query = Mock(from_user=Mock(id=self._user), message=Mock(chat=Mock(id=self._chat)), spec=types.CallbackQuery)
-
-        with patch(self._get_method_target, return_value=(response_data, HTTPStatus.OK)) as mocked_get_callback:
-            await self._service.on_pre_process_callback_query(callback_query=callback_query, data={})
-
-        mocked_get_callback.assert_called_once_with(
-            url=f'{settings.api_url}/v1/telegram_user/{self._chat}',
-            headers=settings.api_headers,
-        )
-
-        fsm_context_constructor_mock.assert_called()
-        fsm_context_constructor_mock.assert_called_with(storage=self._storage_mock, chat=self._chat, user=self._user)
-        expected_state = stage
-        fsm_context_mock.set_state.assert_called_with(state=expected_state)
