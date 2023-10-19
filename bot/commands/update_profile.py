c0c9f95b1001b09f06e3bd493c3e38971c507da6
psgaiduk <psgaiduk@gmail.com>
2023-10-19 23:09:43 +0700

    create handle update profile name

--- bot/commands/update_profile.py
+++ bot/commands/update_profile.py
@@ -18,3 +18,15 @@ async def handle_update_profile(message: Message):
 
     await UpdateProfileService(chat_id=message.from_user.id, start_message_text='').do()
 
+
+@dispatcher.callback_query_handler(Text(equals='user_profile_change_name'), state=State.update_profile.value)
+async def handle_update_profile_name(callback_query: CallbackQuery):
+    """Handle update profile."""
+
+    is_update = await update_user_state(telegram_id=callback_query.from_user.id, state=State.wait_name.value)
+
+    if is_update is False:
+        return
+
+    message_text = 'Введите ваше имя:'
+    await bot.send_message(chat_id=callback_query.from_user.id, text=message_text)
