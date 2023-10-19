2632cbb9c328ebec246f0c133599921208d31aba
psgaiduk <psgaiduk@gmail.com>
2023-10-19 23:43:21 +0700

    create update profile en level

--- bot/commands/update_profile.py
+++ bot/commands/update_profile.py
@@ -1,9 +1,11 @@
 from aiogram.types import CallbackQuery, Message
 from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.storage import FSMContext
 
 from bot import bot, dispatcher
 from choices import State
-from functions import update_user_state
+from dto.telegram_user import TelegramUserDTOModel
+from functions import create_keyboard_for_en_levels, update_user_state
 from services import UpdateProfileService
 
 
@@ -30,3 +32,21 @@ async def handle_update_profile_name(callback_query: CallbackQuery):
 
     message_text = 'Введите ваше имя:'
     await bot.send_message(chat_id=callback_query.from_user.id, text=message_text)
+
+
+@dispatcher.callback_query_handler(Text(equals='user_profile_change_en_level'), state=State.update_profile.value)
+async def handle_update_profile_en_level(callback_query: CallbackQuery, state: FSMContext):
+    """Handle update profile."""
+
+    is_update = await update_user_state(telegram_id=callback_query.from_user.id, state=State.wait_en_level.value)
+
+    if is_update is False:
+        return
+
+    data = await state.get_data()
+    telegram_user: TelegramUserDTOModel = data['user']
+
+    inline_kb = await create_keyboard_for_en_levels(hero_level=telegram_user.hero_level.order)
+
+    message_text = 'Выберите уровень сложности: '
+    await bot.send_message(chat_id=callback_query.from_user.id, text=message_text, reply_markup=inline_kb)
