5d1d47513470863f1299ceb2d692ff12ab2b5e10
psgaiduk <psgaiduk@gmail.com>
2023-10-25 12:05:12 +0800

    create test handle registration

---
+++ bot/tests/test_commands/test_registration_command.py
@@ -0,0 +1,23 @@
+from unittest.mock import AsyncMock, MagicMock, patch
+
+from aiogram import types
+from pytest import mark
+
+from commands import handle_registration
+
+
+class TestRegistrationCommand:
+    """Tests for wait en level"""
+
+    @patch('commands.registration.RegistrationService')
+    @mark.asyncio
+    async def test_handle_registration(self, mock_registration_service, monkeypatch):
+        monkeypatch.setattr('commands.registration.State', MagicMock(value='registration'))
+        mock_message = types.Message(message_id=1, chat=types.Chat(id=1), text='/start')
+
+        mock_registration_service.return_value.do = AsyncMock()
+
+        await handle_registration(mock_message)
+
+        mock_registration_service.assert_called_once_with(message=mock_message)
+        mock_registration_service.return_value.do.assert_called_once()
