2785a386bd3e9eb9b49a76aa2d3fa834c8665cad
psgaiduk <psgaiduk@gmail.com>
2023-10-29 09:58:14 +0800

    add test wait en level for other data

--- bot/tests/test_commands/test_wait_en_level_command.py
+++ bot/tests/test_commands/test_wait_en_level_command.py
@@ -3,7 +3,6 @@ from unittest.mock import AsyncMock, Mock, patch
 from pytest import mark
 
 from bot import bot
-from choices import State
 from commands import (
     handle_wait_en_level,
     handle_wait_en_level_incorrect_text,
@@ -28,3 +27,19 @@ class TestWaitEnLevelCommand:
 
         mock_wait_en_level_service.assert_called_once_with(callback_query=callback, state=state)
         mock_wait_en_level_service.return_value.do.assert_called_once()
+
+    @mark.asyncio
+    async def test_handle_wait_en_level_other_data(self):
+        chat_id = 1
+        callback = Mock()
+        callback.data = 'test'
+        callback.from_user.id = chat_id
+
+        with patch.object(bot, 'send_message', new=AsyncMock()) as mock_send_message:
+
+            await handle_wait_en_level_incorrect_text(message=callback)
+
+            mock_send_message.assert_called_once_with(
+                chat_id=chat_id,
+                text='Нужно нажать по одной из кнопок, чтобы изменить уровень английского.',
+            )
