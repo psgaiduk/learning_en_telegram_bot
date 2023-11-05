5b2a646496ce6fe5fb950dd692a0c06a1658633f
psgaiduk <psgaiduk@gmail.com>
2023-11-05 14:58:38 +0800

    create test not update history book sentences

--- api/tests/test_history/test_update_history_sentence.py
+++ api/tests/test_history/test_update_history_sentence.py
@@ -1,11 +1,9 @@
-from datetime import datetime
-
 from fastapi.testclient import TestClient
 from fastapi import status
 from pytest import mark
 
 from main import app
-from models import Users, UsersBooksSentencesHistory
+from models import UsersBooksSentencesHistory
 from settings import settings
 from tests.connect_db import db_session
 
@@ -97,4 +95,51 @@ class TestUpdateHistorySentenceAPI:
             assert history_sentence.is_read is False
             assert history_sentence.id == history_sentence_id
             assert history_sentence.check_words == expect_words
+
+    def test_not_update_without_check_words_and_is_read_history_sentence(self):
+        """Test not update check words and is_read history sentence."""
+
+        with db_session() as db:
+            history_sentence = db.query(UsersBooksSentencesHistory).first()
+            history_sentence_id = history_sentence.id
+
+        data_for_update = {
+            'id': history_sentence_id,
+        }
+
+        url = f'{self._url}/{history_sentence_id}/'
+        response = self._client.patch(url=url, headers=self._headers, json=data_for_update)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_not_update_without_headers(self):
+        """Test not update read history sentence without headers."""
+
+        with db_session() as db:
+            history_sentence = db.query(UsersBooksSentencesHistory).first()
+            history_sentence_id = history_sentence.id
+
+        data_for_update = {
+            'id': history_sentence_id,
+            'is_read': False,
+        }
+
+        url = f'{self._url}/{history_sentence_id}/'
+        response = self._client.patch(url=url, json=data_for_update)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_not_update_with_wrong_api_key(self):
+        """Test not update history sentence with wrong api key."""
+
+        with db_session() as db:
+            history_sentence = db.query(UsersBooksSentencesHistory).first()
+            history_sentence_id = history_sentence.id
+
+        data_for_update = {
+            'id': history_sentence_id,
+            'is_read': False,
+        }
+
+        url = f'{self._url}/{history_sentence_id}/'
+        response = self._client.patch(url=url, json=data_for_update, headers={'X-API-Key': 'test'})
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
