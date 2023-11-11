1e243265faa09d60773bdc944284f704ca155a77
psgaiduk <psgaiduk@gmail.com>
2023-11-11 12:30:07 +0800

    create command send message

--- bot/services/check_words.py
+++ bot/services/check_words.py
@@ -3,11 +3,18 @@ from aiogram.dispatcher.storage import FSMContext
 
 from bot import bot
 from choices import State
+from dto import TelegramUserDTOModel, WordDTOModel
 from functions import update_data_by_api
 
 
 class CheckWordsService:
     """Check words before sentence."""
+
+    _state: FSMContext
+    _start_text_message: str
+    _telegram_user: TelegramUserDTOModel
+    _words: list
+    _first_word: WordDTOModel
 
     def __init__(self, state: FSMContext, start_text_message: str = '') -> None:
         """Init."""
@@ -19,7 +26,7 @@ class CheckWordsService:
 
         await self._get_user()
         self._words = self._telegram_user.new_sentence.words
-        first_word = self._words.pop(0)
+        self._first_word = self._words.pop(0)
 
         if await self._update_user() is False:
             return
@@ -27,13 +34,8 @@ class CheckWordsService:
         if await self._update_sentence() is False:
             return
 
-        inline_keyboard = InlineKeyboardMarkup()
-        inline_keyboard.add(InlineKeyboardButton(text='Знаю', callback_data=f'know_word_true_{first_word.word_id}'))
-        inline_keyboard.add(InlineKeyboardButton(text='Не знаю', callback_data=f'know_word_false_{first_word.word_id}'))
+        await self._send_message()
 
-        text_message = f'{self._start_text_message}Слово: {first_word.word}\nПеревод: {first_word.translation["ru"]}'
-        await bot.send_message(chat_id=self._telegram_user.telegram_id, text=text_message, reply_markup=inline_keyboard)
-        
     async def _get_user(self) -> None:
         data = await self._state.get_data()
         self._telegram_user = data['user']
@@ -63,3 +65,11 @@ class CheckWordsService:
             params_for_update=data_for_update_history_sentence,
             url_for_update=f'history/sentences/{self._telegram_user.new_sentence.history_sentence_id}',
         )
+
+    async def _send_message(self):
+        inline_keyboard = InlineKeyboardMarkup()
+        inline_keyboard.add(InlineKeyboardButton(text='Знаю', callback_data=f'know_word_true_{self._first_word.word_id}'))
+        inline_keyboard.add(InlineKeyboardButton(text='Не знаю', callback_data=f'know_word_false_{self._first_word.word_id}'))
+
+        text_message = f'{self._start_text_message}Слово: {self._first_word.word}\nПеревод: {self._first_word.translation["ru"]}'
+        await bot.send_message(chat_id=self._telegram_user.telegram_id, text=text_message, reply_markup=inline_keyboard)
