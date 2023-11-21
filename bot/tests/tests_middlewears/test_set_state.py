11265cd339a6ca1eeacfad91fc4463e73a26997f
psgaiduk <psgaiduk@gmail.com>
2023-11-21 09:16:25 +0800

    create test for get_real_state_regular_work

--- bot/tests/tests_middlewears/test_set_state.py
+++ bot/tests/tests_middlewears/test_set_state.py
@@ -347,3 +347,52 @@ class TestSetStateMiddleware:
 
         assert await self._service.get_real_state() == state
         mock_work_with_read_status.assert_called_once()
+
+    @pytest.mark.parametrize('message_text, state, expected_state, is_update', [
+        ('/profile', State.registration.value, State.registration.value, None),
+        ('/profile', State.error.value, State.error.value, None),
+        ('/profile', State.grammar.value, State.grammar.value, None),
+        ('/profile', State.read_book.value, State.update_profile.value, True),
+        ('/profile', State.read_book.value, State.error.value, False),
+        ('/profile', State.wait_name.value, State.update_profile.value, None),
+        ('/profile', State.wait_en_level.value, State.update_profile.value, None),
+        ('/records', State.registration.value, State.registration.value, None),
+        ('/records', State.error.value, State.error.value, None),
+        ('/records', State.grammar.value, State.grammar.value, None),
+        ('/records', State.update_profile.value, State.update_profile.value, None),
+        ('/records', State.read_book.value, State.records.value, None),
+        ('/achievements', State.registration.value, State.registration.value, None),
+        ('/achievements', State.error.value, State.error.value, None),
+        ('/achievements', State.grammar.value, State.grammar.value, None),
+        ('/achievements', State.update_profile.value, State.update_profile.value, None),
+        ('/achievements', State.read_book.value, State.achievements.value, None),
+        ('just text', State.update_profile.value, State.update_profile.value, None),
+        ('just text', State.read_book.value, State.read_book.value, None),
+    ])
+    @patch('middlewears.set_state.update_data_by_api', new_callable=AsyncMock)
+    @pytest.mark.asyncio
+    async def test_get_real_state_regular_work(self, mock_update_user, message_text, state, expected_state, is_update):
+        self._service._state = state
+        self._service._message_text = message_text
+        self._service._telegram_user = TelegramUserDTOModel(**self._response_data['detail'])
+        self._service._telegram_user.stage = state
+        mock_update_user.side_effect = [is_update]
+
+        if state == State.read_book.value and message_text not in {'/profile', '/records', '/achievements'}:
+            mock_work_with_read_status = AsyncMock(return_value=state)
+            self._service.work_with_read_status = mock_work_with_read_status
+
+        assert await self._service.get_real_state() == expected_state
+        
+        if message_text == '/profile' and state == State.read_book.value:
+            expected_data_for_update_user = {
+                'telegram_id': self._service._telegram_user.telegram_id,
+                'previous_stage': self._service._telegram_user.stage,
+            }
+
+            mock_update_user.assert_awaited_once_with(
+                telegram_id=self._service._telegram_user.telegram_id,
+                params_for_update=expected_data_for_update_user,
+                url_for_update=f'telegram_user/{self._service._telegram_user.telegram_id}',
+            )
+
