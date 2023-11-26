0c94d1128621b9840a4f169f75a6820585e4f1b0
psgaiduk <psgaiduk@gmail.com>
2023-11-26 15:34:38 +0800

    create test check words other data for message

--- bot/tests/test_commands/test_check_words_commands.py
+++ bot/tests/test_commands/test_check_words_commands.py
@@ -25,3 +25,18 @@ class TestCheckWordsCommand:
             expected_text = 'Нужно нажать по кнопке I know или I don\'t know'
 
             mock_send_message.assert_called_once_with(chat_id=chat_id, text=expected_text)
+
+    @mark.asyncio
+    async def test_handle_check_words_other_data_message(self, monkeypatch):
+        chat_id = 1
+        monkeypatch.setattr('commands.registration.State', MagicMock(value=State.check_words.value))
+        user = User(id=chat_id, is_bot=False, first_name="Test User")
+        mock_message = Message(id=1, chat=chat_id, data='other_data', from_user=user)
+        mock_message.from_user = user
+
+        with patch.object(bot, 'send_message', new=AsyncMock()) as mock_send_message:
+            await handle_check_words_other_data(mock_message)
+
+            expected_text = 'Нужно нажать по кнопке I know или I don\'t know'
+
+            mock_send_message.assert_called_once_with(chat_id=chat_id, text=expected_text)
