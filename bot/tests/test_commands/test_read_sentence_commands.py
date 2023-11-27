a3f88990a8785a2562de29c66dfdf87b0339e8a9
psgaiduk <psgaiduk@gmail.com>
2023-11-27 09:53:11 +0800

    create test read sentence other data for message

--- bot/tests/test_commands/test_read_sentence_commands.py
+++ bot/tests/test_commands/test_read_sentence_commands.py
@@ -23,3 +23,18 @@ class TestReadSentenceCommand:
             expected_keyboard.add(KeyboardButton(text='Read'))
             expected_text = 'Нужно нажать по кнопке Read'
             mock_send_message.assert_called_once_with(chat_id=chat_id, text=expected_text, reply_markup=expected_keyboard, parse_mode=ParseMode.HTML)
+
+    @mark.asyncio
+    async def test_handle_read_sentence_other_data_message(self):
+        chat_id = 1
+        user = User(id=chat_id, is_bot=False, first_name='Test User')
+        mock_message = Message(id=1, chat=chat_id, text='Read', from_user=user)
+        mock_message.from_user = user
+
+        with patch.object(bot, 'send_message', new=AsyncMock()) as mock_send_message:
+            await handle_read_sentence_other_data(mock_message)
+            expected_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+            expected_keyboard.add(KeyboardButton(text='Read'))
+            expected_text = 'Нужно нажать по кнопке Read'
+            mock_send_message.assert_called_once_with(chat_id=chat_id, text=expected_text, reply_markup=expected_keyboard, parse_mode=ParseMode.HTML)
+
