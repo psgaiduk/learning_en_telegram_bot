f84a65b6b827cbb9b1f15f1b8f61d77eb671bf49
psgaiduk <psgaiduk@gmail.com>
2023-11-27 09:52:32 +0800

    create test read sentence other data for callback

---
+++ bot/tests/test_commands/test_read_sentence_commands.py
@@ -0,0 +1,25 @@
+from unittest.mock import AsyncMock, MagicMock, patch
+
+from aiogram.types import CallbackQuery, Message, ParseMode, ReplyKeyboardMarkup, KeyboardButton, User
+from pytest import mark
+
+from bot import bot
+from commands import handle_read_sentence_other_data
+
+
+class TestReadSentenceCommand:
+    """Tests command read sentence."""
+
+    @mark.asyncio
+    async def test_handle_read_sentence_other_data_callback(self):
+        chat_id = 1
+        user = User(id=chat_id, is_bot=False, first_name='Test User')
+        mock_callback = CallbackQuery(id=1, chat=chat_id, data='other_data', from_user=user)
+        mock_callback.from_user = user
+
+        with patch.object(bot, 'send_message', new=AsyncMock()) as mock_send_message:
+            await handle_read_sentence_other_data(mock_callback)
+            expected_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+            expected_keyboard.add(KeyboardButton(text='Read'))
+            expected_text = 'Нужно нажать по кнопке Read'
+            mock_send_message.assert_called_once_with(chat_id=chat_id, text=expected_text, reply_markup=expected_keyboard, parse_mode=ParseMode.HTML)
