c02655874ab391f4cf26602b35a4c4c28374449e
psgaiduk <psgaiduk@gmail.com>
2023-11-28 09:36:53 +0800

    create test read sentence

--- bot/tests/test_commands/test_read_sentence_commands.py
+++ bot/tests/test_commands/test_read_sentence_commands.py
@@ -1,10 +1,11 @@
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import AsyncMock, patch
 
 from aiogram.types import CallbackQuery, Message, ParseMode, ReplyKeyboardMarkup, KeyboardButton, User
 from pytest import mark
 
 from bot import bot
-from commands import handle_read_sentence_other_data
+from commands import handle_read_sentence, handle_read_sentence_other_data
+from tests.fixtures import telegram_user_with_sentence_and_word, sentence_with_word, word_new
 
 
 class TestReadSentenceCommand:
@@ -38,3 +39,41 @@ class TestReadSentenceCommand:
             expected_text = 'Нужно нажать по кнопке Read'
             mock_send_message.assert_called_once_with(chat_id=chat_id, text=expected_text, reply_markup=expected_keyboard, parse_mode=ParseMode.HTML)
 
+    @mark.parametrize('is_update_sentence', [True, False])
+    @patch('commands.read_sentence.delete_message')
+    @patch('commands.read_sentence.update_data_by_api')
+    @patch('commands.read_sentence.bot', new_callable=AsyncMock)
+    @mark.asyncio
+    async def test_handle_read_sentence(self, mock_bot, mock_update_data, mock_delete_message, is_update_sentence, telegram_user_with_sentence_and_word):
+        chat_id = 1
+        user = User(id=chat_id, is_bot=False, first_name='Test User')
+        mock_callback = CallbackQuery(id=1, chat=chat_id, data='other_data', from_user=user, message=Message(id=1))
+        mock_callback.from_user = user
+        state = AsyncMock()
+        telegram_user = telegram_user_with_sentence_and_word
+        state.get_data = AsyncMock(return_value={'user': telegram_user})
+
+        mock_update_data.return_value = is_update_sentence
+
+        await handle_read_sentence(message=mock_callback, state=state)
+        mock_update_data.assert_called_once_with(
+            telegram_id=chat_id,
+            params_for_update={'id': state.get_data.return_value['user'].new_sentence.history_sentence_id, 'is_read': True},
+            url_for_update=f'history/sentences/{state.get_data.return_value["user"].new_sentence.history_sentence_id}',
+        )
+
+        if is_update_sentence is False:
+            mock_bot.send_message.assert_not_called()
+            mock_delete_message.assert_not_called()
+        else:
+            expected_message_text = (f'{telegram_user.new_sentence.text}\n\n'
+                                     f'<tg-spoiler>{telegram_user.new_sentence.translation.get("ru")}</tg-spoiler>')
+            expected_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+            expected_keyboard.add(KeyboardButton(text='Read'))
+            mock_bot.send_message.assert_called_once_with(
+                chat_id=chat_id,
+                text=expected_message_text,
+                parse_mode=ParseMode.HTML,
+                reply_markup=expected_keyboard,
+            )
+            mock_delete_message.assert_called_once_with(chat_id=chat_id, message_id=mock_callback.message.message_id)
