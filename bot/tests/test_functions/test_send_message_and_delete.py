d9e3782a29dd0661752295e8f7c3902608455967
psgaiduk <psgaiduk@gmail.com>
2023-11-30 10:33:17 +0800

    create test delete message

--- bot/tests/test_functions/test_send_message_and_delete.py
+++ bot/tests/test_functions/test_send_message_and_delete.py
@@ -9,9 +9,17 @@ from functions import delete_message, send_message_and_delete
 class TestSendDeleteMessageFunction:
     """Tests for delete and send message function."""
 
+    @mark.parametrize('error, is_delete', [(None, True), (MessageCantBeDeleted, True), (ValueError, False), (AttributeError, True)])
     @patch('functions.send_and_delete_message.bot', new_callable=AsyncMock)
     @mark.asyncio
-    async def test_delete_message(self, mock_bot):
-        mock_bot.side_effect = [MessageCantBeDeleted]
-        await delete_message(chat_id=1, message_id=1)
-        mock_bot.delete_message.assert_called_once_with(chat_id=1, message_id=1)
+    async def test_delete_message(self, mock_bot, error, is_delete):
+        mock_bot.delete_message.side_effect = error
+        if is_delete is False:
+            try:
+                await delete_message(chat_id=1, message_id=1)
+            except Exception as e:
+                assert isinstance(e, error)
+        else:
+            await delete_message(chat_id=1, message_id=1)
+
+        assert mock_bot.delete_message.call_count == 1
