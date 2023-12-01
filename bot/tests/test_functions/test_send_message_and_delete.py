ac0f3b8d5d38fa0b4c1d472260ba80be6016dd91
psgaiduk <psgaiduk@gmail.com>
2023-12-01 09:52:48 +0800

    create test for send message and delete

--- bot/tests/test_functions/test_send_message_and_delete.py
+++ bot/tests/test_functions/test_send_message_and_delete.py
@@ -23,3 +23,17 @@ class TestSendDeleteMessageFunction:
             await delete_message(chat_id=1, message_id=1)
 
         assert mock_bot.delete_message.call_count == 1
+
+    @patch('functions.send_and_delete_message.bot', new_callable=AsyncMock)
+    @patch('functions.send_and_delete_message.delete_message')
+    @mark.asyncio
+    async def test_send_message_and_delete(self, mock_delete_message, mock_bot):
+        chat_id = 1
+        message_text = 'test'
+        reply_markup = None
+
+        message = AsyncMock(message_id=1)
+        mock_bot.send_message.return_value = message
+        await send_message_and_delete(chat_id=chat_id, message_text=message_text, reply_markup=reply_markup)
+        mock_bot.send_message.assert_called_once_with(chat_id=chat_id, text=message_text, reply_markup=reply_markup)
+        mock_delete_message.assert_called_once_with(chat_id=chat_id, message_id=message.message_id)
