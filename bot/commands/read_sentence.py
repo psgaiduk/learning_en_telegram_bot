6534cd4b40f515e9b5c36949128d9a982eba19d8
psgaiduk <psgaiduk@gmail.com>
2023-12-12 09:23:39 +0300

    for level less than 3 send text with words

--- bot/commands/read_sentence.py
+++ bot/commands/read_sentence.py
@@ -6,6 +6,7 @@ from aiogram.dispatcher.storage import FSMContext
 
 from bot import bot, dispatcher
 from choices import State
+from dto import TelegramUserDTOModel
 from functions import delete_message, update_data_by_api
 
 
@@ -15,9 +16,12 @@ async def handle_read_sentence(message: Union[CallbackQuery, Message], state: FS
     """Handle check words after push button read."""
 
     data = await state.get_data()
-    telegram_user = data['user']
+    telegram_user: TelegramUserDTOModel = data['user']
 
     sentence_text = telegram_user.new_sentence.text
+    if telegram_user.level_en.order < 3:
+        sentence_text = telegram_user.new_sentence.text_with_words
+
     sentence_translation = telegram_user.new_sentence.translation.get('ru')
 
     message_text = f'{sentence_text}\n\n<tg-spoiler>{sentence_translation}</tg-spoiler>'
