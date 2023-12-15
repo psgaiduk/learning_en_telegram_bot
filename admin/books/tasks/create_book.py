e12e1a2a813452b79c06474513db20079b88332e
psgaiduk <psgaiduk@gmail.com>
2023-12-15 08:40:53 +0300

    new logic for create book

--- admin/books/tasks/create_book.py
+++ admin/books/tasks/create_book.py
@@ -3,11 +3,7 @@ from django_q.tasks import Chain
 from loguru import logger
 
 from ai_app import AISDK
-from books.dto import SentenceDTO
 from books.models import BooksModel, BooksSentencesModel, WordsModel, TypeWordsModel
-from books.services import CreateWordsAndSentencesService
-from telegram_users.choices import Language
-from nlp_translate import translate_text
 
 
 def create_book_task(book_id: int) -> None:
@@ -16,68 +12,68 @@ def create_book_task(book_id: int) -> None:
     instance = BooksModel.objects.get(book_id=book_id)
     logger.debug(f'Book {instance.book_id} - {instance.title}')
     logger.debug(f'Book text {instance.text}')
-    sentences = CreateWordsAndSentencesService(book=instance).work()
-    logger.debug(f'Sentences {sentences}')
     chain = Chain(cached=True)
     logger.debug(f'Chain {chain}')
-
-    for sentence in sentences:
-        logger.debug(f'Sentence {sentence}')
-        chain.append(create_sentence, sentence, instance)
-
+    chain.append(create_sentences, instance)
     logger.debug(f'Chain {chain}')
     chain.run()
 
 
-def create_sentence(sentence: SentenceDTO, instance: BooksModel) -> None:
+def create_sentences(instance: BooksModel) -> None:
     """Translate and add sentence."""
-    words_from_ai = AISDK().get_words(sentence=sentence.text, english_level=instance.level_en.title)
-    AISDK().create_audio_file(sentence=sentence.text, file_name=f'{instance.book_id} - {sentence.index}')
-    words_by_type = {1: [], 2: [], 3: []}
+    text_data = AISDK().translate_and_analyse(text=instance.text)
+    logger.debug(f'Text data {text_data}')
+    sentences = text_data.split('\n')
+    sentences = [sentence for sentence in sentences if sentence]
+    logger.debug(f'Sentences {sentences}')
+    index = 1
+    type_word = TypeWordsModel.objects.get(type_word_id=1)
+    type_phrase = TypeWordsModel.objects.get(type_word_id=2)
+    for sentence in sentences:
+        sentence_data = sentence.split('---')
+        english_sentence = sentence_data[0].strip()
+        logger.debug(f'English sentence {english_sentence}')
+        russian_sentence = sentence_data[1].strip()
+        logger.debug(f'Russian sentence {russian_sentence}')
+        words_with_translate = sentence_data[2].replace('.', '').strip().split(', ')
+        logger.debug(f'Words with translate {words_with_translate}')
+        sentence_times = sentence_data[3].strip()
+        logger.debug(f'Sentence times {sentence_times}')
+        description_time = sentence_data[4].strip()
+        logger.debug(f'Description time {description_time}')
 
-    for word in words_from_ai:
-        if '- 1' in word:
-            word = word.replace(' - 1', '').strip()
+        AISDK().create_audio_file(sentence=english_sentence, file_name=f'{instance.book_id} - {index}')
+
+        all_words = [word.split(': ')[0] for word in words_with_translate]
+        logger.debug(f'All words {all_words}')
+
+        words = WordsModel.objects.filter(word__in=all_words)
+        new_words = set(all_words) - set(words.values_list('word', flat=True))
+
+        for words_with_translate in words_with_translate:
+            word, translates_word = words_with_translate.split(': ')
+            if word not in new_words or len(word) < 3:
+                continue
+
+            type_of_word = type_word
             if ' ' in word:
-                words_by_type[3].append(word)
-                continue
-        elif '- 2' in word:
-            word = word.replace(' - 2', '').strip()
-            if ' ' in word:
-                words_by_type[2].append(word)
-                continue
-        elif '- 3' in word:
-            word = word.replace(' - 3', '').strip()
+                type_of_word = type_phrase
 
-        words_by_type[1].append(word)
+            WordsModel.objects.create(word=word, translation=translates_word, type_word=type_of_word)
 
-    all_words = sum(words_by_type.values(), [])
-    words = WordsModel.objects.filter(word__in=all_words)
-    new_words = set(all_words) - set(words.values_list('word', flat=True))
-    for type_word_id, words in words_by_type.items():
-        type_word = TypeWordsModel.objects.get(type_word_id=type_word_id)
-        for word in words:
-            if len(word) < 3 or word not in new_words:
-                continue
-            translates_word = {}
-            for language_code, _ in Language.choices():
-                translates_word[language_code] = translate_text(text_on_en=word, language=language_code)
-            WordsModel.objects.create(word=word, translation=translates_word, type_word=type_word)
+        words = WordsModel.objects.filter(Q(word__in=all_words))
 
-    translates_sentence = {}
-    for language_code, _ in Language.choices():
-        translate_sentence = translate_text(text_on_en=sentence.text, language=language_code)
-        translates_sentence[language_code] = translate_sentence
+        book_sentence, created = BooksSentencesModel.objects.update_or_create(
+            book=instance,
+            order=index,
+            defaults={
+                'text': english_sentence,
+                'translation': russian_sentence,
+                'sentence_times': sentence_times,
+                'description_time': description_time,
+            }
+        )
 
-    words = WordsModel.objects.filter(Q(word__in=all_words))
+        book_sentence.words.set(words)
 
-    book_sentence, created = BooksSentencesModel.objects.update_or_create(
-        book=instance,
-        order=sentence.index,
-        defaults={
-            'text': sentence.text,
-            'translation': translates_sentence,
-        }
-    )
-
-    book_sentence.words.set(words)
+        index += 1
