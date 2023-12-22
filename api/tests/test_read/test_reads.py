14dcc600e49aad2d1b8333412b08bd346a086c0a
psgaiduk <psgaiduk@gmail.com>
2023-12-22 08:53:30 +0300

    add tests for random book and next book

--- api/tests/test_read/test_reads.py
+++ api/tests/test_read/test_reads.py
@@ -59,13 +59,16 @@ class TestReadApi:
             sentence = db.query(BooksSentences).filter(BooksSentences.sentence_id == response['sentence_id']).first()
             assert sentence.order == 1
 
-    def test_get_first_sentence_for_new_read_book(self):
+    def test_get_first_sentence_for_next_read_book(self):
         with db_session() as db:
             telegram_user = db.query(Users).order_by(Users.telegram_id.desc()).first()
             telegram_id = telegram_user.telegram_id
             users_history_book = db.query(UsersBooksHistory).filter(UsersBooksHistory.telegram_user_id == telegram_id).first()
             assert users_history_book is None
-            old_book = db.query(BooksModel).filter(BooksModel.level_en_id == telegram_user.level_en_id).first()
+            old_book = db.query(BooksModel).filter(
+                BooksModel.level_en_id == telegram_user.level_en_id,
+                BooksModel.title == 'First Book - Part 1',
+            ).first()
             old_book_id = old_book.book_id
             history_book = UsersBooksHistory(
                 telegram_user_id=telegram_id,
@@ -109,6 +112,62 @@ class TestReadApi:
             sentence = db.query(BooksSentences).filter(BooksSentences.sentence_id == response['sentence_id']).first()
             assert sentence.order == 1
             assert users_history_book_sentence.id == response['history_sentence_id']
+            assert '- part 2' in response['text'].lower()
+
+    def test_get_first_sentence_for_random_read_book(self):
+        with db_session() as db:
+            telegram_user = db.query(Users).order_by(Users.telegram_id.desc()).first()
+            telegram_id = telegram_user.telegram_id
+            users_history_book = db.query(UsersBooksHistory).filter(UsersBooksHistory.telegram_user_id == telegram_id).first()
+            assert users_history_book is None
+            old_book = db.query(BooksModel).filter(
+                BooksModel.level_en_id == telegram_user.level_en_id,
+                BooksModel.title == 'Second Book',
+            ).first()
+            old_book_id = old_book.book_id
+            history_book = UsersBooksHistory(
+                telegram_user_id=telegram_id,
+                book_id=old_book.book_id,
+                start_read=datetime.utcnow(),
+                end_read=datetime.utcnow(),
+            )
+            db.add(history_book)
+            db.commit()
+
+        url = f'{self._url}/{telegram_id}/'
+        response = self._client.get(url=url, headers=self._headers)
+        assert response.status_code == status.HTTP_200_OK
+        response = response.json()['detail']
+
+        with db_session() as db:
+            assert old_book_id != response['book_id']
+            users_history_book = db.query(UsersBooksHistory).filter(
+                UsersBooksHistory.telegram_user_id == telegram_id,
+                UsersBooksHistory.book_id == response['book_id'],
+            ).first()
+            assert users_history_book is not None
+            book_id = users_history_book.book_id
+            assert users_history_book.start_read is not None
+            assert users_history_book.end_read is None
+            users_history_book_sentence = (
+                db.query(UsersBooksSentencesHistory)
+                .filter(
+                    UsersBooksSentencesHistory.telegram_user_id == telegram_id,
+                    UsersBooksSentencesHistory.sentence_id == response['sentence_id'],
+                )
+                .first()
+            )
+            assert users_history_book_sentence is not None
+            assert users_history_book_sentence.is_read is False
+            assert users_history_book_sentence.sentence_id == response['sentence_id']
+            assert users_history_book_sentence.telegram_user_id == telegram_id
+            book = db.query(BooksModel).filter(BooksModel.book_id == book_id).first()
+            assert f'{book.author} - {book.title}' in response['text']
+            assert len(response['words']) <= 5
+            sentence = db.query(BooksSentences).filter(BooksSentences.sentence_id == response['sentence_id']).first()
+            assert sentence.order == 1
+            assert users_history_book_sentence.id == response['history_sentence_id']
+            assert '- part 2' not in response['text'].lower()
 
     def test_get_first_sentence_after_last_sentence(self):
         with db_session() as db:
