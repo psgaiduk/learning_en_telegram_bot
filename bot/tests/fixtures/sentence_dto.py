9e097a2ffcb21f9cdba1a0c437ea4bb3abfb8426
psgaiduk <psgaiduk@gmail.com>
2023-12-24 12:26:10 +0300

    add new fields: order, text with words, sentence times, description time

--- bot/tests/fixtures/sentence_dto.py
+++ bot/tests/fixtures/sentence_dto.py
@@ -7,10 +7,14 @@ from .word_dto import word_new
 @fixture
 def sentence_with_word(word_new):
     return NewSentenceDTOModel(
-            history_sentence_id=1,
-            book_id=1,
-            sentence_id=1,
-            text='test_text',
-            translation={'ru': 'test_text'},
-            words=[word_new],
-        )
+        history_sentence_id=1,
+        book_id=1,
+        order=1,
+        sentence_id=1,
+        text='test_text',
+        text_with_words='test_text_with_word',
+        translation={'ru': 'test_text'},
+        sentence_times='Present Perfect',
+        description_time='Description',
+        words=[word_new],
+    )
