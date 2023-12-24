def92cbb72989bf0bd3c2412da486ffbbcc1ad39
psgaiduk <psgaiduk@gmail.com>
2023-12-24 12:25:40 +0300

    add level en fixture

--- bot/tests/fixtures/telegram_user_dto.py
+++ bot/tests/fixtures/telegram_user_dto.py
@@ -2,10 +2,11 @@ from pytest import fixture
 
 from dto import TelegramUserDTOModel
 from .sentence_dto import sentence_with_word
+from .level_en_dto import level_en
 
 
 @fixture
-def telegram_user_with_sentence_and_word(sentence_with_word):
+def telegram_user_with_sentence_and_word(sentence_with_word, level_en):
     return TelegramUserDTOModel(
         stage='test_stage',
         user_name='test_name',
@@ -13,7 +14,7 @@ def telegram_user_with_sentence_and_word(sentence_with_word):
         previous_stage='test_previous_stage',
         telegram_id=1,
         hero_level=None,
-        level_en=None,
+        level_en=level_en,
         main_language=None,
         new_sentence=sentence_with_word,
     )
