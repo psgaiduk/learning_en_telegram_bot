d2f23aa58b6075554bbb8a5f930e09964db0875a
psgaiduk <psgaiduk@gmail.com>
2023-12-30 08:55:32 +0300

    create test_handle_check_answer_time_mistake_update_sentence

--- bot/tests/test_commands/test_check_answer_time.py
+++ bot/tests/test_commands/test_check_answer_time.py
@@ -88,3 +88,30 @@ class TestCheckAnswerTime:
 
         mock_bot.send_message.assert_not_called()
         mock_delete_message.assert_not_called()
+
+    @mark.parametrize('data', ['right_answer_time', 'wrong_answer_time'])
+    @patch('commands.check_answer_time.delete_message')
+    @patch('commands.check_answer_time.update_data_by_api')
+    @patch('commands.check_answer_time.bot', new_callable=AsyncMock)
+    @mark.asyncio
+    async def test_handle_check_answer_time_mistake_update_sentence(
+            self, mock_bot, mock_update_data, mock_delete_message, data, telegram_user_with_sentence_and_word):
+        chat_id = 1
+        user = User(id=chat_id, is_bot=False, first_name='Test User')
+        mock_callback = CallbackQuery(id=1, chat=chat_id, data=data, from_user=user)
+        mock_callback.from_user = user
+        state = AsyncMock()
+        telegram_user = telegram_user_with_sentence_and_word
+        state.get_data = AsyncMock(return_value={'user': telegram_user})
+        mock_update_data.side_effect = [False]
+
+        await handle_check_answer_time(message=mock_callback, state=state)
+
+        mock_update_data.assert_called_once_with(
+            telegram_id=chat_id,
+            params_for_update={'id': telegram_user.new_sentence.history_sentence_id, 'is_read': True},
+            url_for_update=f'history/sentences/{telegram_user.new_sentence.history_sentence_id}',
+        )
+
+        mock_bot.send_message.assert_not_called()
+        mock_delete_message.assert_not_called()
