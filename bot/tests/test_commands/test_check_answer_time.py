5c13a943f4ed90148b17f1d09263db29b20b0415
psgaiduk <psgaiduk@gmail.com>
2024-01-01 10:01:32 +0300

    update tests for setup method

--- bot/tests/test_commands/test_check_answer_time.py
+++ bot/tests/test_commands/test_check_answer_time.py
@@ -1,5 +1,5 @@
 from unittest.mock import AsyncMock, patch
-from aiogram.types import CallbackQuery, User, ParseMode, ReplyKeyboardMarkup, KeyboardButton, Message
+from aiogram.types import CallbackQuery, User, ParseMode, ReplyKeyboardMarkup, KeyboardButton
 from pytest import mark, fixture
 
 from choices import State
@@ -17,6 +17,8 @@ class TestCheckAnswerTime:
         self._mock_callback = CallbackQuery(id=1, chat=self._chat_id, from_user=self._user)
         self._mock_callback.from_user = self._user
         self._telegram_user = telegram_user_with_sentence_and_word
+        self._state = AsyncMock()
+        self._state.get_data = AsyncMock(return_value={'user': self._telegram_user})
 
     @mark.parametrize('data', ['right_answer_time', 'wrong_answer_time'])
     @patch('commands.check_answer_time.delete_message')
@@ -25,11 +27,9 @@ class TestCheckAnswerTime:
     @mark.asyncio
     async def test_handle_check_answer_time(self, mock_bot, mock_update_data, mock_delete_message, data):
         self._mock_callback.data = data
-        state = AsyncMock()
-        state.get_data = AsyncMock(return_value={'user': self._telegram_user})
         mock_update_data.side_effect = [True, True]
 
-        await handle_check_answer_time(message=self._mock_callback, state=state)
+        await handle_check_answer_time(message=self._mock_callback, state=self._state)
 
         mock_update_data.assert_any_call(
             telegram_id=self._chat_id,
@@ -64,29 +64,21 @@ class TestCheckAnswerTime:
     @patch('commands.check_answer_time.update_data_by_api')
     @patch('commands.check_answer_time.bot', new_callable=AsyncMock)
     @mark.asyncio
-    async def test_handle_check_answer_time_mistake_update_user(
-            self, mock_bot, mock_update_data, mock_delete_message, data, telegram_user_with_sentence_and_word):
-        chat_id = 1
-        user = User(id=chat_id, is_bot=False, first_name='Test User')
-        mock_callback = CallbackQuery(id=1, chat=chat_id, data=data, from_user=user)
-        mock_callback.from_user = user
-        state = AsyncMock()
-        telegram_user = telegram_user_with_sentence_and_word
-        state.get_data = AsyncMock(return_value={'user': telegram_user})
+    async def test_handle_check_answer_time_mistake_update_user(self, mock_bot, mock_update_data, mock_delete_message, data):
         mock_update_data.side_effect = [True, False]
 
-        await handle_check_answer_time(message=mock_callback, state=state)
+        await handle_check_answer_time(message=self._mock_callback, state=self._state)
 
         mock_update_data.assert_any_call(
-            telegram_id=chat_id,
-            params_for_update={'id': telegram_user.new_sentence.history_sentence_id, 'is_read': True},
-            url_for_update=f'history/sentences/{telegram_user.new_sentence.history_sentence_id}',
+            telegram_id=self._chat_id,
+            params_for_update={'id': self._telegram_user.new_sentence.history_sentence_id, 'is_read': True},
+            url_for_update=f'history/sentences/{self._telegram_user.new_sentence.history_sentence_id}',
         )
 
         mock_update_data.assert_any_call(
-            telegram_id=chat_id,
-            params_for_update={'telegram_id': chat_id, 'stage': State.read_book.value},
-            url_for_update=f'telegram_user/{chat_id}',
+            telegram_id=self._chat_id,
+            params_for_update={'telegram_id': self._chat_id, 'stage': State.read_book.value},
+            url_for_update=f'telegram_user/{self._chat_id}',
         )
 
         mock_bot.send_message.assert_not_called()
@@ -97,42 +89,28 @@ class TestCheckAnswerTime:
     @patch('commands.check_answer_time.update_data_by_api')
     @patch('commands.check_answer_time.bot', new_callable=AsyncMock)
     @mark.asyncio
-    async def test_handle_check_answer_time_mistake_update_sentence(
-            self, mock_bot, mock_update_data, mock_delete_message, data, telegram_user_with_sentence_and_word):
-        chat_id = 1
-        user = User(id=chat_id, is_bot=False, first_name='Test User')
-        mock_callback = CallbackQuery(id=1, chat=chat_id, data=data, from_user=user)
-        mock_callback.from_user = user
-        state = AsyncMock()
-        telegram_user = telegram_user_with_sentence_and_word
-        state.get_data = AsyncMock(return_value={'user': telegram_user})
+    async def test_handle_check_answer_time_mistake_update_sentence(self, mock_bot, mock_update_data, mock_delete_message, data):
         mock_update_data.side_effect = [False]
 
-        await handle_check_answer_time(message=mock_callback, state=state)
+        await handle_check_answer_time(message=self._mock_callback, state=self._state)
 
         mock_update_data.assert_called_once_with(
-            telegram_id=chat_id,
-            params_for_update={'id': telegram_user.new_sentence.history_sentence_id, 'is_read': True},
-            url_for_update=f'history/sentences/{telegram_user.new_sentence.history_sentence_id}',
+            telegram_id=self._chat_id,
+            params_for_update={'id': self._telegram_user.new_sentence.history_sentence_id, 'is_read': True},
+            url_for_update=f'history/sentences/{self._telegram_user.new_sentence.history_sentence_id}',
         )
 
         mock_bot.send_message.assert_not_called()
         mock_delete_message.assert_not_called()
 
-    @mark.parametrize('message_type', [CallbackQuery, Message])
     @patch('commands.check_answer_time.bot', new_callable=AsyncMock)
     @mark.asyncio
-    async def test_handle_check_answer_time_other_data(self, mock_bot, message_type):
-        chat_id = 1
-        user = User(id=chat_id, is_bot=False, first_name='Test User')
-        mock_callback = CallbackQuery(id=1, chat=chat_id, from_user=user)
-        mock_callback.from_user = user
-
-        await handle_check_answer_time_other_data(message=mock_callback)
+    async def test_handle_check_answer_time_other_data(self, mock_bot,):
+        await handle_check_answer_time_other_data(message=self._mock_callback)
 
         expected_message_text = 'Нужно нажать по кнопке со временем предложения.'
         mock_bot.send_message.assert_called_once_with(
-            chat_id=chat_id,
+            chat_id=self._chat_id,
             text=expected_message_text,
             parse_mode=ParseMode.HTML,
         )
