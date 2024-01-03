763fa8ff73115d7b9e1f01cf620be030e6b03f82
psgaiduk <psgaiduk@gmail.com>
2024-01-03 11:17:44 +0300

    delete only for new book

--- admin/books/receivers/create_new_book.py
+++ admin/books/receivers/create_new_book.py
@@ -8,5 +8,4 @@ from books.tasks import create_book_task
 
 @receiver(post_save, sender=BooksModel)
 def create_words_and_sentences(sender, instance, created, **kwargs):
-    if created:
-        create_book_task(instance.book_id)
+    create_book_task(instance.book_id)
