3b1b58a23261682cfaa6522b2ff574c77594d62f
pavel <psgaiduk@gmail.com>
2024-01-30 20:27:57 +0700

    Add detailed docstring to create_words_and_sentences receiver

--- admin/books/receivers/create_new_book.py
+++ admin/books/receivers/create_new_book.py
@@ -8,4 +8,15 @@ from books.tasks import create_book_task
 
 @receiver(post_save, sender=BooksModel)
 def create_words_and_sentences(sender, instance, created, **kwargs):
+    """
+    Signal Receiver: post_save, sender=BooksModel
+
+    Method that serves as a signal receiver for the post_save signal. This method is triggered whenever an instance of
+    the BooksModel is saved. It takes in the following parameters:
+
+    - sender: The sender of the signal.
+    - instance: The instance of the BooksModel that was saved.
+    - created: A boolean flag indicating whether the instance was created or updated.
+    - **kwargs: Additional keyword arguments that may be passed along with the signal.
+    """
     create_book_task(instance.book_id)
