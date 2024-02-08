e6ba86c8160c05cc23ab422edeb424892783f608
pavel <psgaiduk@gmail.com>
2024-02-08 20:14:26 +0700

    Update docstring in api_key_required function

--- api/functions/api_key_required.py
+++ api/functions/api_key_required.py
@@ -4,7 +4,12 @@ from settings import settings
 
 
 def api_key_required(request: Request) -> str:
-    """Check API Key."""
+    """
+    Check API Key.
+
+    :param request: Request object.
+    :return: api key.
+    """
     api_key = request.headers.get('X-API-Key')
     if api_key != settings.api_key:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid API Key')
