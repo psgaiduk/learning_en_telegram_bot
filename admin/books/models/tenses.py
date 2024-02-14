5b4f8049bf286449f8fcfde87b0085fd430e94b8
pavel <psgaiduk@gmail.com>
2024-02-14 08:26:24 +0700

    Create tenses

---
+++ admin/books/models/tenses.py
@@ -0,0 +1,16 @@
+from django.db.models import AutoField, CharField, Model, TextField
+
+
+class Tenses(Model):
+    id = AutoField(primary_key=True)
+    name = CharField(max_length=64)
+    short_description = TextField()
+    full_description = TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        db_table = 'tenses'
+        verbose_name = 'tenses'
+        verbose_name_plural = 'tenses'
