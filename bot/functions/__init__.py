8f199e5a5ba058aa709351f637d2468400f4cf02
pavel <psgaiduk@gmail.com>
2024-02-24 08:40:00 +0700

    create function save word history

--- bot/functions/__init__.py
+++ bot/functions/__init__.py
@@ -1,5 +1,6 @@
 from functions.combintation_english_times import get_combinations
 from functions.create_keyboard_for_en_levels import create_keyboard_for_en_levels
 from functions.encryption import encode_telegram_id, decode_telegram_id
+from functions.save_update_word_history import save_word_history
 from functions.send_and_delete_message import delete_message, send_message_and_delete
 from functions.update_data_by_api import update_data_by_api
---
+++ bot/functions/save_update_word_history.py
@@ -0,0 +1,30 @@
+from loguru import logger
+
+from functions import update_data_by_api
+
+
+async def save_word_history(callback_query):
+    is_known = False
+    start_text_message = 'Хорошо'
+    word_id = int(callback_query.data.split('_')[-1])
+    if 'know_word_true' in callback_query.data:
+        is_known = True
+        start_text_message = 'Отлично! Больше мы его тебе не будем показывать. Давай продолжим.\n\n'
+
+    data_for_update_word = {
+        'telegram_user_id': callback_query.from_user.id,
+        'word_id': word_id,
+        'is_known': is_known,
+    }
+
+    logger.debug(f'data_for_update_word: {data_for_update_word}')
+
+    is_update_history = await update_data_by_api(
+        telegram_id=callback_query.from_user.id,
+        params_for_update=data_for_update_word,
+        url_for_update=f'history/words',
+    )
+
+    logger.debug(f'is_update_history: {is_update_history}')
+
+    return is_update_history, start_text_message
