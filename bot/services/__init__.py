bd7ed5dc99be49fd56fab1ff65816a1c3b64a613
pavel <psgaiduk@gmail.com>
2024-02-26 08:35:43 +0700

    create read sentence service

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -1,4 +1,5 @@
 from services.check_words import CheckWordsService
+from services.read_sentence import ReadSentenceService
 from services.registration import RegistrationService
 from services.update_profile import UpdateProfileService
 from services.wait_en_level import WaitEnLevelService
---
+++ bot/services/read_sentence.py
@@ -0,0 +1,167 @@
+from os import path
+from random import choices, randint
+from typing import Union
+
+from aiogram.types import (
+    CallbackQuery,
+    Message,
+    ParseMode,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+from aiogram.dispatcher.storage import FSMContext
+
+from bot import bot
+from choices import State
+from dto import TelegramUserDTOModel
+from functions import get_combinations, delete_message, save_word_history, update_data_by_api
+
+
+class ReadSentenceService:
+    """Service for reading sentence."""
+
+    _telegram_user: TelegramUserDTOModel
+    _sentence_text: str
+    _sentence_translation: str
+    _message_text: str
+
+    def __init__(self, message: Union[CallbackQuery, Message], state: FSMContext) -> None:
+        """Init."""
+        self._message = message
+        self._state = state
+
+    async def do(self) -> None:
+
+        if isinstance(self._message, CallbackQuery):
+            await save_word_history(callback_query=self._message)
+
+        await self._get_telegram_user()
+        await self._get_sentence()
+        await self._create_keyboard()
+        await self._create_file_path()
+        await self._create_message_text()
+        await self._send_message_or_tenses()
+
+        self._telegram_user.new_sentence.text = ''
+        await self._state.set_data(data={'user': self._telegram_user})
+
+    async def _get_telegram_user(self) -> None:
+        data = await self._state.get_data()
+        self._telegram_user = data['user']
+
+    async def _get_sentence(self) -> None:
+        self._sentence_text = self._telegram_user.new_sentence.text
+        if self._telegram_user.level_en.order < 3:
+            self._sentence_text = self._telegram_user.new_sentence.text_with_words
+
+        self._sentence_translation = self._telegram_user.new_sentence.translation.get('ru')
+
+    async def _create_keyboard(self) -> None:
+        self._keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+        self._keyboard.add(KeyboardButton(text='Read'))
+
+    async def _create_file_path(self) -> None:
+        file_name = f'{self._telegram_user.new_sentence.book_id} - {self._telegram_user.new_sentence.order}'
+        self._file_path = f'static/audio/{file_name}.mp3'
+
+    async def _create_message_text(self) -> None:
+        if randint(1, 3) == 1 and path.isfile(self._file_path):
+            await self._send_audio_message()
+        else:
+            self._message_text = f'{self._sentence_text}\n\n<tg-spoiler>{self._sentence_translation}</tg-spoiler>'
+
+    async def _send_message_or_tenses(self) -> None:
+        if randint(1, 6) == 1:
+            await self._send_tenses()
+        else:
+            await self._send_message()
+
+    async def _send_audio_message(self) -> None:
+        with open(self._file_path, 'rb') as audio:
+            self._message_text = f'Translate:\n\n<tg-spoiler>{self._sentence_translation}</tg-spoiler>'
+            sentence_text = f'Text:\n\n<tg-spoiler>{self._sentence_text}</tg-spoiler>'
+
+            await bot.send_audio(
+                chat_id=self._telegram_user.telegram_id,
+                audio=audio,
+                caption=sentence_text,
+                parse_mode=ParseMode.HTML,
+                reply_markup=self._keyboard,
+            )
+
+    async def _send_tenses(self) -> None:
+        is_update = await self._update_stage_user()
+        if is_update is False:
+            return
+
+        await self._send_text_with_tenses()
+
+    async def _send_message(self) -> None:
+        is_update_sentence = await self._update_history_sentence()
+
+        if is_update_sentence is False:
+            return
+
+        await delete_message(message=self._message)
+
+        await bot.send_message(
+            chat_id=self._telegram_user.telegram_id,
+            text=self._message_text,
+            parse_mode=ParseMode.HTML,
+            reply_markup=self._keyboard,
+        )
+
+    async def _send_text_with_tenses(self) -> None:
+        await bot.send_message(
+            chat_id=self._telegram_user.telegram_id,
+            text=self._message_text,
+            parse_mode=ParseMode.HTML,
+            reply_markup=ReplyKeyboardRemove(),
+        )
+        message_text = 'К какому времени относится предложение?'
+        right_answer = self._telegram_user.new_sentence.sentence_times
+        count_times_in_sentence = right_answer.count(',') + 1
+        all_english_times = get_combinations(count_times_in_sentence)
+        all_answers = [right_answer]
+        other_answers = choices(all_english_times, k=3)
+        all_answers.extend(other_answers)
+
+        sorted(all_answers, key=lambda x: randint(1, 100))
+        keyboard = InlineKeyboardMarkup()
+        for answer in all_answers:
+            callback_data = 'wrong_answer_time' if answer != right_answer else 'right_answer_time'
+            keyboard.add(InlineKeyboardButton(text=answer, callback_data=callback_data))
+
+        await bot.send_message(
+            chat_id=self._telegram_user.telegram_id,
+            text=message_text,
+            parse_mode=ParseMode.HTML,
+            reply_markup=keyboard,
+        )
+
+    async def _update_stage_user(self) -> bool:
+        params_for_update_user = {
+            'telegram_id': self._message.from_user.id,
+            'stage': State.check_answer_time.value,
+        }
+
+        return await update_data_by_api(
+            telegram_id=self._message.from_user.id,
+            params_for_update=params_for_update_user,
+            url_for_update=f'telegram_user/{self._message.from_user.id}',
+        )
+
+    async def _update_history_sentence(self) -> bool:
+        data_for_update_history_sentence = {
+            'id': self._telegram_user.new_sentence.history_sentence_id,
+            'is_read': True,
+        }
+
+        return await update_data_by_api(
+            telegram_id=self._telegram_user.telegram_id,
+            params_for_update=data_for_update_history_sentence,
+            url_for_update=f'history/sentences/{self._telegram_user.new_sentence.history_sentence_id}',
+        )
