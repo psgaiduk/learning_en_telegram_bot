736210c567bcf0171c03bb6de4a8e770f89089bc
pavel <psgaiduk@gmail.com>
2024-02-27 08:35:19 +0700

    create test do with message for read sentence service

--- bot/tests/test_services/test_read_sentences.py
+++ bot/tests/test_services/test_read_sentences.py
@@ -23,6 +23,7 @@ class TestReadSentenceService:
     @patch('services.read_sentence.save_word_history')
     async def test_do_callback(self, mock_save_word_history):
         user = User(id=self._chat_id, is_bot=False, first_name='Test User')
+        # message = Message(id=1, chat=self._chat_id, text='Read', from_user=user)
         mock_callback = CallbackQuery(id=1, chat=self._chat_id, data='test_data', from_user=user)
         mock_callback.from_user = user
         service = ReadSentenceService(message=mock_callback, state=self._state)
@@ -57,4 +58,43 @@ class TestReadSentenceService:
         mock_send_message_or_tenses.assert_called_once()
 
         mock_save_word_history.assert_called_once_with(callback_query=mock_callback)
+        assert service._telegram_user.new_sentence.text == ''
+
+    @mark.asyncio
+    @patch('services.read_sentence.save_word_history')
+    async def test_do_message(self, mock_save_word_history):
+        user = User(id=self._chat_id, is_bot=False, first_name='Test User')
+        message = Message(id=1, chat=self._chat_id, text='Read', from_user=user)
+        service = ReadSentenceService(message=message, state=self._state)
+
+        mock_get_telegram_user = AsyncMock(return_value=None)
+        service._get_telegram_user = mock_get_telegram_user
+        service._telegram_user = deepcopy(self._telegram_user)
+
+        mock_get_sentence = AsyncMock(return_value=None)
+        service._get_sentence = mock_get_sentence
+
+        mock_create_keyboard = AsyncMock(return_value=None)
+        service._create_keyboard = mock_create_keyboard
+
+        mock_create_path_file = AsyncMock(return_value=None)
+        service._create_file_path = mock_create_path_file
+
+        mock_create_message_text = AsyncMock(return_value=None)
+        service._create_message_text = mock_create_message_text
+
+        mock_send_message_or_tenses = AsyncMock(return_value=None)
+        service._send_message_or_tenses = mock_send_message_or_tenses
+
+        assert service._telegram_user.new_sentence.text != ''
+        await service.do()
+
+        mock_get_telegram_user.assert_called_once()
+        mock_get_sentence.assert_called_once()
+        mock_create_keyboard.assert_called_once()
+        mock_create_path_file.assert_called_once()
+        mock_create_message_text.assert_called_once()
+        mock_send_message_or_tenses.assert_called_once()
+
+        mock_save_word_history.assert_not_called()
         assert service._telegram_user.new_sentence.text == ''
