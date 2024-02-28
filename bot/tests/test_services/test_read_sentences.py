8f79e1f7fc5f8995c2e4cbd4beca1edf43654a59
pavel <psgaiduk@gmail.com>
2024-02-28 08:34:42 +0700

    work with self._service

--- bot/tests/test_services/test_read_sentences.py
+++ bot/tests/test_services/test_read_sentences.py
@@ -18,6 +18,9 @@ class TestReadSentenceService:
         self._state = AsyncMock()
         self._telegram_user = telegram_user_with_sentence_and_word
         self._chat_id = self._telegram_user.telegram_id
+        user = User(id=self._chat_id, is_bot=False, first_name='Test User')
+        message = Message(id=1, chat=self._chat_id, text='Read', from_user=user)
+        self._service = ReadSentenceService(message=message, state=self._state)
 
     @mark.asyncio
     @patch('services.read_sentence.save_word_history')
@@ -62,31 +65,27 @@ class TestReadSentenceService:
     @mark.asyncio
     @patch('services.read_sentence.save_word_history')
     async def test_do_message(self, mock_save_word_history):
-        user = User(id=self._chat_id, is_bot=False, first_name='Test User')
-        message = Message(id=1, chat=self._chat_id, text='Read', from_user=user)
-        service = ReadSentenceService(message=message, state=self._state)
-
         mock_get_telegram_user = AsyncMock(return_value=None)
-        service._get_telegram_user = mock_get_telegram_user
-        service._telegram_user = deepcopy(self._telegram_user)
+        self._service._get_telegram_user = mock_get_telegram_user
+        self._service._telegram_user = deepcopy(self._telegram_user)
 
         mock_get_sentence = AsyncMock(return_value=None)
-        service._get_sentence = mock_get_sentence
+        self._service._get_sentence = mock_get_sentence
 
         mock_create_keyboard = AsyncMock(return_value=None)
-        service._create_keyboard = mock_create_keyboard
+        self._service._create_keyboard = mock_create_keyboard
 
         mock_create_path_file = AsyncMock(return_value=None)
-        service._create_file_path = mock_create_path_file
+        self._service._create_file_path = mock_create_path_file
 
         mock_create_message_text = AsyncMock(return_value=None)
-        service._create_message_text = mock_create_message_text
+        self._service._create_message_text = mock_create_message_text
 
         mock_send_message_or_tenses = AsyncMock(return_value=None)
-        service._send_message_or_tenses = mock_send_message_or_tenses
+        self._service._send_message_or_tenses = mock_send_message_or_tenses
 
-        assert service._telegram_user.new_sentence.text != ''
-        await service.do()
+        assert self._service._telegram_user.new_sentence.text != ''
+        await self._service.do()
 
         mock_get_telegram_user.assert_called_once()
         mock_get_sentence.assert_called_once()
@@ -96,16 +95,14 @@ class TestReadSentenceService:
         mock_send_message_or_tenses.assert_called_once()
 
         mock_save_word_history.assert_not_called()
-        assert service._telegram_user.new_sentence.text == ''
+        assert self._service._telegram_user.new_sentence.text == ''
 
     @mark.asyncio
     async def test_get_user(self):
-        user = User(id=self._chat_id, is_bot=False, first_name='Test User')
-        message = Message(id=1, chat=self._chat_id, text='Read', from_user=user)
-        service = ReadSentenceService(message=message, state=self._state)
 
         self._state.get_data = AsyncMock(return_value={'user': self._telegram_user})
 
-        await service._get_telegram_user()
+        await self._service._get_telegram_user()
 
-        assert service._telegram_user == self._telegram_user
+        assert self._service._telegram_user == self._telegram_user
+
