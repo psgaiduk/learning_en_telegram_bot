1e8bfc109e692ce66dfb0b77bf5a0cf6ee445175
pavel <psgaiduk@gmail.com>
2024-03-18 10:00:29 +0700

    create enum type word id

--- admin/books/choices/__init__.py
+++ admin/books/choices/__init__.py
@@ -1,1 +1,2 @@
+from books.choices.type_word_id import TypeWordId
 from books.choices.type_words import TypeWord
---
+++ admin/books/choices/type_word_id.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class TypeWordId(Enum):
+    """Id of words."""
+
+    word = 1
+    phrase_verb = 2
+    idiomatic_expression = 3
