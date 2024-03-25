db6784355cf8b42d62a082f34742c2fb9f1e3406
pavel <psgaiduk@gmail.com>
2024-03-25 18:44:31 +0700

    add anki fields for model dto

--- api/dto/models/history_words.py
+++ api/dto/models/history_words.py
@@ -17,6 +17,8 @@ class HistoryWordModelDTO(BaseModel):
     correct_answers: Optional[int]
     incorrect_answers: Optional[int]
     correct_answers_in_row: Optional[int]
+    increase_factor: Optional[float]
+    interval_repeat: Optional[int]
     translation: Optional[dict]
     created_at: Optional[datetime]
     updated_at: Optional[datetime]
