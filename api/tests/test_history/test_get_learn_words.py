d8cd101d56f6c66e372c50db8a95a5cf8620f7f1
pavel <psgaiduk@gmail.com>
2024-04-07 16:07:04 +0400

    test: create test_get_learn_words

---
+++ api/tests/test_history/test_get_learn_words.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from fastapi.testclient import TestClient
+from fastapi import status
+from pytest import mark
+
+from main import app
+from models import UsersWordsHistory
+from settings import settings
+from tests.connect_db import db_session
+
+from tests.fixtures import *
+
+
+@mark.usefixtures('create_test_database')
+class TestGetLearnWordsAPI:
+
+    @classmethod
+    def setup_class(cls):
+        cls._headers = {'X-API-Key': settings.api_key}
+        cls._client = TestClient(app)
+        cls._url = '/api/v1/history/learn-words'
+
+    def test_get_learn_words(self, history_word_mock):
+        with db_session() as db:
+            history_word = db.query(UsersWordsHistory).first()
+            history_word.is_known = True
+            db.commit()
+            telegram_id = history_word.telegram_user_id
+
+        url = f'{self._url}/{telegram_id}/'
+        response = self._client.get(url=url, headers=self._headers)
+        assert response.status_code == status.HTTP_200_OK
+        response = response.json()
+
+        assert len(response) == 1
+        assert response[0]['repeat_datetime'] < datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
