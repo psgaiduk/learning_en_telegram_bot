4466d6cab3f7eb57aeb0a8a0d70e12e605ae7ff4
pavel <psgaiduk@gmail.com>
2024-04-08 09:37:03 +0400

    test: create test_not_get_learn_words_with_is_false_known for TestGetLearnWordsAPI

--- api/tests/test_history/test_get_learn_words.py
+++ api/tests/test_history/test_get_learn_words.py
@@ -50,3 +50,17 @@ class TestGetLearnWordsAPI:
         response = response.json()
 
         assert len(response) == 0
+
+    def test_not_get_learn_words_with_is_false_known(self, history_word_mock):
+        with db_session() as db:
+            history_word: UsersWordsHistory = db.query(UsersWordsHistory).first()
+            assert history_word
+            assert history_word.is_known is False
+            telegram_id = history_word.telegram_user_id
+
+        url = f'{self._url}/{telegram_id}/'
+        response = self._client.get(url=url, headers=self._headers)
+        assert response.status_code == status.HTTP_200_OK
+        response = response.json()
+
+        assert len(response) == 0
