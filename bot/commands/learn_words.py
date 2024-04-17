5f68ba07a5d4aa825c55ff257ab83b1ef9bf1135
pavel <psgaiduk@gmail.com>
2024-04-17 09:34:54 +0400

    feat(bot): update work handle learn words with service LearnWordsService

--- bot/commands/learn_words.py
+++ bot/commands/learn_words.py
@@ -1,14 +1,13 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.storage import FSMContext
 
-from bot import bot, dispatcher
+from bot import dispatcher
 from choices import State
+from services import LearnWordsService
 
 
 @dispatcher.message_handler(Text(equals='Read'), state=State.learn_words.value)
-async def handle_learn_words(message: types.Message) -> None:
+async def handle_learn_words(message: types.Message, state: FSMContext) -> None:
     """Handle learn words."""
-    await bot.send_message(
-        chat_id=message.from_user.id,
-        text='learn words',
-    )
+    await LearnWordsService(message=message, state=state).do()
