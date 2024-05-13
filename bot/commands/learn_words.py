1027dd695b992c3c1a0149723eb23695f0533584
pavel <psgaiduk@gmail.com>
2024-05-13 10:36:37 +0700

    feat(bot): create handle for error data on state learn_words

--- bot/commands/learn_words.py
+++ bot/commands/learn_words.py
@@ -1,7 +1,10 @@
+from typing import Union
+
 from aiogram import types
+from aiogram.types import CallbackQuery, Message
 from aiogram.dispatcher.storage import FSMContext
 
-from bot import dispatcher
+from bot import bot, dispatcher
 from choices import State
 from services import LearnWordsService
 
@@ -10,3 +13,11 @@ from services import LearnWordsService
 async def handle_learn_words(message: types.Message, state: FSMContext) -> None:
     """Handle learn words."""
     await LearnWordsService(message=message, state=state).do()
+
+
+@dispatcher.message_handler(state=State.learn_words.value)
+@dispatcher.callback_query_handler(state=State.learn_words.value)
+async def handle_error_learn_words(message_data: Union[CallbackQuery, Message]) -> None:
+    """Handle learn words for other data."""
+    message_text = 'Нужно нажать по кнопке I remember или I don\'t remember'
+    await bot.send_message(chat_id=message_data.from_user.id, text=message_text)
