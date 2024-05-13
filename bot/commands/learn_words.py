a8ddfe78e7dc66481e156b2738b704207fbe6b58
pavel <psgaiduk@gmail.com>
2024-05-13 10:33:18 +0700

    feat(bot): update dispatcher for handle_learn_words

--- bot/commands/learn_words.py
+++ bot/commands/learn_words.py
@@ -1,5 +1,4 @@
 from aiogram import types
-from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.storage import FSMContext
 
 from bot import dispatcher
@@ -7,7 +6,7 @@ from choices import State
 from services import LearnWordsService
 
 
-@dispatcher.message_handler(Text(equals='Read'), state=State.learn_words.value)
+@dispatcher.callback_query_handler(lambda c: c.data and c.data.startswith('learn_word_'), state=State.learn_words.value)
 async def handle_learn_words(message: types.Message, state: FSMContext) -> None:
     """Handle learn words."""
     await LearnWordsService(message=message, state=state).do()
