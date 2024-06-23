20d3c761f463dbe8c38edabfa29c96d045f69b54
psgaiduk <psgaiduk@gmail.com>
2024-06-23 09:34:43 +0400

    refactoring(bot): delete intend in word_new test_fixture

--- bot/tests/fixtures/word_dto.py
+++ bot/tests/fixtures/word_dto.py
@@ -8,16 +8,16 @@ from dto import WordDTOModel
 @fixture
 def word_new():
     return WordDTOModel(
-            word_id=1,
-            word='test',
-            type_word_id=1,
-            translation={'ru': 'тест'},
-            is_known=False,
-            count_view=0,
-            correct_answers=0,
-            incorrect_answers=0,
-            correct_answers_in_row=0,
-            increase_factor=2.0,
-            interval_repeat=600,
-            repeat_datetime=datetime.utcnow(),
-        )
+        word_id=1,
+        word='test',
+        type_word_id=1,
+        translation={'ru': 'тест'},
+        is_known=False,
+        count_view=0,
+        correct_answers=0,
+        incorrect_answers=0,
+        correct_answers_in_row=0,
+        increase_factor=2.0,
+        interval_repeat=600,
+        repeat_datetime=datetime.utcnow(),
+    )
