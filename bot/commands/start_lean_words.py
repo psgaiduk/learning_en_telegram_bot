3baec1f25ccfcf45af80c0fabc25f6d43edc0745
psgaiduk <psgaiduk@gmail.com>
2024-06-25 09:54:11 +0400

    refactoring(bot): use function send_message_learn_word in handle_start_lean_words

--- bot/commands/start_lean_words.py
+++ bot/commands/start_lean_words.py
@@ -4,9 +4,6 @@ from aiogram.types import (
     CallbackQuery,
     Message,
     ParseMode,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton
-
 )
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.storage import FSMContext
@@ -15,7 +12,7 @@ from loguru import logger
 from bot import bot, dispatcher
 from dto import TelegramUserDTOModel
 from choices import State
-from functions import update_data_by_api
+from functions import send_message_learn_word, update_data_by_api
 
 
 @dispatcher.message_handler(Text(equals='Read'), state=State.start_learn_words.value)
@@ -38,9 +35,6 @@ async def handle_start_lean_words(message: Message, state: FSMContext) -> None:
         url_for_update=f'telegram_user/{message.from_user.id}',
     )
     logger.debug(f'update user = {is_update}')
-    inline_keyboard = None
-    message_text = '🤖 Что-то пошло не так. Попробуйте еще раз, чуть позже.'
-
     if is_update:
         logger.debug('Всё хорошо, можно отправлять сообщения')
         message_start_learn_word = 'Прежде чем продолжить повторим слова.'
@@ -53,19 +47,13 @@ async def handle_start_lean_words(message: Message, state: FSMContext) -> None:
 
         first_word = telegram_user.learn_words[0]
         logger.debug(f'Получили первое слово = {first_word}')
-        message_text = (
-            f'Помните перевод слова: {first_word.word}\n'
-            f'Перевод: <tg-spoiler>{first_word.translation}</tg-spoiler>'
-        )
-        inline_keyboard = InlineKeyboardMarkup()
-        inline_keyboard.add(InlineKeyboardButton(text='I remember', callback_data='learn_word_yes'))
-        inline_keyboard.add(InlineKeyboardButton(text='I don\'t remember', callback_data='learn_word_no'))
+        return await send_message_learn_word(word=first_word, telegram_id=telegram_user.telegram_id)
 
+    message_text = '🤖 Что-то пошло не так. Попробуйте еще раз, чуть позже.'
     await bot.send_message(
         chat_id=telegram_user.telegram_id,
         text=message_text,
         parse_mode=ParseMode.HTML,
-        reply_markup=inline_keyboard,
     )
 
 
