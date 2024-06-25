b175485b97e36962d205c8880b7b0d2619898c16
psgaiduk <psgaiduk@gmail.com>
2024-06-25 09:49:44 +0400

    feat(bot): create function send_message_learn_word

--- bot/functions/__init__.py
+++ bot/functions/__init__.py
@@ -3,4 +3,5 @@ from functions.create_keyboard_for_en_levels import create_keyboard_for_en_level
 from functions.encryption import encode_telegram_id, decode_telegram_id
 from functions.save_update_word_history import save_word_history
 from functions.send_and_delete_message import delete_message, send_message_and_delete
+from functions.send_message_learn_word import send_message_learn_word
 from functions.update_data_by_api import update_data_by_api
---
+++ bot/functions/send_message_learn_word.py
@@ -0,0 +1,36 @@
+from aiogram.types import (
+    ParseMode,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+
+)
+from loguru import logger
+
+from bot import bot
+from dto import WordDTOModel
+
+
+async def send_message_learn_word(word: WordDTOModel, telegram_id: int) -> None:
+    """
+    Send message for learn word.
+
+    :params word: word by WordDTOModel.
+    :params telegram_id: users telegram id.
+    """
+    message_text = (
+        f'Помните перевод слова: {word.word}\n'
+        f'Перевод: <tg-spoiler>{word.translation}</tg-spoiler>'
+    )
+    logger.debug(f'message text = {message_text}')
+
+    inline_keyboard = InlineKeyboardMarkup()
+    inline_keyboard.add(InlineKeyboardButton(text='I remember', callback_data='learn_word_yes'))
+    inline_keyboard.add(InlineKeyboardButton(text='I don\'t remember', callback_data='learn_word_no'))
+    logger.debug(f'keyboard = {inline_keyboard}')
+
+    await bot.send_message(
+        chat_id=telegram_id,
+        text=message_text,
+        parse_mode=ParseMode.HTML,
+        reply_markup=inline_keyboard,
+    )
