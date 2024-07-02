1b9f1bff4df0a3188b7c6287eb5b1fdd4ca195d5
psgaiduk <psgaiduk@gmail.com>
2024-07-02 09:16:31 +0400

    feat(api): add anki fields in  UpdateHistoryWordDTO

--- api/dto/requests/history_words/update_history_word.py
+++ api/dto/requests/history_words/update_history_word.py
@@ -14,5 +14,8 @@ class UpdateHistoryWordDTO(BaseModel):
     correct_answers: Optional[int]
     incorrect_answers: Optional[int]
     correct_answers_in_row: Optional[int]
+    increase_factor: Optional[float]
+    interval_repeat: Optional[int]
+    repeat_datetime: Optional[datetime]
     created_at: Optional[datetime]
     updated_at: Optional[datetime]
