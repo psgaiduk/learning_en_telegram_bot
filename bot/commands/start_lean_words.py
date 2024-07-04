628dec88452067577ea8ef9022bc1683b1dea9ff
psgaiduk <psgaiduk@gmail.com>
2024-07-04 19:16:12 +0400

    feat(bot): add translate to handle_error_start_learn_words

--- bot/commands/start_lean_words.py
+++ bot/commands/start_lean_words.py
@@ -61,5 +61,5 @@ async def handle_start_lean_words(message: Message, state: FSMContext) -> None:
 @dispatcher.callback_query_handler(state=State.start_learn_words.value)
 async def handle_error_start_learn_words(message_data: Union[CallbackQuery, Message]) -> None:
     """Handle start learn words for other data."""
-    message_text = 'Нужно нажать по кнопке I remember или I don\'t remember'
+    message_text = 'Нужно нажать по кнопке "Помню" или "Не помню"'
     await bot.send_message(chat_id=message_data.from_user.id, text=message_text)
