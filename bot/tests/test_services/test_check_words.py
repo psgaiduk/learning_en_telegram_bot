c8f8303e4dbeefd70916fd81d5a7e32af1cf484d
pavel <psgaiduk@gmail.com>
2024-07-18 09:17:12 +0400

    #69 test: update test for check work in TestCheckWordsService

--- bot/tests/test_services/test_check_words.py
+++ bot/tests/test_services/test_check_words.py
@@ -21,7 +21,7 @@ class TestCheckWordsService:
 
     @mark.asyncio
     async def test_do_all_true(self):
-        service = CheckWordsService(state=self._state, start_text_message='')
+        service = CheckWordsService(state=self._state, start_text_message="")
 
         mock_get_user = AsyncMock(return_value=None)
         service._get_user = mock_get_user
@@ -48,7 +48,7 @@ class TestCheckWordsService:
 
     @mark.asyncio
     async def test_do_update_user_false(self):
-        service = CheckWordsService(state=self._state, start_text_message='')
+        service = CheckWordsService(state=self._state, start_text_message="")
 
         mock_get_user = AsyncMock(return_value=None)
         service._get_user = mock_get_user
@@ -72,7 +72,7 @@ class TestCheckWordsService:
 
     @mark.asyncio
     async def test_do_update_sentence_false(self):
-        service = CheckWordsService(state=self._state, start_text_message='')
+        service = CheckWordsService(state=self._state, start_text_message="")
 
         mock_get_user = AsyncMock(return_value=None)
         service._get_user = mock_get_user
@@ -96,19 +96,19 @@ class TestCheckWordsService:
 
     @mark.asyncio
     async def test_get_user(self):
-        service = CheckWordsService(state=self._state, start_text_message='')
+        service = CheckWordsService(state=self._state, start_text_message="")
 
-        self._state.get_data = AsyncMock(return_value={'user': self._telegram_user})
+        self._state.get_data = AsyncMock(return_value={"user": self._telegram_user})
 
         await service._get_user()
 
         assert service._telegram_user == self._telegram_user
 
-    @mark.parametrize('update_status', [True, False])
+    @mark.parametrize("update_status", [True, False])
     @mark.asyncio
-    @patch('services.check_words.update_data_by_api', new_callable=AsyncMock)
+    @patch("services.check_words.update_data_by_api", new_callable=AsyncMock)
     async def test_update_user(self, mock_update_user, update_status):
-        service = CheckWordsService(state=self._state, start_text_message='')
+        service = CheckWordsService(state=self._state, start_text_message="")
         service._telegram_user = deepcopy(self._telegram_user)
 
         mock_update_user.side_effect = [update_status]
@@ -118,19 +118,19 @@ class TestCheckWordsService:
         mock_update_user.assert_called_once_with(
             telegram_id=self._telegram_user.telegram_id,
             params_for_update={
-                'telegram_id': self._telegram_user.telegram_id,
-                'stage': State.read_book.value,
+                "telegram_id": self._telegram_user.telegram_id,
+                "stage": State.read_book.value,
             },
-            url_for_update=f'telegram_user/{self._telegram_user.telegram_id}',
+            url_for_update=f"telegram_user/{self._telegram_user.telegram_id}",
         )
 
         assert return_value is update_status
 
-    @mark.parametrize('update_status', [True, False])
+    @mark.parametrize("update_status", [True, False])
     @mark.asyncio
-    @patch('services.check_words.update_data_by_api', new_callable=AsyncMock)
+    @patch("services.check_words.update_data_by_api", new_callable=AsyncMock)
     async def test_update_sentence(self, mock_update_user, update_status):
-        service = CheckWordsService(state=self._state, start_text_message='')
+        service = CheckWordsService(state=self._state, start_text_message="")
         service._telegram_user = deepcopy(self._telegram_user)
         service._words = deepcopy(self._telegram_user.new_sentence.words)
 
@@ -143,27 +143,29 @@ class TestCheckWordsService:
         mock_update_user.assert_called_once_with(
             telegram_id=self._telegram_user.telegram_id,
             params_for_update={
-                'id': self._telegram_user.new_sentence.history_sentence_id,
-                'check_words': words_ids,
+                "id": self._telegram_user.new_sentence.history_sentence_id,
+                "check_words": words_ids,
             },
-            url_for_update=f'history/sentences/{self._telegram_user.new_sentence.history_sentence_id}',
+            url_for_update=f"history/sentences/{self._telegram_user.new_sentence.history_sentence_id}",
         )
 
         assert return_value is update_status
 
-    @mark.parametrize('start_message_text', ['', 'Text', 'Another text'])
+    @mark.parametrize("start_message_text", ["", "Text", "Another text"])
     @mark.asyncio
     async def test_send_message(self, start_message_text):
-        service = CheckWordsService(state=self._state, start_text_message='')
+        service = CheckWordsService(state=self._state, start_text_message="")
         service._telegram_user = deepcopy(self._telegram_user)
         word = deepcopy(self._telegram_user.new_sentence.words)[0]
         service._first_word = word
         service._start_text_message = start_message_text
 
-        with patch.object(bot, 'send_message', new=AsyncMock()) as mock_send_message:
+        with patch.object(bot, "send_message", new=AsyncMock()) as mock_send_message:
             await service._send_message()
 
-            excepted_text = f'{start_message_text}Слово: {word.word}\nПеревод: {word.translation["ru"]}'
+            excepted_text = (
+                f'{start_message_text}\nТы знаешь это слово?\n\nСлово: {word.word}\nПеревод: {word.translation["ru"]}'
+            )
 
             mock_send_message.assert_called_once_with(
                 chat_id=self._chat_id,
@@ -172,10 +174,10 @@ class TestCheckWordsService:
             )
 
             reply_markup_call = mock_send_message.call_args_list[0]
-            reply_markup = reply_markup_call.kwargs['reply_markup']
+            reply_markup = reply_markup_call.kwargs["reply_markup"]
 
             assert isinstance(reply_markup, InlineKeyboardMarkup)
             assert reply_markup.inline_keyboard == [
-                [InlineKeyboardButton(text='I know', callback_data=f'know_word_true_{word.word_id}')],
-                [InlineKeyboardButton(text='I don\'t know', callback_data=f'know_word_false_{word.word_id}')],
+                [InlineKeyboardButton(text="Знаю", callback_data=f"know_word_true_{word.word_id}")],
+                [InlineKeyboardButton(text="Не знаю", callback_data=f"know_word_false_{word.word_id}")],
             ]
