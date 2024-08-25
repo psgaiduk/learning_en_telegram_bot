b43c7d79b70e372f460de70624feff74c75bd54c
psgaiduk <psgaiduk@gmail.com>
2024-08-25 10:37:05 +0400

    #84 feat: update logic get new sentence

--- api/api/v1/read.py
+++ api/api/v1/read.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from loguru import logger
-from sqlalchemy import func, not_, select
-from sqlalchemy.orm import Session, aliased, joinedload, contains_eager
+from sqlalchemy import Boolean, and_, func, select
+from sqlalchemy.orm import Session, contains_eager, joinedload, subqueryload
 
 from database import get_db
 from dto.models import HistoryWordModelForReadDTO, SentenceModelForReadDTO
@@ -61,22 +61,90 @@ class ReadBookService:
         self._telegram_id = telegram_id
         self._db = db
         self._is_new_sentence = True
+        self._title_book = ''
 
     async def work(self) -> SentenceModelForReadDTO:
         """Start work."""
         await self._get_user()
         await self._check_count_read_sentences_today()
-
-        self._start_read_book = self._db.query(UsersBooksHistory).filter(
-            UsersBooksHistory.telegram_user_id == self._telegram_id,
-            UsersBooksHistory.end_read.is_(None),
-        ).first()
-
-        if not self._start_read_book:
-            await self._get_first_sentence_from_book()
-        else:
-            self._title_book = f'{self._start_read_book.book.author} - {self._start_read_book.book.title}'
-            await self._get_next_sentence()
+        
+        need_sentence = (
+            self._db.query(BooksSentences)
+            .join(UsersBooksHistory, BooksSentences.book_id == UsersBooksHistory.book_id)
+            .outerjoin(UsersBooksSentencesHistory, 
+                    and_(
+                        BooksSentences.sentence_id == UsersBooksSentencesHistory.sentence_id, 
+                        UsersBooksSentencesHistory.created_at >= UsersBooksHistory.start_read
+                    ))
+            .filter(UsersBooksHistory.telegram_user_id == self._telegram_id)
+            .filter(UsersBooksHistory.end_read == None)
+            .filter(UsersBooksSentencesHistory.created_at == None)
+            .order_by(BooksSentences.order)
+            .options(subqueryload(BooksSentences.tenses))
+        )
+        logger.debug(f'query get sentence for started book = {need_sentence}')
+        self._need_sentence = need_sentence.first()
+        logger.debug(f'need sentence from started book = {self._need_sentence}')
+        
+        if not self._need_sentence:
+            subquery1 = (
+                self._db.query(
+                    BooksModel.book_id.label('book_id'), 
+                    BooksModel.title.label('title'),
+                    BooksModel.author.label('author'),
+                    func.cast(False, Boolean).label('repeat'))
+                .outerjoin(
+                    UsersBooksHistory,
+                    (UsersBooksHistory.book_id == BooksModel.book_id) &
+                    (UsersBooksHistory.telegram_user_id == self._telegram_id))
+                .filter(UsersBooksHistory.start_read.is_(None))
+                .limit(1)
+            )
+            
+            subquery2 = (
+                self._db.query(
+                    BooksModel.book_id.label('book_id'),
+                    BooksModel.title.label('title'),
+                    BooksModel.author.label('author'),
+                    func.cast(True, Boolean).label('repeat'))
+                .order_by(func.random())
+                .limit(1)
+            )
+            
+            union_subquery = subquery1.union_all(subquery2).limit(1).subquery()
+            
+            need_sentence = (
+                self._db.query(BooksSentences)
+                .join(union_subquery, BooksSentences.book_id == union_subquery.c.book_id)
+                .filter(BooksSentences.order == 1)
+                .options(subqueryload(BooksSentences.tenses))
+                .add_columns(
+                    union_subquery.c.title,
+                    union_subquery.c.author,
+                    union_subquery.c.repeat
+                )
+            )
+            
+            logger.debug(f'query get sentence = {need_sentence}')
+            need_sentence = need_sentence.first()
+            logger.debug(f'get started book = {need_sentence}')
+            
+            self._title_book = f'{need_sentence.author} - {need_sentence.title}'
+            
+            if need_sentence.repeat:
+                self._title_book = (
+                    'Закончились доступные книги на этом уровне. '
+                    f'Повторим ранее прочитанную книгу\n{self._title_book}\n'
+                )
+            
+            self._need_sentence = need_sentence[0]
+            logger.debug(f'get started book = {self._need_sentence}')
+            
+            new_history_book = UsersBooksHistory(
+                telegram_user_id=self._telegram_id, 
+                book_id=self._need_sentence.book_id,
+            )
+            self._db.add(new_history_book)
 
         return await self._get_sentence_dto()
 
@@ -113,183 +181,7 @@ class ReadBookService:
                 status_code=status.HTTP_206_PARTIAL_CONTENT,
                 detail='You have  read the maximum number of sentences today.')
 
-    async def _get_first_sentence_from_book(self):
-        """Get first sentence from book."""
-        logger.debug(f'Get first sentence from book for user {self._telegram_id}')
-
-        next_book = await self._get_sentence_from_next_book()
-
-        self._title_book = ''
-
-        if next_book:
-            selected_book = next_book
-        else:
-            selected_book = await self._get_sentence_from_random_book()
-            if not selected_book:
-                selected_book = await self._get_sentence_from_old_book()
-
-        self._title_book += f'{selected_book.author} - {selected_book.title}'
-        logger.debug(f'title book: {self._title_book}')
-
-        if selected_book.books_sentences:
-            sorted_sentences = sorted(selected_book.books_sentences, key=lambda sentence: sentence.order)
-            self._need_sentence = sorted_sentences[0]
-        else:
-            self._need_sentence = None
-
-        if not self._need_sentence:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='No sentences available in the book.')
-
-        new_history_book = UsersBooksHistory(telegram_user_id=self._telegram_id, book_id=self._need_sentence.book_id)
-        self._db.add(new_history_book)
-
-    async  def _get_sentence_from_next_book(self):
-        UsersBooksHistoryAlias = aliased(UsersBooksHistory)
-
-        last_read_book_subquery = (
-            self._db.query(UsersBooksHistoryAlias.book_id)
-            .filter(
-                UsersBooksHistoryAlias.telegram_user_id == self._telegram_id,
-                UsersBooksHistoryAlias.end_read.isnot(None)
-            )
-            .order_by(UsersBooksHistoryAlias.end_read.desc())
-            .limit(1)
-            .subquery()
-        )
-
-        next_book = (
-            self._db.query(BooksModel)
-            .options(
-                joinedload(BooksModel.books_sentences).joinedload(BooksSentences.words),
-                joinedload(BooksModel.books_sentences).joinedload(BooksSentences.tenses)
-            )
-            .filter(BooksModel.previous_book_id == last_read_book_subquery)
-            .first()
-        )
-
-        logger.debug('Get first sentence from next book:')
-        logger.debug(f'{self._telegram_id} - {next_book.__dict__ if next_book else "not found"}')
-
-        return next_book
-
-    async def _get_sentence_from_random_book(self):
-        logger.debug(f'Get first sentence from random book for user {self._telegram_id}')
-
-        read_books_subquery = (
-            self._db.query(UsersBooksHistory.book_id)
-            .filter(
-                UsersBooksHistory.telegram_user_id == self._telegram_id,
-                UsersBooksHistory.end_read.isnot(None)
-            )
-            .subquery()
-        )
-
-        selected_book = (
-            self._db.query(BooksModel)
-            .options(
-                joinedload(BooksModel.books_sentences).joinedload(BooksSentences.words),
-                joinedload(BooksModel.books_sentences).joinedload(BooksSentences.tenses)
-            )
-            .filter(
-                BooksModel.level_en_id == self._user.level_en_id,
-                not_(BooksModel.book_id.in_(read_books_subquery)),
-                BooksModel.previous_book_id.is_(None),
-            )
-            .order_by(func.random())
-            .first()
-        )
-
-        logger.debug(f'Get first sentence from random book {selected_book.__dict__ if selected_book else "not found"}')
-
-        return selected_book
-
-    async def _get_sentence_from_old_book(self):
-        selected_book = (
-            self._db.query(BooksModel)
-            .options(
-                joinedload(BooksModel.books_sentences).joinedload(BooksSentences.words),
-                joinedload(BooksModel.books_sentences).joinedload(BooksSentences.tenses)
-            )
-            .filter(
-                BooksModel.level_en_id == self._user.level_en_id,
-                BooksModel.previous_book_id.is_(None),
-            )
-            .order_by(func.random())
-            .first()
-        )
-
-        logger.debug(f'Get old random book {selected_book.__dict__}')
-
-        self._title_book = (
-            f'Закончились доступные книги на этом уровне. Повторим ранее прочитанную книгу\n{self._title_book}\n'
-        )
-
-        logger.debug(f'title book: {self._title_book}')
-        return selected_book
-
-    async def _get_next_sentence(self):
-        UsersBooksSentencesHistoryAlias = aliased(UsersBooksSentencesHistory)
-
-        not_read_sentence_in_user_history = (
-            self._db.query(BooksSentences, UsersBooksSentencesHistoryAlias)
-            .join(
-                UsersBooksSentencesHistoryAlias,
-                BooksSentences.sentence_id == UsersBooksSentencesHistoryAlias.sentence_id
-            )
-            .filter(
-                UsersBooksSentencesHistoryAlias.telegram_user_id == self._telegram_id,
-                UsersBooksSentencesHistoryAlias.is_read.is_(False),
-                BooksSentences.book_id == self._start_read_book.book_id,
-            )
-            .order_by(BooksSentences.order.desc())
-            .first()
-        )
-
-        if not_read_sentence_in_user_history:
-            self._is_new_sentence = False
-            self._need_sentence, user_sentence_history = not_read_sentence_in_user_history
-            self._need_sentence.history_sentence_id = user_sentence_history.id
-            check_words = user_sentence_history.check_words
-            self._need_sentence.check_words = check_words
-            return
-
-        last_read_sentence = (
-            self._db.query(UsersBooksSentencesHistory, BooksSentences)
-            .join(BooksSentences, UsersBooksSentencesHistory.sentence_id == BooksSentences.sentence_id)
-            .filter(
-                UsersBooksSentencesHistory.telegram_user_id == self._telegram_id,
-                UsersBooksSentencesHistory.is_read.is_(True),
-                BooksSentences.book_id == self._start_read_book.book_id,
-            )
-            .order_by(BooksSentences.order.desc())
-            .first()
-        )
-
-        if last_read_sentence:
-            last_read_order = last_read_sentence.BooksSentences.order
-        else:
-            last_read_order = 0
-
-        next_sentence = (
-            self._db.query(BooksSentences)
-            .filter(
-                BooksSentences.book_id == self._start_read_book.book_id,
-                BooksSentences.order > last_read_order,
-            )
-            .order_by(BooksSentences.order)
-            .first()
-        )
-
-        if next_sentence:
-            self._need_sentence = next_sentence
-            return
-
-        self._start_read_book.end_read = datetime.utcnow()
-
-        await self._get_first_sentence_from_book()
-
     async def _get_sentence_dto(self):
-
         subquery = (
             select(sentence_word_association.c.wordsmodel_id)
             .where(sentence_word_association.c.bookssentencesmodel_id == self._need_sentence.sentence_id)
@@ -308,17 +200,13 @@ class ReadBookService:
         logger.debug(f'query = {str(words_with_history)}')
         words_with_history = words_with_history.all()
 
-        self._need_sentence.words = words_with_history
-
-        sentence_info = self._need_sentence.__dict__
-        logger.debug(f'sentence_info: {sentence_info}')
         sentence_for_read = {}
-        sentence_text = sentence_info["text"]
-
-        sentence_for_read['sentence_id'] = sentence_info['sentence_id']
-        sentence_for_read['book_id'] = sentence_info['book_id']
-        sentence_for_read['translation'] = sentence_info['translation']
-        sentence_for_read['order'] = sentence_info['order']
+        sentence_text = self._need_sentence.text
+        sentence_for_read['words'] = words_with_history
+        sentence_for_read['sentence_id'] = self._need_sentence.sentence_id
+        sentence_for_read['book_id'] = self._need_sentence.book_id
+        sentence_for_read['translation'] = self._need_sentence.translation
+        sentence_for_read['order'] = self._need_sentence.order
         sentence_for_read['sentence_times'] = []
         sentence_for_read['description_time'] = []
 
@@ -330,18 +218,18 @@ class ReadBookService:
         sentence_for_read['sentence_times'] = ', '.join(sentence_for_read['sentence_times'])
         sentence_for_read['description_time'] = '\n'.join(sentence_for_read['description_time'])
 
-        words_for_learn, words_for_sentence = await self._get_words_for_learn(words=sentence_info['words'])
+        words_for_learn, words_for_sentence = await self._get_words_for_learn(words=sentence_for_read['words'])
 
-        text_with_words = replace_with_translation(text=sentence_info['text'], words=words_for_sentence['all'])
+        text_with_words = replace_with_translation(text=sentence_text, words=words_for_sentence['all'])
         text_with_new_words = replace_with_translation(text=sentence_text, words=words_for_sentence['new'])
         logger.debug(f'text with words = {text_with_words}')
         logger.debug(f'text with new words = {text_with_new_words}')
         
-        if sentence_info['order'] == 1:
+        if self._need_sentence.order == 1:
             logger.debug(f'it is first sentence in book add title')
-            sentence_text = f'{self._title_book}\n\n{sentence_info["text"]}'
+            sentence_text = f'{self._title_book}\n\n{sentence_text}'
         else:
-            sentence_text = sentence_info['text']
+            sentence_text = sentence_text
         logger.debug(f'sentence text = {sentence_text}')
         sentence_for_read['text'] = sentence_text
 
@@ -353,14 +241,14 @@ class ReadBookService:
             logger.debug('it is new sentence save history in db')
             new_history_sentence = UsersBooksSentencesHistory(
                 telegram_user_id=self._telegram_id,
-                sentence_id=sentence_info['sentence_id'],
+                sentence_id=self._need_sentence.sentence_id,
                 check_words=[word['word_id'] for word in words_for_learn],
             )
             self._db.add(new_history_sentence)
             self._db.flush()
             sentence_for_read['history_sentence_id'] = new_history_sentence.id
         else:
-            sentence_for_read['history_sentence_id'] = sentence_info['history_sentence_id']
+            sentence_for_read['history_sentence_id'] = self._need_sentence.history_sentence_id
         self._db.commit()
 
         sentence = SentenceModelForReadDTO(**sentence_for_read)
