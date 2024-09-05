edc0473ce74347d9461dd4427b64a1030b914852
pavel <psgaiduk@gmail.com>
2024-09-05 10:25:51 +0400

    #87 feat(bot): add book_title in fixture sentence_with_word

--- bot/tests/fixtures/sentence_dto.py
+++ bot/tests/fixtures/sentence_dto.py
@@ -9,6 +9,7 @@ def sentence_with_word(word_new):
     return NewSentenceDTOModel(
         history_sentence_id=1,
         book_id=1,
+        book_title='text_title',
         order=1,
         sentence_id=1,
         text='test_text',
