cef1c1fefecf8dc6dedc0050469d00bb3fdcc5f4
pavel <psgaiduk@gmail.com>
2024-09-10 09:21:35 +0400

    #88 feat(admin) add part of speech in WordsModel

--- admin/books/models/words.py
+++ admin/books/models/words.py
@@ -1,19 +1,22 @@
 from django.db.models import AutoField, CASCADE, CharField, ForeignKey, JSONField, Model
 
+from books.choices import PartOfSpeechChoice
+
 
 class WordsModel(Model):
     """Model of words."""
 
     word_id = AutoField(primary_key=True)
-    type_word = ForeignKey('TypeWordsModel', on_delete=CASCADE)
+    type_word = ForeignKey("TypeWordsModel", on_delete=CASCADE)
     word = CharField(max_length=128, )
     translation = JSONField(blank=True, null=True)
     transcription = CharField(max_length=128)
+    part_of_speech = CharField(max_length=64, choices=PartOfSpeechChoice.choices())
 
     def __str__(self) -> str:
-        return f'{self.word}'
+        return f"{self.word}"
 
     class Meta:
-        db_table = 'words'
-        verbose_name = 'Word'
-        verbose_name_plural = 'Words'
+        db_table = "words"
+        verbose_name = "Word"
+        verbose_name_plural = "Words"
