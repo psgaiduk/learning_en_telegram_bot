14fd26df52df52a168b3483813ceccf5be43c085
pavel <psgaiduk@gmail.com>
2024-09-19 09:21:00 +0400

    #88 feat(admin): add new fields for WordsAdmin

--- admin/books/admin/words.py
+++ admin/books/admin/words.py
@@ -9,8 +9,8 @@ class WordsAdmin(admin.ModelAdmin):
     """Words admin."""
 
     form = get_translate_model_form()
-    list_display = ('__str__', 'type_word')
-    list_filter = ('type_word__title', )
+    list_display = ('__str__', 'type_word', 'part_of_speech', 'translation')
+    list_filter = ('type_word__title', 'part_of_speech')
     search_fields = ('word', )
 
     def get_readonly_fields(self, request, obj=None):
