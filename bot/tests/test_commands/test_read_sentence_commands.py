36e0e8ccb5863c3382a1e833dc1016ba3d5869fe
pavel <psgaiduk@gmail.com>
2024-09-27 09:18:56 +0400

    #82 test(bot): update tests for use send_message_end_read_today_func

--- bot/tests/test_commands/test_read_sentence_commands.py
+++ bot/tests/test_commands/test_read_sentence_commands.py
@@ -54,34 +54,24 @@ class TestReadSentenceCommand:
 
         mock_read_sentence_service.assert_called_once()
 
-    @patch('commands.read_sentence.delete_message')
+    @patch('commands.read_sentence.send_message_end_read_today_func')
     @mark.asyncio
-    async def test_handle_end_read_sentence_today_message(self, mock_delete_message):
+    async def test_handle_end_read_sentence_today_message(self, mock_send_message_end_read_today_func):
         chat_id = 1
         user = User(id=chat_id, is_bot=False, first_name='Test User')
         mock_message = Message(id=1, chat=chat_id, text='Read', from_user=user)
         mock_message.from_user = user
 
-        with patch.object(bot, 'send_message', new=AsyncMock()) as mock_send_message:
-            await handle_end_read_sentence_today(mock_message)
-            expected_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-            expected_keyboard.add(KeyboardButton(text='Read'))
-            expected_text = 'Вы прочитали все предложения на сегодня. Приходите завтра.'
-            mock_send_message.assert_called_once_with(chat_id=chat_id, text=expected_text, reply_markup=expected_keyboard, parse_mode=ParseMode.HTML)
-            mock_delete_message.assert_called_once_with(message=mock_message)
+        await handle_end_read_sentence_today(mock_message)
+        mock_send_message_end_read_today_func.assert_called_once_with(message=mock_message)
 
-    @patch('commands.read_sentence.delete_message')
+    @patch('commands.read_sentence.send_message_end_read_today_func')
     @mark.asyncio
-    async def test_handle_end_read_sentence_today_callback(self, mock_delete_message):
+    async def test_handle_end_read_sentence_today_callback(self, mock_send_message_end_read_today_func):
         chat_id = 1
         user = User(id=chat_id, is_bot=False, first_name='Test User')
         mock_callback = CallbackQuery(id=1, chat=chat_id, data='other_data', from_user=user)
         mock_callback.from_user = user
 
-        with patch.object(bot, 'send_message', new=AsyncMock()) as mock_send_message:
-            await handle_end_read_sentence_today(mock_callback)
-            expected_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-            expected_keyboard.add(KeyboardButton(text='Read'))
-            expected_text = 'Вы прочитали все предложения на сегодня. Приходите завтра.'
-            mock_send_message.assert_called_once_with(chat_id=chat_id, text=expected_text, reply_markup=expected_keyboard, parse_mode=ParseMode.HTML)
-            mock_delete_message.assert_called_once_with(message=mock_callback)
+        await handle_end_read_sentence_today(mock_callback)
+        mock_send_message_end_read_today_func.assert_called_once_with(message=mock_callback)
