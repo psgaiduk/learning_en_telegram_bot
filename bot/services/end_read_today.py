2e2d10a95a594d670018152f9589e38dc4f3bd7b
pavel <psgaiduk@gmail.com>
2024-10-26 22:06:36 +0400

    #99 feat(bot): create _get_messages_for_delete in TestEndReadTodayService

--- bot/services/end_read_today.py
+++ bot/services/end_read_today.py
@@ -13,17 +13,15 @@ from aiogram.dispatcher.storage import FSMContext
 from loguru import logger
 
 from bot import bot
-from dto import TelegramUserDTOModel
 from functions import delete_message
 
 
 class EndReadTodayService:
     """Service for reading sentence."""
 
-    _telegram_user: TelegramUserDTOModel
-    _sentence_text: str
-    _sentence_translation: str
-    _message_text: str
+    message: Union[CallbackQuery, Message]
+    state: FSMContext
+    messages_for_delete: list
 
     def __init__(self, message: Union[CallbackQuery, Message], state: FSMContext) -> None:
         """Init."""
@@ -37,9 +35,6 @@ class EndReadTodayService:
         keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(KeyboardButton(text="Read"))
 
-        data = await self.state.get_data()
-        messages_for_delete = data.get("messages_for_delete", [])
-
         send_message = await bot.send_message(
             chat_id=self.message.from_user.id,
             text=message_text,
@@ -47,7 +42,7 @@ class EndReadTodayService:
             reply_markup=keyboard,
         )
         logger.debug(f"send message = {send_message.__dict__}")
-        messages_for_delete.append(send_message.message_id)
+        self.messages_for_delete.append(send_message.message_id)
 
         message_with_grammar = "Но можно продолжить повторение слов"
         inline_keyboard = InlineKeyboardMarkup()
@@ -58,5 +53,9 @@ class EndReadTodayService:
             parse_mode=ParseMode.HTML,
             reply_markup=inline_keyboard,
         )
-        messages_for_delete.append(send_message.message_id)
-        await self.state.update_data(messages_for_delete=messages_for_delete)
+        self.messages_for_delete.append(send_message.message_id)
+        await self.state.update_data(messages_for_delete=self.messages_for_delete)
+
+    async def _get_messages_for_delete(self) -> None:
+        data = await self.state.get_data()
+        self.messages_for_delete = data.get("messages_for_delete", [])
--- bot/tests/test_services/test_end_read_sentence_today.py
+++ bot/tests/test_services/test_end_read_sentence_today.py
@@ -9,7 +9,7 @@ from aiogram.types import (
     KeyboardButton,
     User,
 )
-from pytest import mark
+from pytest import fixture, mark
 
 from bot import bot
 from services import EndReadTodayService
@@ -19,23 +19,29 @@ from tests.fixtures import *
 class TestEndReadTodayService:
     """Tests send message if end read today service."""
 
+    @fixture(autouse=True)
+    def setup_method(self):
+        self.state = AsyncMock()
+        self.chat_id = 1
+        user = User(id=self.chat_id, is_bot=False, first_name="Test User")
+        self.mock_message = Message(id=1, chat=self.chat_id, text="Read", from_user=user)
+        self.mock_message.from_user = user
+        self.service = EndReadTodayService(message=self.mock_message, state=self.state)
+
     @patch("services.end_read_today.delete_message")
-    @mark.parametrize("state_data, expected_ids", [[{}, [1, 2]], [{"messages_for_delete": [3]}, [3, 1, 2]]])
+    @mark.parametrize("old_messages_ids, expected_ids", [[[], [1, 2]], [[3], [3, 1, 2]]])
     @mark.asyncio
-    async def test_send_message_if_end_sentences(self, mock_delete_message, state_data, expected_ids):
-        chat_id = 1
-        state = AsyncMock()
-        user = User(id=chat_id, is_bot=False, first_name="Test User")
-        mock_message = Message(id=1, chat=chat_id, text="Read", from_user=user)
-        mock_message.from_user = user
+    async def test_send_message_if_end_sentences(self, mock_delete_message, old_messages_ids, expected_ids):
+
+        self.service.messages_for_delete = old_messages_ids
 
         with patch.object(bot, "send_message", new=AsyncMock()) as mock_send_message:
-            state.get_data = AsyncMock(return_value=state_data)
+
             mock_send_message.side_effect = [
                 AsyncMock(message_id=1),
                 AsyncMock(message_id=2),
             ]
-            await EndReadTodayService(message=mock_message, state=state).work()
+            await self.service.work()
             expected_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
             expected_keyboard.add(KeyboardButton(text="Read"))
             expected_text = "Вы прочитали все предложения на сегодня. Новые предложения будут доступны завтра."
@@ -48,13 +54,13 @@ class TestEndReadTodayService:
             mock_send_message.assert_has_calls(
                 [
                     call(
-                        chat_id=chat_id,
+                        chat_id=self.chat_id,
                         text=expected_text,
                         parse_mode=ParseMode.HTML,
                         reply_markup=expected_keyboard,
                     ),
                     call(
-                        chat_id=chat_id,
+                        chat_id=self.chat_id,
                         text=expected_text_second,
                         parse_mode=ParseMode.HTML,
                         reply_markup=expected_keyboard_second,
@@ -65,5 +71,12 @@ class TestEndReadTodayService:
 
             assert mock_send_message.call_count == 2
 
-            mock_delete_message.assert_called_once_with(message=mock_message, state=state)
-            state.update_data.assert_called_once_with(messages_for_delete=expected_ids)
+            mock_delete_message.assert_called_once_with(message=self.mock_message, state=self.state)
+            self.state.update_data.assert_called_once_with(messages_for_delete=expected_ids)
+
+    @mark.parametrize("state_data, expected_ids", [[{}, []], [{"messages_for_delete": [3]}, [3]]])
+    @mark.asyncio
+    async def test_get_messages_for_delete(self, state_data, expected_ids) -> None:
+        self.state.get_data = AsyncMock(return_value=state_data)
+        await self.service._get_messages_for_delete()
+        assert self.service.messages_for_delete == expected_ids
